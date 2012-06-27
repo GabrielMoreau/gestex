22d57ca04ef7dfe38ec5f9b4f296039abe263993
Gabriel Moreau <Gabriel.Moreau@univ-grenoble-alpes.fr>
2012-06-27 18:43:04 +0000

    * Lots a small typo...

--- index.php
+++ index.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-  <meta http-equiv="content-type"
- content="text/html; charset=ISO-8859-1">
+  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
   <title>Gestion de Projets Legi</title>
-<link href="pool_project.css" rel="stylesheet" type="text/css">
+  <link href="pool_project.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <br>
 <div width="100%" height="100%" align="center" valign="center">
-<?php require("html_functions.php"); en_tete("Gestion des plateformes expérimentales du LEGI");?>
-  <br>
-
-GestEx est une application web developp&eacute;s au LEGI (collaboration entre les services technique / intrumentation / informatique)
-destin&eacute; &agrave; g&eacute;rer l'historique des montages exerimentaux du laboratoire.<br>
-Il faut &ecirc;tre utilisateur referenc&eacute; pour pouvoir acc&eacute;der &agrave; ce systeme.
-<br>
-<table cellpadding="2" cellspacing="2" border="0"
- style="text-align: center; width: 75%;" align="center">
+<?php require("html_functions.php"); en_tete("Gestion des plateformes exp&eacute;rimentales du LEGI");?>
+<br />
+GestEx est une application web developp&eacute;e au LEGI (collaboration entre les services technique / instrumentation / informatique)
+destin&eacute;e &agrave; g&eacute;rer l'historique des montages exp&eacute;rimentaux du laboratoire.<br>
+Il faut &ecirc;tre utilisateur r&eacute;f&eacute;renc&eacute; pour pouvoir acc&eacute;der &agrave; ce syst&egrave;me.
+<br />
+<table cellpadding="2" cellspacing="2" border="0" style="text-align: center; width: 75%;" align="center">
   <tbody>
     <tr>
       <td style="vertical-align: top;">
-	<a href="add_user.php">Demander son inscription</a><br>
+        <a href="add_user.php">Demander son inscription</a><br />
       </td>
       <td style="vertical-align: top;">
-	<a href="login.php?variable=projet">Acceder au gestionnaire de Projets</a><br>
+        <a href="login.php?variable=projet">Acceder au gestionnaire de Projets</a><br />
       </td>
     </tr>
- </tbody>
+  </tbody>
 </table>
-  <br>
-<br>
+<br />
+<br />
 </div>
 <?php pied_page() ?>
 </body>
