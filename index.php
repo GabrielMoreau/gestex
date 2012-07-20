8b9c707fe2932bfff16ef55764e0ebe85926e620
Gabriel Moreau <Gabriel.Moreau@univ-grenoble-alpes.fr>
2012-07-20 12:00:16 +0000

    * Mise en place d'un portail central

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
-  <title>Gestion de Projets LEGI</title>
+  <title>Portail GestEx</title>
   <link href="pool_project.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
@@ -12,23 +12,24 @@
 <br />
 
 <p>GestEx est une application web developp&eacute;e au LEGI (collaboration entre les services technique / instrumentation / informatique)
-destin&eacute;e &agrave; g&eacute;rer l'historique des montages exp&eacute;rimentaux du laboratoire.</p>
-<p>Il faut &ecirc;tre un utilisateur r&eacute;f&eacute;renc&eacute; pour pouvoir acc&eacute;der &agrave; ce syst&egrave;me.</p>
+destin&eacute;e &agrave; g&eacute;rer l'historique des montages et le suivi de l'instrumentation des plateformes exp&eacute;rimentales du laboratoire.</p>
 
 <br />
 
-<table cellpadding="2" cellspacing="2" border="0" style="text-align: center; width: 75%;" align="center">
-  <tbody>
-    <tr>
-      <td style="vertical-align: top;">
-        <a href="add_user.php">Demander son inscription</a>
-      </td>
-      <td style="vertical-align: top;">
-        <a href="login.php?variable=projet">Acc&eacute;der au gestionnaire de Projets</a>
-      </td>
-    </tr>
-  </tbody>
-</table>
+<h2>Gestion intrumention</h2>
+<ul>
+  <li><a href="essai1.php">inventaire des matériels</a> sous sa responsabilité.</li>
+  <li><a href="pret.php">gestion des prêts des appareils</a> du service instrumentation</li>
+  <li><a href="labview.php">liste des programmes Labview développés</a> au LEGI</li>
+</ul>
+
+<h2>Gestion des projets techniques</h2>
+<p>Il faut &ecirc;tre un utilisateur r&eacute;f&eacute;renc&eacute; pour pouvoir acc&eacute;der &agrave; cette partie.</p>
+<ul>
+  <li><a href="add_user.php">Demander son inscription</a></li>
+  <li><a href="login.php?variable=projet">Acc&eacute;der au gestionnaire de Projets</a></li>
+</ul>
+
 <br />
 <br />
 </div>
