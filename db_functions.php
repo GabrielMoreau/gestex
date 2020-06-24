e138eeedd192155db3f0408ff22e1735c31c30c8
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-06-24 18:01:47 +0200

    mise en place de pdo

--- db_functions.php
+++ db_functions.php
@@ -3,38 +3,50 @@
 require("connect.php");
 
 //////////////////////////////////////////////////////
-function connect_db(){
+// function connect_db(){
 //connexion au serveur mySQL
-$connexion= mysql_pconnect(SERVER, USER, PASSWD);
+// $connexion= mysql_pconnect(SERVER, USER, PASSWD);
 
 // selection de la base bultaco
-return mysql_select_db(DATABASE, $connexion);
+// return mysql_select_db(DATABASE, $connexion);
+// }
+
+
+function connect_db(){
+  try{
+    $db = new PDO('mysql:host='.SERVER.'; dbname='.DATABASE, USER, PASSWD);
+  } catch(PDOException $exception){
+    error_log('Connection error: '.$exception->getMessage());
+    echo $exception->getMessage();
+    return false;
+  }
+  return $db;
 }
 
- function query_db ($statement) {
-   $result=mysql_query($statement) or die("<pre>\n\nCan't perform query: " . mysql_error() . " \n\n$statement\n\n</pre>");
-    $num_rows = numrows_db($result);
-    return array($result, $num_rows);
-  }
 
-  function numrows_db ($result) 
-  {
-    return @mysql_num_rows($result);
-  }
-  
-  function result_db ($result,$i=-1) {
-    if ($i >= 0) {
-      @mysql_data_seek($result,$i);
-    }
-    return mysql_fetch_array($result);
-  }
-  
-  function last_id_db() {
-    return mysql_insert_id();
-  }
 
-  function check_val( $table, $col, $value )
-  {
+function query_db ($statement) {
+  $result=mysql_query($statement) or die("<pre>\n\nCan't perform query: " . mysql_error() . " \n\n$statement\n\n</pre>");
+  $num_rows = numrows_db($result);
+  return array($result, $num_rows);
+}
+
+function numrows_db ($result){
+  return @mysql_num_rows($result);
+}
+  
+function result_db ($result,$i=-1) {
+  if ($i >= 0) {
+    @mysql_data_seek($result,$i);
+  }
+  return mysql_fetch_array($result);
+}
+  
+function last_id_db() {
+  return mysql_insert_id();
+}
+
+function check_val( $table, $col, $value ){
   //teste l'existence de $value dans le champ $col de la table $table
 	//echo "check in:".$table.":".$col." for ".$value."<br />";
 
@@ -42,8 +54,8 @@ return mysql_select_db(DATABASE, $connexion);
 	
 	///echo "check_val:".numrows_db($reponse)."<br />";
 
-  //renvoie 0 si non trouvé
+  //renvoie 0 si non trouvï¿½
   //renvoie le nbre d'occurences autrement
-	return @mysql_num_rows($reponse);;
-  }
+	return @mysql_num_rows($reponse);
+}
 ?>
