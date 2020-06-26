75aa137cad6ecbd9af1fb85118308427ecd06901
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-06-27 00:10:08 +0200

    * Rename #define to connec to the database

--- connect.php
+++ connect.php
@@ -3,10 +3,10 @@
 //connect.php
 //infos de connexions a la base mySQL 
 
-define('USER', "pool");
-define('PASSWD', "web;2pool");
-define('SERVER', "localhost");
-define('DATABASE', "pool");
+define('GESTEX_DB_USER',     "pool");
+define('GESTEX_DB_PASSWORD', "web;2pool");
+define('GESTEX_DB_SERVER',   "localhost");
+define('GESTEX_DB_DATABASE', "pool");
 
 define('GESTEX_ADMIN_MAIL',  "Gabriel.Moreau@legi.grenoble-inp.fr");
 define('GESTEX_URL_ENTITY',  "http://intranet.legi.grenoble-inp.fr/");
--- db_functions.php
+++ db_functions.php
@@ -4,58 +4,56 @@ require("connect.php");
 
 //////////////////////////////////////////////////////
 // function connect_db(){
-//connexion au serveur mySQL
+// connexion au serveur mySQL
 // $connexion= mysql_pconnect(SERVER, USER, PASSWD);
 
 // selection de la base bultaco
 // return mysql_select_db(DATABASE, $connexion);
 // }
 
-
-function connect_db(){
-  try{
-    $db = new PDO('mysql:host='.SERVER.'; dbname='.DATABASE, USER, PASSWD);
-  } catch(PDOException $exception){
-    error_log('Connection error: '.$exception->getMessage());
-    echo $exception->getMessage();
-    return false;
-  }
-  return $db;
+function connect_db() {
+	try{
+		$db = new PDO('mysql:host='.GESTEX_DB_SERVER.'; dbname='.GESTEX_DB_DATABASE, GESTEX_DB_USER, GESTEX_DB_PASSWORD);
+	}
+	catch(PDOException $exception){
+		error_log('Connection error: '.$exception->getMessage());
+		echo $exception->getMessage();
+		return false;
+	}
+	return $db;
 }
 
-
-
-function query_db ($statement) {
-  $result=mysql_query($statement) or die("<pre>\n\nCan't perform query: " . mysql_error() . " \n\n$statement\n\n</pre>");
-  $num_rows = numrows_db($result);
-  return array($result, $num_rows);
+function query_db($statement) {
+	$result   = mysql_query($statement) or die("<pre>\n\nCan't perform query: " . mysql_error() . " \n\n$statement\n\n</pre>");
+	$num_rows = numrows_db($result);
+	return array($result, $num_rows);
 }
 
-function numrows_db ($result){
-  return @mysql_num_rows($result);
+function numrows_db($result) {
+	return @mysql_num_rows($result);
 }
   
-function result_db ($result,$i=-1) {
-  if ($i >= 0) {
-    @mysql_data_seek($result,$i);
-  }
-  return mysql_fetch_array($result);
+function result_db($result,$i=-1) {
+	if ($i >= 0) {
+		@mysql_data_seek($result,$i);
+	}
+	return mysql_fetch_array($result);
 }
   
 function last_id_db() {
-  return mysql_insert_id();
+	return mysql_insert_id();
 }
 
-function check_val( $table, $col, $value ){
-  //teste l'existence de $value dans le champ $col de la table $table
+function check_val($table, $col, $value) {
+	//teste l'existence de $value dans le champ $col de la table $table
 	//echo "check in:".$table.":".$col." for ".$value."<br />";
 
-	$reponse = mysql_query ("select * from $table where $col='$value' ");
-	
+	$reponse = mysql_query("select * from $table where $col='$value' ");
+
 	///echo "check_val:".numrows_db($reponse)."<br />";
 
-  //renvoie 0 si non trouve
-  //renvoie le nbre d'occurences autrement
+	//renvoie 0 si non trouve
+	//renvoie le nbre d'occurences autrement
 	return @mysql_num_rows($reponse);
 }
 ?>
