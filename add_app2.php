208d659133a20cee89fe83d36605ef9e1b856ae4
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-06-26 11:14:07 +0200

    mise en place de pdo

--- add_app2.php
+++ add_app2.php
@@ -10,43 +10,54 @@
 	$logged_in_user = strtolower($_SESSION['logged_in_user']);
 
 //recuper la methode de tri
-$tri = $_GET[tri];
-if (empty($tri))
+if (empty($_GET['tri']))
 	$tri ="id";
+else
+	$tri = $_GET['tri'];
 
-$cat=$_GET[categorie];
+if (empty($_GET['categorie']))
+	$cat = "";
+else
+	$cat=$_GET['categorie'];
 //echo "$cat";
-//recupère la catégorie de la page categorie
-$app_id = $_GET[id];
+//recupï¿½re la catï¿½gorie de la page categorie
+if (empty($_GET['id']))
+	$app_id = "";
+else
+	$app_id = $_GET['id'];
 
 
-if (empty($app_id)){
+
 
 	// recupere la liste de appareils
-if ( $connex = connect_db() ){
+if ( $pdo = connect_db() ){
 
 
-$querry = "SELECT * FROM categorie where id='$cat'" ;
-	list($qh,$num) = query_db($querry);
-	$last_id=0;
-$datax = result_db($qh);}
-
+$sql = 'SELECT * FROM categorie where id = ?;' ;
+// 	list($qh,$num) = query_db($querry);
+// 	$last_id=0;
+// $datax = result_db($qh);}
+$stmt = $pdo->prepare($sql);
+$stmt->execute(array($cat));
+$categorie = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (empty($app_id)){ 
 	//->nouvel appareil
 	$mode ="ajouter";
-	$action="valid_app2.php?categorie=".$datax[id]."";
-//transmet la valeur de la categorie à la page valid appareil
+	$action="valid_app2.php?categorie=".$cat;
+//transmet la valeur de la categorie ï¿½ la page valid appareil
 }
 else{
 
 
 	//->modif appareil
 	$mode ="modifier";
-	$action="modif_app2.php?categorie=".$datax[id]."";
+	$action="modif_app2.php?categorie=".$cat;
 
+}
 }
 
 require("mise_en_page.php");
-if ( $connex = connect_db() ){
+if ( $pdo = connect_db() ){
 if ($mode=="ajouter"){
 	en_tete("Voila un formulaire pour ajouter un appareil");
 
@@ -54,11 +65,13 @@ if ($mode=="ajouter"){
 else if ($mode=="modifier"){
 	en_tete("Voila un formulaire pour modifier les caracteristiques d'un appareil");
 
-	// recupere le appareil selectionné
-	$querry = "SELECT * FROM Listing WHERE id='$app_id'";
-	list($qh,$num) = query_db($querry);
-	$data = result_db($qh);
-
+	// recupere le appareil selectionnï¿½
+	$sql = 'SELECT * FROM Listing WHERE id = ?';
+	// list($qh,$num) = query_db($querry);
+	// $data = result_db($qh);
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute(array($app_id));
+	$listing = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 	
 }
@@ -91,18 +104,23 @@ else if ($mode=="modifier"){
 <select name="categorie">
 //listing des categories
 <?php
-	$querry = "SELECT id, nom FROM categorie order by nom ";
-	list($qheq,$numeq) = query_db($querry);
-		while ($chef = result_db($qheq)){
-			echo "<option value=\"".$chef[id]."\"";
-			if ($mode=="modifier" && $chef[id] == $data[categorie]) {
+	$sql = 'SELECT id, nom FROM categorie order by nom ';
+	// list($qheq,$numeq) = query_db($querry);
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute();
+	$categorie = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	// 	while ($chef = result_db($qheq)){
+		foreach($categorie as $chef){
+			echo "<option value=\"".$chef['id']."\"";
+			if ($mode=="modifier" && $chef['id'] == $listing[0]['categorie']) {
 				echo " selected";	}
-if ($mode=="ajouter" && $chef[id] == $cat) {
+if ($mode=="ajouter" && $chef['id'] == $cat) {
 				echo " selected";	}
 
-//si on choisit ajouter, le listing présélectionne la categorie
+//si on choisit ajouter, le listing prï¿½sï¿½lectionne la categorie
 
-			echo ">".$chef[nom]."</option>";
+			echo ">".$chef['nom']."</option>";
 		}//end while
 
 
@@ -119,7 +137,7 @@ if ($mode=="ajouter" && $chef[id] == $cat) {
       <td style="vertical-align: top;">Nom *<br />
       </td>
       <td style="vertical-align: top;">
-	<input type="text" name="nom" size="30"  value="<?php echo $data[nom] ?>" ><br />
+	<input type="text" name="nom" size="30"  value="<?php if ($mode == "modifier"){ echo $listing[0]['nom'];} ?>" ><br />
       </td>
     </tr>
 
@@ -127,14 +145,14 @@ if ($mode=="ajouter" && $chef[id] == $cat) {
       <td style="vertical-align: top;">Modele *<br />
       </td>
       <td style="vertical-align: top;">
-      <input type="text"name="modele" size="30" value="<?php echo $data[modele] ?>"<br />
+      <input type="text"name="modele" size="30" value="<?php if ($mode == "modifier"){ echo $listing[0]['modele'] ;}?>"<br />
 	</td>
     </tr>  
 <tr>
  <td style="vertical-align: top;">Gamme *<br />
       </td>
       <td style="vertical-align: top;">
-     	<input type="text" name="gamme" size="10" maxlength="30" value="<?php echo $data[gamme] ?>" ><br />
+     	<input type="text" name="gamme" size="10" maxlength="30" value="<?php if ($mode == "modifier"){echo $listing[0]['gamme'] ;}?>" ><br />
 
 	</td>
     </tr>  
@@ -147,13 +165,17 @@ if ($mode=="ajouter" && $chef[id] == $cat) {
 	<select name="equipe">
 	<?php 
 	// recupere la liste des equipes
-	$querry = "SELECT id, nom FROM equipe order by nom";
-	list($qheq,$numeq) = query_db($querry);
-		while ($chef = result_db($qheq)){
-			echo "<option value=\"".$chef[id]."\"";
-			if ($mode=="modifier" && $chef[id] == $data[equipe]) {
+	$sql = 'SELECT id, nom FROM equipe order by nom';
+	// list($qheq,$numeq) = query_db($querry);
+	// 	while ($chef = result_db($qheq)){
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+		$equipe = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		foreach($equipe as $chef){
+			echo "<option value=\"".$chef['id']."\"";
+			if ($mode=="modifier" && $chef['id'] == $listing[0]['equipe']) {
 				echo " selected";	}
-			echo ">".$chef[nom]."</option>";
+			echo ">".$chef['nom']."</option>";
 		}//end while
 		 ?>
 	</select><br />
@@ -167,13 +189,17 @@ if ($mode=="ajouter" && $chef[id] == $cat) {
 	<select name="fourn">
 	<?php 
 	// recupere la liste des fournisseurs
-	$querry = "SELECT id, nom FROM fournisseurs order by nom ";
-	list($qheq,$numeq) = query_db($querry);
-		while ($chef = result_db($qheq)){
-			echo "<option value=\"".$chef[id]."\"";
-			if ($mode=="modifier" && $chef[id] == $data[fournisseur]) {
+	$sql = 'SELECT id, nom FROM fournisseurs order by nom ';
+	// list($qheq,$numeq) = query_db($querry);
+	// 	while ($chef = result_db($qheq)){
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+		$fournisseur = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		foreach($fournisseur as $chef){
+			echo "<option value=\"".$chef['id']."\"";
+			if ($mode=="modifier" && $chef['id'] == $listing[0]['fournisseur']) {
 				echo " selected";	}
-			echo ">".$chef[nom]."</option>";
+			echo ">".$chef['nom']."</option>";
 		}//end while
 		 ?>
 	</select><br />
@@ -188,7 +214,7 @@ if ($mode=="ajouter" && $chef[id] == $cat) {
       <th style="vertical-align: top;">
 	<input type="text" name="achat" size="10" maxlength="10" value="<?php
  if ($mode =="modifier")
-			echo $data[achat];
+			echo $listing[0]['achat'];
 		else 
 			echo date('Y-m-d', time() );
 	?>" ><br />
@@ -202,13 +228,18 @@ if ($mode=="ajouter" && $chef[id] == $cat) {
      <select name="tech">
 	<?php 
 	// recupere la liste des tech
-	$querry = "SELECT id, nom FROM users WHERE level >1";
-	list($qheq,$numeq) = query_db($querry);
-		while ($chef = result_db($qheq)){
-			echo "<option value=\"".$chef[id]."\"";
-			if ($mode=="modifier" && $chef[id] == $data[responsable]) {
+	$sql = 'SELECT id, nom FROM users WHERE level >1';
+	// list($qheq,$numeq) = query_db($querry);
+	// 	while ($chef = result_db($qheq)){
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+		$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		foreach($user as $chef){
+
+			echo "<option value=\"".$chef['id']."\"";
+			if ($mode=="modifier" && $chef['id'] == $listing[0]['responsable']) {
 				echo " selected";	}
-			echo ">".$chef[nom]."</option>";
+			echo ">".$chef['nom']."</option>";
 		}//end while
 		 ?>
 	</select><br />
@@ -220,14 +251,14 @@ if ($mode=="ajouter" && $chef[id] == $cat) {
       <td style="vertical-align: top;">Reparation *<br />
       </td>
       <td style="vertical-align: top;">
-	<input type="text" name="reparation" size="30" maxlength="30" value="<?php echo $data[reparation] ?>" ><br />
+	<input type="text" name="reparation" size="30" maxlength="30" value="<?php if ($mode == "modifier"){ echo $listing[0]['reparation'];} ?>" ><br />
       </td>
     </tr>
 <tr>
  <td style="vertical-align: top;">Accessoires *<br />
       </td>
       <td style="vertical-align: top;">
-	<input type="text" name="accessoires" size="30" maxlength="30" value="<?php echo $data[accessoires] ?>" ><br />
+	<input type="text" name="accessoires" size="30" maxlength="30" value="<?php if ($mode == "modifier"){echo $listing[0]['accessoires'];} ?>" ><br />
       </td>
     </tr>
 
@@ -235,7 +266,7 @@ if ($mode=="ajouter" && $chef[id] == $cat) {
  <td style="vertical-align: top;">Inventaire (facultatif)<br />
       </td>
       <td style="vertical-align: top;">
-	<input type="text" name="inventaire" size="30" maxlength="30" value="<?php echo $data[inventaire] ?>" ><br />
+	<input type="text" name="inventaire" size="30" maxlength="30" value="<?php if ($mode == "modifier"){echo $listing[0]['inventaire'];} ?>" ><br />
       </td>
     </tr>
 
@@ -243,7 +274,7 @@ if ($mode=="ajouter" && $chef[id] == $cat) {
  <td style="vertical-align: top;">Notice (facultatif)<br />
       </td>
       <td style="vertical-align: top;">
-	<input type="text" name="notice" size="30" maxlength="30" value="<?php echo $data[notice] ?>" ><br />
+	<input type="text" name="notice" size="30" maxlength="30" value="<?php if ($mode == "modifier"){echo $listing[0]['notice'];} ?>" ><br />
       </td>
     </tr>
 
