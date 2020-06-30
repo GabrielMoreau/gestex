499cca817457fea91a0c3388f773187379d90b32
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-06-30 15:04:55 +0200

    mise en place de pdo

--- add_intapp.php
+++ add_intapp.php
@@ -9,44 +9,50 @@
 	$user_id = $_SESSION['user_id'];
 	$logged_in_user = strtolower($_SESSION['logged_in_user']);
 
-$app_id = $_GET[app];
-if (empty($app_id))
+if (empty($_GET['app']))
 	Header("Location: list_intapp.php");
-
-$int_id = $_GET[id];
-if (empty($int_id)){
+else{
+	$app_id = $_GET['app'];
+}
+if (empty($_GET['id'])){
 	//->nouvel intervention
 	$mode ="ajouter";
 	$action="valid_intapp.php";
+	$int_id = '';
 }
 else{
 	//->modif intervention
 	$mode ="modifier";
 	$action="modif_intapp.php";
+	$int_id = $_GET['id'];
 
 }
 
 require("html_functions.php");
-if ( $connex = connect_db() ){
+if ( $pdo = connect_db() ){
 
-// recupere le appareil selectionné
-	$querry = "SELECT nom FROM appareils WHERE id='$app_id'";
-	list($qh,$num) = query_db($querry);
-	$data = result_db($qh);
+// recupere le appareil selectionnï¿½
+	$sql = 'SELECT nom FROM appareils WHERE id = ?;';
+	// list($qh,$num) = query_db($querry);
+	// $data = result_db($qh);
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute(array($app_id));
+	$appareil = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 if ($mode=="ajouter"){
-	$titre= "Formulaire pour ajouter une intervention à ".$data['nom'];
+	$titre= "Formulaire pour ajouter une intervention ï¿½ ".$appareil[0]['nom'];
 
 }
 else if ($mode=="modifier"){
-	$titre="Formulaire pour modifier les caracteristiques d'une intervention à ".$data['nom'];
+	$titre="Formulaire pour modifier les caracteristiques d'une intervention ï¿½ ".$appareil[0]['nom'];
 
-	// recupere l'intervention selectionnée
-	$querry = "SELECT * FROM intervention WHERE id='$int_id'";
-	list($qh,$num) = query_db($querry);
-	$data = result_db($qh);
-
-	
+	// recupere l'intervention selectionnï¿½e
+	$sql = 'SELECT * FROM intervention WHERE id = ?;';
+	// list($qh,$num) = query_db($querry);
+	// $data = result_db($qh);
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute(array($int_id));
+	$intervention = $stmt->fetchAll(PDO::FETCH_ASSOC);	
 }
 en_tete($titre);
 ?>
@@ -62,7 +68,7 @@ en_tete($titre);
       <td style="vertical-align: top;">Description<br />
       </td>
       <td style="vertical-align: top;">
-      <textarea name="descr" cols="50" rows="5"> <?php echo $data['descr'] ?></textarea>
+      <textarea name="descr" cols="50" rows="5"> <?php if($mode == 'modifier'){ echo $intervention[0]['descr']; } ?></textarea>
 	</td>
     </tr>  
    
@@ -74,13 +80,17 @@ en_tete($titre);
 	<select name="tech">
 	<?php 
 	// recupere la liste des tech
-	$querry = "SELECT id, nom FROM users WHERE level >1";
-	list($qheq,$numeq) = query_db($querry);
-		while ($chef = result_db($qheq)){
-			echo "<option value=\"".$chef[id]."\"";
-			if ($mode=="modifier" && $chef[id] == $data['tech']) {
+	$sql = 'SELECT id, nom FROM users WHERE level >1;';
+	// list($qheq,$numeq) = query_db($querry);
+	// 	while ($chef = result_db($qheq)){
+		$stmt = $pdo->prepare($sql);
+        $stmt->execute();
+		$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		foreach($user as $chef){
+			echo "<option value=\"".$chef['id']."\"";
+			if ($mode=="modifier" && $chef['id'] == $intervention[0]['tech']) {
 				echo " selected";	}
-			echo ">".$chef[nom]."</option>";
+			echo ">".$chef['nom']."</option>";
 		}//end while
 		 ?>
 	</select><br />
@@ -94,13 +104,17 @@ en_tete($titre);
 	<select name="fourn">
 	<?php 
 	// recupere la liste des fournisseurs
-	$querry = "SELECT id, nom FROM fournisseurs ";
-	list($qheq,$numeq) = query_db($querry);
-		while ($chef = result_db($qheq)){
-			echo "<option value=\"".$chef[id]."\"";
-			if ($mode=="modifier" && $chef[id] == $data['fournisseur']) {
+	$sql = 'SELECT id, nom FROM fournisseurs;';
+	// list($qheq,$numeq) = query_db($querry);
+	// 	while ($chef = result_db($qheq)){
+		$stmt = $pdo->prepare($sql);
+        $stmt->execute();
+		$fournisseur = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		foreach($fournisseur as $fourn){
+			echo "<option value=\"".$fourn['id']."\"";
+			if ($mode=="modifier" && $fourn['id'] == $intervention[0]['fournisseur']) {
 				echo " selected";	}
-			echo ">".$chef[nom]."</option>";
+			echo ">".$fourn['nom']."</option>";
 		}//end while
 		 ?>
 	</select><br />
@@ -114,7 +128,7 @@ en_tete($titre);
 	<input type="text" name="date" size="10" maxlength="10" value="
 	<?php 
 	if ($mode == "modifier")
-		echo $data['date'] ;
+		echo $intervention[0]['date'] ;
 	else
 		echo date('Y-m-d', time() );
        ?>"><br /></td>
@@ -124,7 +138,7 @@ en_tete($titre);
       <td style="vertical-align: top;">Facture<br />
       </td>
       <td style="vertical-align: top;">
-	<input type="text" name="facture" size="30" maxlength="30" value="<?php echo $data['facture'] ?>" ><br />
+	<input type="text" name="facture" size="30" maxlength="30" value="<?php if($mode == 'modifier'){  echo $intervention[0]['facture']; } ?>" ><br />
       </td>
     </tr><tr>
 
