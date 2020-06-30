204b7b4a15e305029c9b7cb2eb4af130ca6e99b5
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-06-30 09:23:26 +0200

    mise en place de pdo

--- add_manip.php
+++ add_manip.php
@@ -10,15 +10,13 @@
 	$user_id = $_SESSION['user_id'];
 	$logged_in_user = strtolower($_SESSION['logged_in_user']);
 
-$manip_id = $_GET[id];
-if (empty($manip_id)){
+if (empty($_GET['id'])){
 	//->nouvelle manip
 	$mode ="ajouter";
 	$action="valid_manip.php";
-}
-else{
+}else{
 	//->modif manip
-
+	$manip_id = $_GET['id'];
 	$mode ="modifier";
 	$action="modif_manip.php";
 
@@ -26,7 +24,7 @@ else{
 
 require("html_functions.php");
 
-if ( $connex = connect_db() ){
+if ( $pdo = connect_db() ){
 
 if ($mode=="ajouter"){
 	en_tete("Voila un formulaire pour inscrire une nouvelle manip");
@@ -35,10 +33,13 @@ if ($mode=="ajouter"){
 else if ($mode=="modifier"){
 	en_tete("Voila un formulaire pour modifier les caracteristiques d'une manip");
 
-	// recupere la manip selectionnée
-	$querry = "SELECT * FROM manip WHERE id='$manip_id'";
-	list($qh,$num) = query_db($querry);
-	$data = result_db($qh);
+	// recupere la manip selectionnï¿½e
+	$sql = 'SELECT * FROM manip WHERE id = ?;';
+	// list($qh,$num) = query_db($querry);
+	// $data = result_db($qh);
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute(array($manip_id));
+	$manip = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 	}
 }//end if connex
@@ -57,13 +58,13 @@ else if ($mode=="modifier"){
       <td style="vertical-align: top;">Nom *<br />
       </td>
       <td style="vertical-align: top;">
-	<input type="text" name="nom" size="20" maxlength="20" value="<?php echo $data['nom'] ?>" ><br />
+	<input type="text" name="nom" size="20" maxlength="20" value="<?php if($mode=='modifier'){ echo $manip[0]['nom']; } ?>" ><br />
       </td>
     </tr><tr>
       <td style="vertical-align: top;">Description<br />
       </td>
       <td style="vertical-align: top;">
-	<textarea name="descr" cols="50" rows="5"> <?php echo $data['descr'] ?>
+	<textarea name="descr" cols="50" rows="5"> <?php if($mode=='modifier'){ echo $manip[0]['descr']; } ?>
 	</textarea>
       </td>
     </tr>  
@@ -71,25 +72,30 @@ else if ($mode=="modifier"){
       <td style="vertical-align: top;">Local *<br />
       </td>
       <td style="vertical-align: top;">
-	<input type="text" name="local" size="5" maxlength="10" value="<?php echo $data['local'] ?>" ><br />
+	<input type="text" name="local" size="5" maxlength="10" value="<?php if($mode=='modifier'){ echo $manip[0]['local'];} ?>" ><br />
       </td>
     </tr>
     <tr>
 	<?php // recupere la liste des equipes
-		$querry = "SELECT id,nom FROM equipe order by nom";
-		list($qheq,$numeq) = query_db($querry);
+		$sql = 'SELECT id,nom FROM equipe order by nom;';
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+		$equipe = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		// list($qheq,$numeq) = query_db($querry);
 		?>
       <td style="vertical-align: top;">Equipe <i> qui utilisera la manip</i><br />
       </td>
       <td style="vertical-align: top;">
 	<select name="equipe"   >
 		<?php 	
-		while(	$equipes = result_db($qheq)	){
-
+		// while(	$equipes = result_db($qheq)	){
+			foreach($equipe as $equipes){
 			echo "<option value=\"".$equipes['id']."\"";
-			if ($equipes['id'] == $data['equipe']){
-				echo " selected";
-				}
+			if($mode=='modifier'){
+				if ($equipes['id'] == $manip[0]['equipe']){
+					echo " selected";
+					}
+			}
 			echo ">".$equipes['nom']."</option>";
 		}//end while	?>
 	</select>
@@ -97,40 +103,50 @@ else if ($mode=="modifier"){
     </tr>
     <tr>
 	<?php // recupere la liste des chercheurs
-		$querry = "SELECT id,nom FROM users WHERE level=1 AND valid=1 order by nom";
-		list($qheq,$numeq) = query_db($querry);
+		$sql = 'SELECT id,nom FROM users WHERE level=1 AND valid=1 order by nom;';
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+		$equipe = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		// list($qheq,$numeq) = query_db($querry);
 		?>
       <td style="vertical-align: top;">Chercheur <i>( qui commande la manip)</i><br />
       </td>
       <td style="vertical-align: top;">
 	<select name="cherch"   >
 		<?php 	
-		while(	$equipes = result_db($qheq)	){
-
+		// while(	$equipes = result_db($qheq)	){
+			foreach($equipe as $equipes){
 			echo "<option value=\"".$equipes['id']."\"";
-			if ($equipes['id'] == $data['chercheur']){
-				echo " selected";
+			if($mode=='modifier'){
+				if ($equipes['id'] == $manip[0]['chercheur']){
+					echo " selected";
 				}
+			}
 			echo ">".$equipes['nom']."</option>";
 		}//end while	?>
 	</select>
       </td>
     </tr><tr>
 	<?php // recupere la liste des chercheurs
-		$querry = "SELECT id,nom FROM users WHERE level<=2 AND valid=1 order by nom ";
-		list($qheq,$numeq) = query_db($querry);
+		$sql = 'SELECT id,nom FROM users WHERE level<=2 AND valid=1 order by nom;';
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+		$equipe = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		// list($qheq,$numeq) = query_db($querry);
 		?>
 	<td style="vertical-align: top;">Chercheur/Etudiant <i>( qui utilisera la manip)</i><br />
       </td>
       <td style="vertical-align: top;">
 	<select name="cherch_bis"   >
 		<?php 	
-		while(	$equipes = result_db($qheq)	){
-
+		// while(	$equipes = result_db($qheq)	){
+			foreach($equipe as $equipes){
 			echo "<option value=\"".$equipes['id']."\"";
-			if ($equipes['id'] == $data['chercheur_bis']){
-				echo " selected";
+			if($mode=='modifier'){
+				if ($equipes['id'] == $manip[0]['chercheur_bis']){
+					echo " selected";
 				}
+			}
 			echo ">".$equipes['nom']."</option>";
 		}//end while	?>
 	</select>
@@ -144,7 +160,7 @@ else if ($mode=="modifier"){
       <th style="vertical-align: top;">
 	<input type="text" name="date" size="10" maxlength="10" value="<?php 
 				if ($mode=="modifier")
-					echo $data['date'];
+					echo $manip[0]['date'];
 				else  //ajout->aujourd'hui
 					echo date('Y-m-d', time() ); 
 						?> " ><br />
