2aa1cbfbc77a3fde04e93e35cc321058ecec331d
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-01 11:05:33 +0200

    mise en place de pdo

--- valid_app.php
+++ valid_app.php
@@ -7,35 +7,48 @@ require("html_functions.php");
 unset($erreur);
 //variables ne pouvant etre nulles
 
-if (empty($_POST[nom]))
+if (empty($_POST['nom']))
 	$erreur="nom non pr&eacute;cis&eacute;";
 else{
-	$nom =$_POST[nom];
-	if (empty($_POST[descr]))
+	$nom =$_POST['nom'];
+	if (empty($_POST['descr']))
 		$erreur="Description non pr&eacute;cis&eacute;";
 	else{
-		$descr=$_POST[descr];
-		if (empty($_POST[equipe]))
+		$descr=$_POST['descr'];
+		if (empty($_POST['equipe']))
 			$erreur="equipe non pr&eacute;cis&eacute;";
 		else{
-			$equipe =$_POST[equipe];
-			if (empty($_POST[tech]))
+			$equipe =$_POST['equipe'];
+			if (empty($_POST['tech']))
 				$erreur="tech non pr&eacute;cis&eacute;";
 			else{
-				$tech =$_POST[tech];
-				if (empty($_POST[fourn]))
+				$tech =$_POST['tech'];
+				if (empty($_POST['fourn']))
 					$erreur="fournisseur non pr&eacute;cis&eacute;";
 				else{
-					$fourn =$_POST[fourn];
+					$fourn =$_POST['fourn'];
 
 							//variables pouvant etre nulles
-					$achat = $_POST[achat];
-					$facture=$_POST[facture];
+					if(empty($_POST['achat'])){
+						$achat='';
+					}else{
+						$achat = $_POST['achat'];
 
-}}}}}
+						if(empty($_POST['facture'])){
+							$facture = '';
+						}else{
+							$facture=$_POST['facture'];
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
 
 en_tete("resultat ajout appareil ");
-
+nav_bar();
 if (!empty($erreur) ){
 
 	//erreur
@@ -52,26 +65,25 @@ else{
 ///on inscrit
 require("db_functions.php");
 
-if ( $connex = connect_db() ){
+if ( $pdo = connect_db() ){
 		//inscription
-	$table = "appareils";
-		$result = mysql_query("INSERT INTO $table ".
-			"(nom,descr, equipe, tech, fournisseur, achat, facture)".
-			" VALUES ('$nom', '$descr', '$equipe', '$tech', '$fourn', '$achat', '$facture')");
+	// $table = "appareils";
+	// 	$result = mysql_query("INSERT INTO $table ".
+	// 		"(nom,descr, equipe, tech, fournisseur, achat, facture)".
+	// 		" VALUES ('$nom', '$descr', '$equipe', '$tech', '$fourn', '$achat', '$facture')");
 			//
- 		if (!$result){
-			//inscription !ok
-			$erreur = mysql_error();
-		echo "<br />erreur :".$erreur;
-		}
-
+	$sql = 'INSERT INTO appareils (nom,descr,equipe,tech, fournisseur, achat, facture) VALUES ( ?, ?, ?, ?, ?, ?, ?);';
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute(array($nom, $descr, $equipe, $tech, $fourn, $achat, $facture));
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	echo "<br /> Votre requête a bien été ajouté";
 	}//end if connect
+
+
 
 ////en_tete("inscription Valid&eacute;e");
 
-echo "ajout de ".$nom."<br />";
-echo" <img src=\"images/pool_project.jpg\" nosave=\"\" height=\"100\" align=\"middle\" alt=\"\">";
-echo" est valid&eacute;e ";
+echo "ajout de ".$nom."valid&eacute;e ";
 echo"<br /><br /><a href=\"list_app.php\">Suite</a><br /><br />\n";
 pied_page();
 exit();
