ae35e94a97ecb54a008b49eec15bfcb5eea6aac8
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-02 12:11:29 +0200

    redirection vers la page index.php aprÃs la dÃ©connectio

--- logout.php
+++ logout.php
@@ -3,16 +3,16 @@
 
 include("session_auth.php");
 
-$truc2=$_GET[variable];
+// $truc2=$_GET['variable'];
 logout();
 
 	//go to the login page
-if ($truc2=="instru")
-	Header("Location: login.php?variable=instru");
+// if ($truc2=="instru")
+// 	Header("Location: login.php?variable=instru");
 
-if ($truc2=="projet")
-	Header("Location: login.php?variable=projet");
+// if ($truc2=="projet")
+// 	Header("Location: login.php?variable=projet");
 //if ($truc2=="pret")
 	//Header("Location: login.php?variable=pret");
-
+	Header("Location: ./");
 ?>
