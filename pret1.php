3955ac756648ea1e1ac387646414ed4407bd4a2e
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-02 19:12:54 +0200

    Remove all LEGI

--- pret1.php
+++ pret1.php
@@ -8,9 +8,9 @@ include("session_auth.php");
 if (!auth(1))
 	Header("Location: login.php");
 
-$user_id = $_SESSION['user_id'];
+$user_id        = $_SESSION['user_id'];
 $logged_in_user = strtolower($_SESSION['logged_in_user']);
-$user_level= $_SESSION['level'];
+$user_level     = $_SESSION['level'];
 
 require("html_functions.php");
 
@@ -22,23 +22,22 @@ else
 	$tri = $_GET['tri'];
 
 echo "Tu es connect&eacute; en tant que : ".$logged_in_user." (".$user_id.")<br />";
-
 ?>
 
 <i> Consulter la liste des &eacute;quipement communs disponibles au service instrumentation et choisir : 'Demande de pr&ecirc;t' en face de l'appareil souhait&eacute;</i><br />
 <br />
 <table cellpadding="2" cellspacing="2" border="1"
- style="width: 70%; text-align: left; margin-left: auto; margin-right: auto;">
-  <tbody>
-    <tr>
-	 <td style="vertical-align: top; text-align: center;">
-	<a href="http://intranet.legi.grenoble-inp.fr">Retour &agrave;<br />l'intranet</a>
-	<br /></td>
-<?php
+	style="width: 70%; text-align: left; margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<td style="vertical-align: top; text-align: center;">
+				<a href="<?php GESTEX_URL_ENTITY ?>">Retour &agrave;<br />l'intranet</a>
+				<br />
+			</td>
 
+<?php
 	// recupere les refs du user
 	$pdo = connect_db();
-
 	$sql = 'SELECT id from equipe ORDER by ?';
 	$stmt = $pdo->prepare($sql);
 	$stmt->execute(array($tri));
@@ -49,25 +48,20 @@ echo "Tu es connect&eacute; en tant que : ".$logged_in_user." (".$user_id.")<br 
 // $data = result_db($qh);
 
 // while ($data = result_db($qh)){
-	foreach($equipe as $data){
-if ($data['id'] == 15)
-
-  {     
-	  echo"<td style=\"vertical-align: top;\">";
-
-	echo "<a href =\"instru.php?equipe=".$data['id']." pret=".$data['id']."\">Liste des appareils en pr&ecirc;t</a>";
-echo "</td>";
-
-  echo"<td style=\"vertical-align: top;\">";
-	echo "<a href =\"reserva.php?user=".$user_level." \">Liste des r&eacute;servations</a>";
-echo"<br /></td>";
+foreach($equipe as $data){
+	if ($data['id'] == 15) {     
+		echo"<td style=\"vertical-align: top;\">";
+		echo "	<a href =\"instru.php?equipe=".$data['id']." pret=".$data['id']."\">Liste des appareils en pr&ecirc;t</a>";
+		echo "</td>";
+		echo"<td style=\"vertical-align: top;\">";
+		echo "	<a href =\"reserva.php?user=".$user_level." \">Liste des r&eacute;servations</a>";
+		echo "	<br />";
+		echo "</td>";
+	}
 }
-
-}
-
 ?>
-
-</tr></tbody>
+		</tr>
+	</tbody>
 </table>
 
 <?php pied_page() ?>
