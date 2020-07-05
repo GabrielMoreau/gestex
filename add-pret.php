0997bbab5ecdb73abcf0daf43332786bef7f5cde
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-05 16:31:38 +0200

    Just format

--- add-pret.php
+++ add-pret.php
@@ -1,50 +1,45 @@
 <?php
 /// add_pret.php
-	// Authenticate
-	include("session_auth.php");
+// Authenticate
+include("session_auth.php");
 
-	//if (!auth(3))
-		//Header("Location: login.php");
-
-	//$user_id = $_SESSION['user_id'];
-	//$logged_in_user = strtolower($_SESSION['logged_in_user']);
+//if (!auth(3))
+	//Header("Location: login.php");
+//$user_id = $_SESSION['user_id'];
+//$logged_in_user = strtolower($_SESSION['logged_in_user']);
 
 //recuper la methode de tri
 if (empty($_GET['tri']))
-	$tri ="id";
+	$tri = 'id';
 else
 	$tri = $_GET['tri'];
 
-
-if (empty($_GET['id'])){
-	$pret ="";
+if (empty($_GET['id'])) {
+	$pret = '';
 	//->nouvel appareil
-}
-else{
-	$pret=$_GET['id'];
+} else {
+	$pret = $_GET['id'];
 }
 
-if(empty($_GET['pret'])){
-	$mode ="ajouter";
-	$action="valid-pret.php";
-	$id_pret ="";
-}else{
-	$mode ="modifier";
-	$action="modiff_pret.php";
+if (empty($_GET['pret'])) {
+	$mode    = 'ajouter';
+	$action  = 'valid-pret.php';
+	$id_pret = '';
+} else {
+	$mode    = 'modifier';
+	$action  = 'modiff_pret.php';
 	$id_pret = $_GET['pret'];
 }
 
-	
 //transmet la valeur de la categorie a la page valid appareil
 
 require("html_functions.php");
-if ( $pdo = connect_db() ){
-if ($mode=="ajouter"){
-	en_tete("Voila un formulaire pour ajouter un pr&ecirc;t");
-
-}
-else if ($mode=="modifier"){
-	en_tete("Voila un formulaire pour modifier les pr&ecirc;ts d'un appareil");
+if ($pdo = connect_db()) {
+	if ($mode == 'ajouter') {
+		en_tete("Voila un formulaire pour ajouter un pr&ecirc;t");
+	}
+	else if ($mode == 'modifier') {
+		en_tete("Voila un formulaire pour modifier les pr&ecirc;ts d'un appareil");
 
 	// recupere l'appareil selectionne
 	// $sql = 'SELECT * FROM pret WHERE id = ?;';
@@ -53,129 +48,117 @@ else if ($mode=="modifier"){
 	// $stmt = $pdo->prepare($sql);
 	// $stmt->execute(array($pret));
 	// $pret = $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
+	}
 ?>
 
 <table cellpadding="2" cellspacing="2" border="1" style="text-align: left; width: 75%;" align="center">
+	<tbody>
+		<form action="<?php echo $action ?>" method="POST" name="inscrForm">
+			<input type="hidden" name="id_app" value="<?php echo $app_id ?>" >
+		<tr>
+			<td style="vertical-align: top;">Nom<br />
+			</td>
+			<td style="vertical-align: top;">
+				<select name="nom">
+				<!-- listing des appareils -->
+				<?php
+				$sql = 'SELECT id, nom FROM listing where equipe = 15 and id= ?; ';
+				// list($qheq,$numeq) = query_db($querry);
+				// 	while ($chef = result_db($qheq)){
+				if ($mode == 'ajouter') {
+					$stmt = $pdo->prepare($sql);
+					$stmt->execute(array($pret));
+					$chef = $stmt->fetchAll(PDO::FETCH_ASSOC);
+					echo '<option value="'.$chef[0]['id'].'">'.$chef[0]['nom'].'</option>';
+				}
+				?>
+				</select>
+				<br />
+			</td>
+		</tr>
 
-  <tbody>
-<form action="<?php echo $action ?>" method="POST" name="inscrForm">
-		<input type="hidden" name="id_app" value="<?php echo $app_id ?>" >
+		<tr>
+			<td style="vertical-align: top;">&Eacute;quipe *<br />
+			</td>
+			<td style="vertical-align: top;">
+				<select name="equipe">
+				<?php
+				// recupere la liste des equipes
+				$sql = 'SELECT id, nom FROM equipe order by nom';
+				// list($qheq,$numeq) = query_db($querry);
+				$stmt = $pdo->prepare($sql);
+				$stmt->execute(array($courseId,$courseId));
+				$equipe = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				// 	while ($chef = result_db($qheq)){
+				foreach($equipe as $chef){
+					echo '<option value="'.$chef['id'].'"';
+					if ($mode == 'modifier' && $chef['id'] == $pret[0]['equipe']) {
+						echo ' selected';
+					}
+					echo '>'.$chef['nom'].'</option>';
+				} //end foreach
+				?>
+				</select>
+				<br />
+			</td>
+		</tr>
 
-  <tr>
-      <td style="vertical-align: top;">Nom<br />
-      </td>
+		<tr>
+			<td style="vertical-align: top;">Date demande pr&ecirc;t * (<i>format YYYY-MM-DD</i>)<br />
+			</td>
+			<td style="vertical-align: top;">
+				<input type="text" name="emprunt" size="10" maxlength="10" value="<?php
+					if ($mode == 'modifier')
+						echo $pret[0]['emprunt'];
+					else
+						echo date('Y-m-d', time() );
+					?>" ><br />
+			</td>
+		</tr>
 
-  <td style="vertical-align: top;">
+		<tr>
+			<td style="vertical-align: top;">Date de retour estim&eacute;e * (<i>format YYYY-MM-DD</i>)<br />
+			</td>
+			<td style="vertical-align: top;">
+				<input type="text" name="retour" size="10" maxlength="10" value="<?php
+					if ($mode == 'modifier')
+						echo $pret[0]['retour'];
+					else
+						echo date('Y-m-d', time() );
+					?>" ><br />
+			</td>
+		</tr>
 
-<select name="nom">
-<!-- listing des appareils -->
-<?php
-	$sql = 'SELECT id, nom FROM listing where equipe = 15 and id= ?; ';
-	// list($qheq,$numeq) = query_db($querry);
-	// 	while ($chef = result_db($qheq)){
-			if ($mode=="ajouter" ){
-		$stmt = $pdo->prepare($sql);
-        $stmt->execute(array($pret));
-        $chef = $stmt->fetchAll(PDO::FETCH_ASSOC);
-			echo "<option value=\"".$chef[0]['id']."\"";
+		<tr>
+			<td style="vertical-align: top;">Commentaire<br />
+			</td>
+			<td style="vertical-align: top;">
+				<input type="text" name="commentaire" size="30" maxlength="30" value="<?php if($mode=='modifier'){echo $pret[0]['commentaire'];} ?>" ><br />
+			</td>
+		</tr>
 
-
-			echo ">".$chef[0]['nom']."</option>";
-		}
-	?>
-</select>
-<br />
-      </td>
-    </tr>
-
-    <tr>
-      <td style="vertical-align: top;">&Eacute;quipe *<br />
-      </td>
-      <td style="vertical-align: top;">
-	<select name="equipe">
-	<?php
-	// recupere la liste des equipes
-	$sql = 'SELECT id, nom FROM equipe order by nom';
-	// list($qheq,$numeq) = query_db($querry);
-
-	$stmt = $pdo->prepare($sql);
-	$stmt->execute(array($courseId,$courseId));
-	$equipe = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	// 	while ($chef = result_db($qheq)){
-		foreach($equipe as $chef){
-			echo "<option value=\"".$chef['id']."\"";
-			if ($mode=="modifier" && $chef['id'] == $pret[0]['equipe']) {
-				echo " selected";	}
-			echo ">".$chef['nom']."</option>";
-		}//end while
-		 ?>
-	</select><br />
-      </td>
-    </tr>
-
-  <tr>
-
-      <td style="vertical-align: top;">Date demande pr&ecirc;t * (<i>format YYYY-MM-DD</i>)<br />
-      </td>
-      <th style="vertical-align: top;">
-	<input type="text" name="emprunt" size="10" maxlength="10" value="<?php
- if ($mode =="modifier")
-			echo $pret[0]['emprunt'];
-		else
-			echo date('Y-m-d', time() );
-	?>" ><br />
-      </td>
-    </tr>
-
-  <tr>
-
-      <td style="vertical-align: top;">Date de retour estim&eacute;e *<i>format YYYY-MM-DD</i><br />
- </td>
-      <td style="vertical-align: top;">
-<input type="text" name="retour" size="10" maxlength="10" value="<?php
- if ($mode =="modifier")
-			echo $pret[0]['retour'];
-		else
-			echo date('Y-m-d', time() );
-	?>" ><br />
-
-      </td>
-
-    </tr>
-<tr>
- <td style="vertical-align: top;">Commentaire<br />
-      </td>
-<td style="vertical-align: top;">
-<input type="text" name="commentaire" size="30" maxlength="30" value="<?php if($mode=='modifier'){echo $pret[0]['commentaire'];} ?>" ><br />
-
-      </td>
-
-    </tr>
-
-    <tr>
-   <td style="vertical-align: top;">Les champs avec * sont &agrave;
+		<tr>
+			<td style="vertical-align: top;">Les champs avec * sont &agrave;
 remplir obligatoirement, les autres sont optionnels.<br />
-      </td>
-      <td style="vertical-align: top;" align="right">
-<input type="submit" name="Login" value="<?php echo $mode ?>">
-      </td>
-    </tr></form>
-  </tbody>
- <tbody>
+			</td>
+			<td style="vertical-align: top;" align="right">
+				<input type="submit" name="Login" value="<?php echo $mode ?>">
+			</td>
+		</tr>
+		</form>
 
-	<form action="essai1.php"method="POST" name="annulForm">
- 	<tr >   <td colspan="2" style="vertical-align: top; text-align: right;">
-	<input type="submit" name="annul" value="Annuler">
-	 </td>    </tr>
-	</form>
-</tbody>
+		<form action="essai1.php"method="POST" name="annulForm">
+		<tr>
+			<td colspan="2" style="vertical-align: top; text-align: right;">
+				<input type="submit" name="annul" value="Annuler">
+			</td>
+		</tr>
+		</form>
+	</tbody>
 </table>
 <br />
-	<?php }else{
-		echo "probl&egrave;me de connexion a la badd";
-	}
-		?>
+
+<?php } else { echo "probl&egrave;me de connexion a la base de donn&eacute;e"; } ?>
 <br />
 </div>
 <?php pied_page() ?>
