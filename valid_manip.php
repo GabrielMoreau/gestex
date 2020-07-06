a8c43e926a14dddc85e7e3403ae2de86b9ec668e
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-06 14:15:33 +0200

    mise ne place de pdo

--- valid_manip.php
+++ valid_manip.php
@@ -8,21 +8,21 @@ require("html_functions.php");
 unset($erreur); unset($nom);
 //variables ne pouvant etre nulles
 
-    if (empty($_POST[nom]))
+    if (empty($_POST['nom']))
      $erreur="nom non pr&eacute;cis&eacute;";
     else{
-     $nom =$_POST[nom];
-     if (empty($_POST[local]))
+     $nom =$_POST['nom'];
+     if (empty($_POST['local']))
       $erreur="Local non pr&eacute;cis&eacute;";
      else{
-      $local = $_POST[local];
+      $local = $_POST['local'];
 
-       $descr=$_POST[descr];
+       $descr=$_POST['descr'];
        //variables pouvant etre nulles
-       $equipe =$_POST[equipe];
-       $chercheur =$_POST[cherch];
-       $chercheur_bis =$_POST[cherch_bis];
-       $date =$_POST[date];
+       $equipe =$_POST['equipe'];
+       $chercheur =$_POST['cherch'];
+       $chercheur_bis =$_POST['cherch_bis'];
+       $date =$_POST['date'];
 
 }}
 
@@ -44,35 +44,44 @@ else{
 ///on inscrit
 require("db_functions.php");
 
-if ( $connex = connect_db() ){
+if ( $pdo = connect_db() ){
   //ajout de la manip
- $querry = "INSERT INTO manip (nom,descr,equipe,chercheur, chercheur_bis,local,date)".
-   " VALUES ('$nom',  '$descr', '$equipe', '$chercheur', '$chercheur_bis', '$local', '$date')";
-  $result = mysql_query($querry);
+ $sql = "INSERT INTO manip (nom,descr,equipe,chercheur, chercheur_bis,local,date) VALUES (?, ?, ?, ?, ?, ?, ?);";
+  // $result = mysql_query($querry);
    //
+   $stmt = $pdo->prepare($sql);
+	$stmt->execute(array($nom, $descr, $equipe, $chercheur, $chercheur_bis, $local, $date));
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
   if ($result){
    $manip_id = last_id_db($result);
    $nom = "Projet par defaut"; $descr="";
    //insere un projet par defaut
-   $querry = "INSERT INTO projet (manip, nom, descr, date)".
-    " VALUES ('$manip_id', '$nom',  '$descr', '$date')";
-   $result = mysql_query($querry);
+   $sql = "INSERT INTO projet (manip, nom, descr, date) VALUES (?, ?,  ?, ?)";
+  //  $result = mysql_query($querry);
+  $stmt = $pdo->prepare($sql);
+	$stmt->execute(array($manip_id, $nom, $descr, $date));
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
    if ($result){
     $proj_id = last_id_db($result);
     //insere un tache par defaut
     $nom = "T&acirc;che par defaut"; $descr="";
-    $querry = "INSERT INTO tache (projet, nom, descr, date, fourniss)".
-     " VALUES ('$proj_id', '$nom',  '$descr', '$date', 0)";
-    $result = mysql_query($querry);
+    $sql = "INSERT INTO tache (projet, nom, descr, date, fourniss) VALUES (?, ?, ?, ?, 0)";
+    // $result = mysql_query($querry);
+  $stmt = $pdo->prepare($sql);
+	$stmt->execute(array($proj_id, $nom, $descr, $date));
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    
     }
    }
-   if (!$result){
-   //inscription !ok
-   $erreur = mysql_error();
-  echo "<br />erreur :".$erreur;
-  echo"<br /><br /><a href=\"add_manip.php\">Suite</a><br /><br />\n";
-  }
-  else{ //result=ok
+  //  if (!$result){
+  //  //inscription !ok
+  // //  $erreur = mysql_error();
+  // echo "<br />erreur :".$erreur;
+  // echo"<br /><br /><a href=\"add_manip.php\">Suite</a><br /><br />\n";
+  // }
+  // else{ //result=ok
 //ajout d'un repertoire dans data
 //remplace les espaces par des underscore
  $dossier_proj = str_replace(" ", "_", $nom);
@@ -82,7 +91,7 @@ echo "inscription de la manip ".$nom."<br />";
 echo" <img src=\"images/pool_project.jpg\" height=\"100\" nosave=\"\" align=\"middle\" alt=\"\">";
 echo" est valid&eacute;e ";
 echo"<br /><br /><a href=\"accueil.php\">Suite</a><br /><br />\n";
-  }
+  // }
  }//end if connect
 
 pied_page();
