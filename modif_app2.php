53fbb7b9380e1bd4729c461249d4794ea9be272c
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-06 16:57:28 +0200

    mise ne place de pdo

--- modif_app2.php
+++ modif_app2.php
@@ -15,55 +15,55 @@ require ("db_functions.php");
 //modification d'un appareil
 unset($erreur);
 //variables ne pouvant etre nulles
-if (empty($_POST[id_app]))
+if (empty($_POST['id_app']))
 	$erreur="id non pr&eacute;cis&eacute;";
 else{
-	$id_app =$_POST[id_app];
+	$id_app =$_POST['id_app'];
 
-if (empty($_POST[categorie]))
+if (empty($_POST['categorie']))
 	$erreur="categorie non pr&eacute;cis&eacute;";
 else{
-	$categorie =$_POST[categorie];
+	$categorie =$_POST['categorie'];
 
-if (empty($_POST[nom]))
+if (empty($_POST['nom']))
 	$erreur="nom non pr&eacute;cis&eacute;";
 else{
-	$nom =$_POST[nom];
-	if (empty($_POST[modele]))
+	$nom =$_POST['nom'];
+	if (empty($_POST['modele']))
 		$erreur="Modele non pr&eacute;cis&eacute;";
 	else{
-		$modele=$_POST[modele];
-$gamme=$_POST[gamme];
+		$modele=$_POST['modele'];
+$gamme=$_POST['gamme'];
 
-		if (empty($_POST[equipe]))
+		if (empty($_POST['equipe']))
 			$erreur="equipe non pr&eacute;cis&eacute;";
 		else{
-			$equipe =$_POST[equipe];
-			if (empty($_POST[tech]))
+			$equipe =$_POST['equipe'];
+			if (empty($_POST['tech']))
 				$erreur="tech non pr&eacute;cis&eacute;";
 			else{
-				$tech =$_POST[tech];
-				if (empty($_POST[fourn]))
+				$tech =$_POST['tech'];
+				if (empty($_POST['fourn']))
 					$erreur="fournisseur non pr&eacute;cis&eacute;";
 				else{
-					$fourn =$_POST[fourn];
+					$fourn =$_POST['fourn'];
 
 							//variables pouvant etre nulles
-					$achat = $_POST[achat];
-					$reparation=$_POST[reparation];
-	$accessoires=$_POST[accessoires];
-$inventaire=$_POST[inventaire];
-$notice=$_POST[notice];
+					$achat = $_POST['achat'];
+					$reparation=$_POST['reparation'];
+	$accessoires=$_POST['accessoires'];
+$inventaire=$_POST['inventaire'];
+$notice=$_POST['notice'];
 
 }}}}}}}
 
 en_tete('R&eacute;sultat modification appareil');
 
-$tri = $_GET[tri];
+$tri = $_GET['tri'];
 if (empty($tri))
 	$tri ="id";
 
-$cat=$_GET[categorie];
+$cat=$_GET['categorie'];
 echo "$cat";
 //recupere la categorie de la page ajout appareil
 
@@ -82,14 +82,16 @@ else{
 //pas d'erreur
 ///on inscrit
 
-if ( $connex = connect_db() ){
+if ( $pdo = connect_db() ){
 
 	//recupere les anciennes caracteristiques
 
-	$querry="SELECT * FROM Listing WHERE id='$id_app'";
-	list($qh,$num) = query_db($querry);
-	$data = result_db($qh);
-
+	$sql = 'SELECT * FROM Listing WHERE id = ?';
+	// list($qh,$num) = query_db($querry);
+	// $data = result_db($qh);
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute(array($id_app));
+	$listing = $stmt->fetchAll(PDO::FETCH_ASSOC);
 /*
 echo $nom." ".$data['nom']."<br />";
 echo $modele." ".$data['modele']."<br />";
@@ -101,66 +103,66 @@ $modif=0;
 //on construit la demande
 	$querry = "UPDATE LOW_PRIORITY Listing SET ";
 
-if ($categorie!=$data['categorie']){
+if ($categorie!=$listing[0]['categorie']){
 			//modif de la categorie
 			$modif=1;
 			$querry.="categorie='$categorie',";
 	}
 
-		if ($nom!=$data['nom']){
+		if ($nom!=$listing[0]['nom']){
 			//modif du nom
 			$modif=1;
 			$querry.="nom='$nom',";
 		}
-		if ($modele!=$data['modele']){
+		if ($modele!=$listing[0]['modele']){
 			//modif du modele
 			$modif=1;
 			$querry.="modele='$modele',";
 		}
 
-if ($gamme!=$data['gamme']){
+if ($gamme!=$listing[0]['gamme']){
 			//modif de la gamme
 			$modif=1;
 			$querry.="gamme='$gamme',";
 		}
 
-		if ($tech!=$data['tech']){
+		if ($tech!=$listing[0]['tech']){
 			//modif du tech
 			$modif=1;
 			$querry.="responsable='$tech',";
 		}
-		if ($equipe!=$data['equipe']){
+		if ($equipe!=$listing[0]['equipe']){
 			//modif de l'equipe
 			$modif=1;
 			$querry.="equipe='$equipe',";
 		}
-		if ($fourn!=$data['fourn']){
+		if ($fourn!=$listing[0]['fourn']){
 			//modif du fourn
 			$modif=1;
 			$querry.="fournisseur='$fourn',";
 		}
-		if ($achat!=$data['achat']){
+		if ($achat!=$listing[0]['achat']){
 			//modif de l'achat
 			$modif=1;
 			$querry.="achat='$achat',";
 		}
-		if ($reparation!=$data['reparation']){
+		if ($reparation!=$listing[0]['reparation']){
 			//modif de reparation
 			$modif=1;
 			$querry.="reparation='$reparation',";
 		}
-if ($accessoires!=$data['accessoires']){
+if ($accessoires!=$listing[0]['accessoires']){
 			//modif de accessoires
 			$modif=1;
 			$querry.="accessoires='$accessoires',";
 		}
-if ($inventaire!=$data['inventaire']){
+if ($inventaire!=$listing[0]['inventaire']){
 			//modif de inventaire
 			$modif=1;
 			$querry.="inventaire='$inventaire',";
 		}
 
-if ($notice!=$data['notice']){
+if ($notice!=$listing[0]['notice']){
 			//modif de notice
 			$modif=1;
 			$querry.="notice='$notice',";
@@ -172,12 +174,16 @@ if ($notice!=$data['notice']){
 		$querry.=" WHERE id='$id_app'";
 	if ($modif!=0){
 			//echo $querry;
-		$result = mysql_query($querry);
+		// $result = mysql_query($querry);
 			//
+			$stmt = $pdo->prepare($querry);
+			$stmt->execute();
+			$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
  		if (!$result){
 			//inscription !ok
-			$erreur = mysql_error();
-			echo "<br />erreur :".$erreur;
+			// $erreur = mysql_error();
+			echo "<br />erreur ";
 		}
 	}//end if modif
 	else{
@@ -193,15 +199,19 @@ if ($notice!=$data['notice']){
 if ( $connex = connect_db() ){
 	// recupere la liste de appareils
 
-$querry = "SELECT * FROM categorie where id='$cat'" ;
-	list($qh,$num) = query_db($querry);
-	$last_id=0;
-$datax = result_db($qh);}
+$sql = 'SELECT * FROM categorie where id = ?;' ;
+// 	list($qh,$num) = query_db($querry);
+// 	$last_id=0;
+// $datax = result_db($qh);
+$stmt = $pdo->prepare($sql);
+$stmt->execute(array($cat));
+$categorie = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
 echo "$cat";
 echo "<br />modification de ".$nom."<br />";
 echo" est valid&eacute;e ";
-echo"<br /><br /><a href=\"instru.php?categorie=".$datax[id]."\">Suite</a><br /><br />\n";
+echo"<br /><br /><a href=\"instru.php?categorie=".$categorie[0]['id']."\">Suite</a><br /><br />\n";
 //quand on va sur suite, on retourne sur la page de la categorie choisie
 
 pied_page();
