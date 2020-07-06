4db6c09c7868047ab716f2ccb4aee9ff16e34a5d
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-06 10:22:34 +0200

    ajout de condition

--- add_user.php
+++ add_user.php
@@ -11,15 +11,16 @@ $user_id        = $_SESSION['user_id'];
 $logged_in_user = strtolower($_SESSION['logged_in_user']);
 $user_level     = $_SESSION['level'];
 
-$user2ch_id = $_GET['id'];
-if (empty($user2ch_id)) {
+if (empty($_GET['id'])) {
 	//->nouvelle inscription
 	$mode   = 'ajouter';
 	$action = 'valid_inscr.php';
+	$user2ch_id = '';
 } else {
 	//->modif coordonnees
-	$mode   = 'modif';
+	$mode   = 'modifier';
 	$action = 'modif_inscr.php';
+	$user2ch_id = $_GET['id'];
 }
 
 require("html_functions.php");
@@ -27,7 +28,7 @@ require("html_functions.php");
 if ($pdo = connect_db()) {
 	if ($mode == 'ajouter'){
 		en_tete('Inscrire un utilisateur');
-	} else if ($mode=="modif") {
+	} else if ($mode=="modifier") {
 		en_tete('Modifier vos coordonn&eacute;es');
 		// recupere la liste des users
 		// $querry = "SELECT * FROM users WHERE id='$user2ch_id'";
@@ -39,6 +40,7 @@ if ($pdo = connect_db()) {
 		$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		$data = $user[0];
 	}
+	
 
 	echo 'id:'.$user_id .' : '.$logged_in_user.' lvl:'.$user_level;
 ?>
@@ -65,14 +67,14 @@ if ($pdo = connect_db()) {
 			<td style="vertical-align: top;">Mot de passe *<br />
 			</td>
 			<td style="vertical-align: top;">
-				<input type="password" name="password" size="25" maxlength="25" value="<?php echo $data['password'] ?>" ><br />
+				<input type="password" name="password" size="25" maxlength="25" value="" ><br />
 			</td>
 		</tr>
 		<tr>
 			<td style="vertical-align: top;">Mot de passe (pour confirmer) *<br />
 			</td>
 			<td style="vertical-align: top;">
-				<input type="password" name="password2" size="25" maxlength="25" value="<?php echo $data['password'] ?>" ><br />
+				<input type="password" name="password2" size="25" maxlength="25" value="" ><br />
 			</td>
 		</tr>
 		<?php } //end if mode ?>
@@ -81,28 +83,28 @@ if ($pdo = connect_db()) {
 			<td style="vertical-align: top;">Nom de famille *<br />
 			</td>
 			<td style="vertical-align: top;">
-				<input type="text" name="nom" size="25" maxlength="25" value="<?php echo $data['nom'] ?>" ><br />
+				<input type="text" name="nom" size="25" maxlength="25" value="<?php if($mode=='modifier'){ echo $data['nom']; } ?>" ><br />
 			</td>
 		</tr>
 		<tr>
 			<td style="vertical-align: top;">Pr&eacute;nom<br />
 			</td>
 			<td style="vertical-align: top;">
-				<input type="text" name="prenom" size="25" maxlength="25" value="<?php echo $data['prenom'] ?>" ><br />
+				<input type="text" name="prenom" size="25" maxlength="25" value="<?php if($mode=='modifier'){ echo $data['prenom']; } ?>" ><br />
 			</td>
 		</tr>
 		<tr>
 			<td style="vertical-align: top;">Adresse courriel *<br />
 			</td>
 			<td style="vertical-align: top;">
-				<input type="text" name="addr_mail" size="25" maxlength="50" value="<?php echo $data['email'] ?>" ><br />
+				<input type="text" name="addr_mail" size="25" maxlength="50" value="<?php if($mode=='modifier'){ echo $data['email']; } ?>" ><br />
 			</td>
 		</tr>
 		<tr>
 			<td style="vertical-align: top;">T&eacute;l&eacute;phone<br />
 			</td>
 			<td style="vertical-align: top;">
-				<input type="text" name="phone" size="10" maxlength="10" value="<?php echo $data['tel'] ?>" ><br />
+				<input type="text" name="phone" size="10" maxlength="10" value="<?php if($mode=='modifier'){ echo $data['tel']; } ?>" ><br />
 			</td>
 		</tr>
 		<tr>
@@ -123,8 +125,10 @@ if ($pdo = connect_db()) {
 					foreach ($equipe_fetch as $equipe) {
 						echo '<option value="'.$equipe['id'].'"';
 						/// selectionne la bonne equipe
+						if($mode == 'modifier'){
 						if ($equipe['id'] == $data['equipe'])
 							echo ' selected';
+						}
 						echo '>'.$equipe['nom'].'</option>';
 					} //end foreach ?>
 				</select>
@@ -136,9 +140,9 @@ if ($pdo = connect_db()) {
 			</td>
 			<td style="vertical-align: top;">
 				<?php  if ($user_level >= 3 || !isset($user_level)) { // admin loggue ou premiere inscription: modif possible ?>
-				<input type="radio" name="level" value="0" <?php if ($data['level']==0) echo 'checked="checked"' ?> >&Eacute;tudiant<br />
-				<input type="radio" name="level" value="1" <?php if ($data['level']==1) echo 'checked="checked"' ?> >Chercheur<br />
-				<input type="radio" name="level" value="2" <?php if ($data['level']==2) echo 'checked="checked"' ?> >ITA<br />
+				<input type="radio" name="level" value="0" <?php if ($mode ==='modifier' && $data['level']==0) echo 'checked="checked"' ?> >&Eacute;tudiant<br />
+				<input type="radio" name="level" value="1" <?php if ($mode ==='modifier' && $data['level']==1) echo 'checked="checked"' ?> >Chercheur<br />
+				<input type="radio" name="level" value="2" <?php if ($mode ==='modifier' && $data['level']==2) echo 'checked="checked"' ?> >ITA<br />
 				<?php } ?>
 				
 				<?php if (isset($user_level) && $user_level==3) { ?>
