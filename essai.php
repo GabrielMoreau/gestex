073ad3855a64aed424da1b7962e9e49bb8605d7a
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-07 19:09:16 +0200

    Format code

--- essai.php
+++ essai.php
@@ -6,14 +6,13 @@ include("session_auth.php");
 session_start();
 // if (!auth(1))
 	// Header("Location: instru.php");
-if(empty($_SESSION['logged_in_user'])){
+if (empty($_SESSION['logged_in_user'])) {
 	$log = false;
-}else{
-	$user_id = $_SESSION['user_id'];
+} else {
+	$user_id        = $_SESSION['user_id'];
 	$logged_in_user = strtolower($_SESSION['logged_in_user']);
-	$user_level= $_SESSION['level'];
-	$log=true;
-
+	$user_level     = $_SESSION['level'];
+	$log = true;
 }
 require("html_functions.php");
 
@@ -22,118 +21,103 @@ en_tete('Liste des appareils');
 //recupere la methode de tri
 
 if (empty($_GET['tri']))
-	$tri ="id";
+	$tri = 'id';
 else
 	$tri = $_GET['tri'];
 ?>
 
 <br />
 <table cellpadding="2" cellspacing="2" border="1"
- style="width: 90%; text-align: left; margin-left: auto; margin-right: auto;">
-  <tbody>
-    <tr>
-	 <td style="vertical-align: top; text-align: center;">
-	<a href="<?php GESTEX_URL_SERVICE ?>">Retour &agrave;<br /> la page du service</a>
-	<br /></td>
-<?php 
-if($log ==true){
-	if ( $user_level >=2 ) {	?>
- <td style="vertical-align: top; text-align: center;">
-	<a href="add_app2.php">Ajout<br />d'un appareil</a>
-	<br /></td>
- <td style="vertical-align: top; text-align: center;">
-	<a href="list_fourn.php">Liste<br />des fournisseurs</a>
-	<br /></td>
-
- <td style="vertical-align: top; text-align: center;">
-	<a href="add_fourn.php">Ajout<br />d'un fournisseur</a>
-	<br /></td>
-
-<?php }	?>
-
-<?php if ( $user_level >=3 ) {	?>
- <td style="vertical-align: top; text-align: center;">
-	<a href="add_categorie.php">Ajout<br />d'une cat&eacute;gorie</a>
-	<br /></td>
-
-<?php }	}?>
-
-</tr></tbody>
+	style="width: 90%; text-align: left; margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<td style="vertical-align: top; text-align: center;">
+			<a href="<?php GESTEX_URL_SERVICE ?>">Retour &agrave;<br /> la page du service</a>
+			<br />
+		</td>
+		<?php 
+		if($log == true) {
+			if ($user_level >= 2) { ?>
+				<td style="vertical-align: top; text-align: center;">
+					<a href="add_app2.php">Ajout<br />d'un appareil</a>
+					<br />
+				</td>
+				<td style="vertical-align: top; text-align: center;">
+					<a href="list_fourn.php">Liste<br />des fournisseurs</a>
+					<br />
+				</td>
+				<td style="vertical-align: top; text-align: center;">
+					<a href="add_fourn.php">Ajout<br />d'un fournisseur</a>
+					<br />
+				</td>
+			<?php }	?>
+			<?php if ($user_level >= 3) { ?>
+				<td style="vertical-align: top; text-align: center;">
+					<a href="add_categorie.php">Ajout<br />d'une cat&eacute;gorie</a>
+					<br />
+				</td>
+			<?php } ?>
+		<?php } ?>
+		</tr>
+	</tbody>
 </table>
 
 <br />
 Liste des appareils : <br />
 <i>Affichage de la liste globale ou bien des appareils en pr&ecirc;t au service instrumentation...</i><br />
-<br /><table cellpadding="20" cellspacing="4" border="1"
- style="width: 70%; text-align: left; margin-left: auto; margin-right: auto;">
-
- <tbody>
-    <tr bgcolor="#f7d709">
-
-	 <th style="vertical-align: top; text-align: center;">
-
-	<a href ="instru.php?categorie=0 ">Liste globale</a><br />
-      </th>
-
- <th style="vertical-align: top; text-align: center;">
-
-	<a href ="instru.php?equipe=15">Appareils au service instrumentation</a><br />
-      </th>
-</tr></tbody>
-
+<br />
+<table cellpadding="20" cellspacing="4" border="1"
+	style="width: 70%; text-align: left; margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr bgcolor="#f7d709">
+			<th style="vertical-align: top; text-align: center;">
+				<a href ="instru.php?categorie=0 ">Liste globale</a><br />
+			</th>
+			<th style="vertical-align: top; text-align: center;">
+				<a href ="instru.php?equipe=15">Appareils au service instrumentation</a><br />
+			</th>
+		</tr>
+	</tbody>
 </table>
+
 <br />
 Liste des appareils par cat&eacute;gorie : <br />
-
 <i>Cliquer sur une cat&eacute;gorie pour voir la liste...</i><br />
-<br /><table cellpadding="10" cellspacing="2" border="1"
- style="width: 70%; text-align: center; margin-left: auto; margin-right: auto;">
-  <tbody>
-    <tr bgcolor="#f7d709">
+<br />
+<table cellpadding="10" cellspacing="2" border="1"
+	style="width: 70%; text-align: center; margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr bgcolor="#f7d709">
+			<td style=\"vertical-align: top;\">
 
 <?php
-if ( $pdo = connect_db() ){
-
+if ($pdo = connect_db()) {
 	// recupere les refs du user
-
-	$sql = 'SELECT * FROM categorie ORDER BY ? ASC';
-	// list($qh,$num) = query_db($querry);
-	// $last_id=0;
-// $data = result_db($qh);
-
+	$sql = 'SELECT id, nom FROM categorie ORDER BY ? ASC;';
 	$stmt =$pdo->prepare($sql);
 	$stmt->execute(array($tri));
 	$categorie = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	echo"<td style=\"vertical-align: top;\">";
-	// echo "<a href =\"instru.php?categorie=".$data['id']."\">". $data['nom']."</a>";
-	// echo "<br />";
-// while ($data = result_db($qh))
-foreach($categorie as $data){
-	//  echo"<td style=\"vertical-align: top;\">";
-
-	echo "<a href =\"instru.php?categorie=".$data['id']."\">". $data['nom']."</a>";
-echo "<br />";
-
-}
-
-      echo"</tr>";
-
+	foreach ($categorie as $data) {
+		echo '<a href="instru.php?categorie='.$data['id'].'">'.$data['nom'].'</a>';
+		echo '<br />'.PHP_EOL;
+	}
 }
 ?>
-</tbody>
+
+			</td>'
+		</tr>'
+	</tbody>
 </table>
 
-<?php
- if ($log==true && (( $user_id ==33)|| ( $user_id ==2) || $user_id == 105))
-{?>
+<?php if ($log == true && (($user_id == 33)|| ( $user_id == 2) || $user_id == 105)) { ?>
 <br /><br /><br />
- <td style="vertical-align: top; text-align: center;">
+<td style="vertical-align: top; text-align: center;">
 	<a href="demandes.php">Demandes en cours</a>
 <td style="vertical-align: top; text-align: center;">
 	<a href="prets.html">Pr&ecirc;ts en cours</a>
 <?php } ?>
 
-<br /><br /><br />
+<br />
 </div>
 <?php pied_page() ?>
 
