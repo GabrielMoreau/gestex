ae7651e55199f055309decad04984ab96e4b5210
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-08 11:03:12 +0200

    mise en place de pdo

--- del_task.php
+++ del_task.php
@@ -15,12 +15,12 @@ en_tete('Suppression T&acirc;che');
 $user_id = $_SESSION['user_id'];
 $logged_in_user = strtolower($_SESSION['logged_in_user']);
 
-$valid= $_GET[ok];
+$valid= $_GET['ok'];
 
-$id_manip = $_GET[idm];
+$id_manip = $_GET['idm'];
 if (empty($id_manip))
  Header("Location: manip_maint.php");
-$id_task = $_GET[idt];
+$id_task = $_GET['idt'];
 if (empty($id_task))
  Header("Location: manip_maint.php");
 
@@ -34,18 +34,21 @@ if (!isset($valid) || empty($valid) || $valid=="no"){
 }
 else{
 
-if ( $connex = connect_db() ){
+if ( $pdo = connect_db() ){
 
   //on supprime cette tache
 
-  $querry = "DELETE LOW_PRIORITY FROM tache WHERE id=$id_task LIMIT 1";
+  $sql = 'DELETE LOW_PRIORITY FROM tache WHERE id = ? LIMIT 1';
   ///query_db($querry);
-  $result = mysql_query($querry);
+  // $result = mysql_query($querry);
+  $stmt = $pdo->prepare($sql);
+    $stmt->execute(array($id_task));
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
    //
    if (!$result){
    //inscription !ok
-   $erreur = mysql_error();
-   echo "<br />erreur :".$erreur;
+  //  $erreur = mysql_error();
+   echo "<br />erreur ";
 
  }
 else
