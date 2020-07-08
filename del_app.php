886a0530e5d14621ced26728d94d9b6973e23b6c
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-08 11:00:18 +0200

    mise en place de pdo

--- del_app.php
+++ del_app.php
@@ -11,15 +11,17 @@ if (!auth(3))
 $user_id = $_SESSION['user_id'];
 $logged_in_user = strtolower($_SESSION['logged_in_user']);
 
-$id_app = $_GET[id];
+$id_app = $_GET['id'];
 if (empty($id_app))
 	Header("Location: list_app.php");
 
-if ( $connex = connect_db() ){
+if ( $pdo = connect_db() ){
 
 // on supprime le fournisseur
-	$querry = "DELETE LOW_PRIORITY FROM appareils WHERE id=$id_app LIMIT 1";
+	$sql = 'DELETE LOW_PRIORITY FROM appareils WHERE id = ? LIMIT 1';
 	list($qh,$num) = query_db($querry);
+	$stmt = $pdo->prepare($sql);
+    $stmt->execute(array($id_app));
 
 }
 
