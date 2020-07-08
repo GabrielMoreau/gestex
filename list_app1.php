d25be65fbc46e7f3fbe2881fec2a1f316c215633
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-08 21:45:38 +0200

    Change link (but waht is really app1 ?

--- list_app1.php
+++ list_app1.php
@@ -119,12 +119,12 @@ while ($data = result_db($qh)) {
 
  if ( $user_level >=2 ) {
       echo"</td><td style=\"vertical-align: top;\">";
-      echo "<a href=\"add_app.php?id=".$data['id']."\"<img src=\"images/pen.svg\" nosave=\"\" title=\">Modifier\"></a>";
+      echo "<a href=\"add_app2.php?id=".$data['id']."\"<img src=\"images/pen.svg\" nosave=\"\" title=\">Modifier\"></a>";
       echo"</td>";
 	}//end if
  if ( $user_level >=3 ) {
       echo"</td><td style=\"vertical-align: top;\">";
-      echo "<a href=\"del_app.php?id=".$data['id']."\"><img src=\"images/trash.svg\" nosave=\"\" title=\"Supprimer\"></a>";
+      echo "<a href=\"del_app2.php?id=".$data['id']."\"><img src=\"images/trash.svg\" nosave=\"\" title=\"Supprimer\"></a>";
       echo"</td>";
 
 	}//end if
