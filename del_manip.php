98aa7c3c433cd13b96cd277a915637340d0c3709
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-08 11:02:44 +0200

    mise en place de pdo

--- del_manip.php
+++ del_manip.php
@@ -15,9 +15,9 @@ en_tete('Suppression Manip');
 $user_id = $_SESSION['user_id'];
 $logged_in_user = strtolower($_SESSION['logged_in_user']);
 
-$valid= $_GET[ok];
+$valid= $_GET['ok'];
 
-$id_manip = $_GET[id];
+$id_manip = $_GET['id'];
 if (empty($id_manip))
  Header("Location: list_manip.php");
 
@@ -29,37 +29,53 @@ if (!isset($valid) || empty($valid) || $valid=="no"){
   echo "<a href=\"".$_SERVER[HTTP_REFERER]."\">NON</a><br />";
 
 }
-else{if ( $connex = connect_db() ){
-$result=1;
-//on cherche tous les projets correspondant a la manip
- $querry="SELECT id FROM projet WHERE manip=$id_manip";
- list($qh,$num) = query_db($querry);
- while ($data = result_db($qh) && $result) {
- //pour chaque projet de la manip
-  // on supprime toutes les taches liees a ce projet
-  $querry = "DELETE LOW_PRIORITY FROM tache WHERE projet=$data['id']";
-    $result = mysql_query($querry);
-  //on supprime ce projet
-  $querry = "DELETE LOW_PRIORITY FROM projet WHERE manip=$id_manip";
-    $result = mysql_query($querry);
- }//end while projet
- if ($result){
-  //on supprime la manip
-  $querry = "DELETE LOW_PRIORITY FROM manip WHERE id=$id_manip";
+else{
+  if ( $pdo = connect_db() ){
+    //on cherche tous les projets correspondant a la manip
+    $sql='SELECT id FROM projet WHERE manip = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array($id_manip));
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // list($qh,$num) = query_db($querry);
+    // while ($data = result_db($qh) && $result) {
+      foreach($result as $data){
+    //pour chaque projet de la manip
+      // on supprime toutes les taches liees a ce projet
+      $sql = 'DELETE LOW_PRIORITY FROM tache WHERE projet = ?';
+      $stmt = $pdo->prepare($sql);
+        $stmt->execute(array($data['id']));
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $result = mysql_query($querry);
-     }
-   if (!$result){
-   //inscription !ok
-   $erreur = mysql_error();
-   echo "<br />erreur :".$erreur;
+        // $result = mysql_query($querry);
+      //on supprime ce projet
+      $sql = "DELETE LOW_PRIORITY FROM projet WHERE manip = ?";
+      $stmt = $pdo->prepare($sql);
+        $stmt->execute(array($id_manip));
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // $result = mysql_query($querry);
+    }//end while projet
+    if ($result){
+      //on supprime la manip
+      $sql = 'DELETE LOW_PRIORITY FROM manip WHERE id = ?';
 
- }
-else
- echo "Manip ".$manip_id." supprim&eacute;e, ainsi que ses projets et taches!<br />";
-//on retourne a la page precedente
-  echo "<a href=\"list_manip.php\">Suite</a><br />";
-}}
+        // $result = mysql_query($querry);
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array($id_manip));
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+      if (!$result){
+      //inscription !ok
+      $erreur = mysql_error();
+      echo "<br />erreur :".$erreur;
+
+    }
+    else
+    echo "Manip ".$manip_id." supprim&eacute;e, ainsi que ses projets et taches!<br />";
+    //on retourne a la page precedente
+    echo "<a href=\"list_manip.php\">Suite</a><br />";
+  }
+}
 
 ?>
 <?php pied_page() ?>
