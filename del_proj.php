2cc6cfa017012277bcd3c8828b900929eacb6601
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-08 11:03:01 +0200

    mise en place de pdo

--- del_proj.php
+++ del_proj.php
@@ -15,12 +15,12 @@ en_tete('Suppression Projet');
 $user_id = $_SESSION['user_id'];
 $logged_in_user = strtolower($_SESSION['logged_in_user']);
 
-$valid= $_GET[ok];
+$valid= $_GET['ok'];
 
-$id_manip = $_GET[idm];
+$id_manip = $_GET['idm'];
 if (empty($id_manip))
  Header("Location: manip_maint.php");
-$id_proj = $_GET[idp];
+$id_proj = $_GET['idp'];
 if (empty($id_proj))
  Header("Location: manip_maint.php");
 
@@ -33,21 +33,27 @@ if (!isset($valid) || empty($valid) || $valid=="no"){
 
 }
 else{
-if ( $connex = connect_db() ){
+if ( $pdo = connect_db() ){
 
  // on supprime toutes les taches liees a ce projet
- $querry = "DELETE LOW_PRIORITY FROM tache WHERE projet=$id_proj";
-    $result = mysql_query($querry);
+ $sql = 'DELETE LOW_PRIORITY FROM tache WHERE projet = ?';
+    // $result = mysql_query($querry);
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array($id_proj));
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
  if ($result){
   //on supprime ce projet
-  $querry = "DELETE LOW_PRIORITY FROM projet WHERE id=$id_proj LIMIT 1";
-  $result = mysql_query($querry);
+  $sql = 'DELETE LOW_PRIORITY FROM projet WHERE id = ? LIMIT 1';
+  // $result = mysql_query($querry);
+  $stmt = $pdo->prepare($sql);
+    $stmt->execute(array($id_proj));
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
  }
 
    if (!$result){
    //inscription !ok
-   $erreur = mysql_error();
-   echo "<br />erreur :".$erreur;
+  //  $erreur = mysql_error();
+   echo "<br />erreur ";
 
  }
 else
