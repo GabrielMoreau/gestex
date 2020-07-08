650a8aa5f91fd48f72f63df9464c727b94e9c4af
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-08 15:21:33 +0200

    indentation + recup URL + vérif suppression

--- del_user.php
+++ del_user.php
@@ -4,51 +4,49 @@
 
 // Authenticate
 include("session_auth.php");
+require("html_functions.php");
 
 if (!auth(3))
- Header("Location: login.php");
-require("html_functions.php");
+  Header("Location: login.php");
 
 en_tete('Suppression Utilisateur');
 
 $user_id = $_SESSION['user_id'];
 $logged_in_user = strtolower($_SESSION['logged_in_user']);
 
-$valid= $_GET['ok'];
-$id_user = $_GET['id'];
-if (empty($id_user))
+if (empty($_GET['id']))
  Header("Location: list_user.php");
+else
+  $id_user = $_GET['id'];
 
-echo "user:".$id_equip. " ok :".$valid."<br />";
-
+if(empty($_GET['ok'])) // On récupère une variable ok qui sert a vérifier que la personne est bien sûr de supprimer la catégorie choisi
+	$valide ='no'	// s'il n'y a pas d'id, on met 'no' dans $valid
+else if($_GET['ok']=='yes') // si ok dans l'url est 'yes', on valide la suppression
+	$valide = 'yes';
+else	// si c'est n'importe quoi d'autre, on ne valide pas la suppression
+  $valid = 'no'; 
+  
 if (!isset($valid) || empty($valid) || $valid=="no"){
- echo "Sur de supprimer l'utilisateur ".$user_id. " ?<br />";
- echo "<a href=\"".$_SERVER[PHP_SELF]."?id=".$user_id."&ok=yes\">OUI</a><br />";
+ echo "Sur de supprimer l'utilisateur ".$id_user. " ?<br />";
+ echo "<a href=\"".$_SERVER[PHP_SELF]."?id=".$id_user."&ok=yes\">OUI</a><br />";
   echo "<a href=\"".$_SERVER[HTTP_REFERER]."\">NON</a><br />";
 
 }
 else{
-if ( $pdo = connect_db() ){
-
-  //on supprime cet user
-
-  $sql = 'DELETE LOW_PRIORITY FROM users WHERE id = ? LIMIT 1';
-// $result = mysql_query($querry);
-$stmt = $pdo->prepare($sql);
-    $stmt->execute(array($user_id));
+  if ( $pdo = connect_db() ){
+    //on supprime cet user
+    $sql = 'DELETE LOW_PRIORITY FROM users WHERE id = ? LIMIT 1';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array($id_user));
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-   //
-   if (!$result){
-   //inscription !ok
-  //  $erreur = mysql_error();
-   echo "<br />erreur ";
-
- }
-else
- echo "Utilisateur ".$user_id." supprim&eacute;!<br />";
-//on retourne a la page precedente
-  echo "<a href=\"list_user.php\">Suite</a><br />";
-}
+    if (!$result){
+      echo "<br />erreur dans la suppression de l'utilisateur : ".$id_user;
+    }
+    else
+      echo "Utilisateur ".$id_user." supprim&eacute;!<br />";
+    //on retourne a la page precedente
+    echo "<a href=\"list_user.php\">Suite</a><br />";
+  }
 
 }
 
