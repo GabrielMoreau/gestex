38bd6d61b644ee46e15fe6f03aa845c77e426c95
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-09 18:04:58 +0200

    Format file

--- list_pret.php
+++ list_pret.php
@@ -1,5 +1,5 @@
 <?php
-
+// list_prets.php
 // Authenticate
 
 //include("db_functions.php");
@@ -8,9 +8,10 @@ session_start();
 //if (!auth(1))
 	//Header("Location: login.php");
 
-$user_id = $_SESSION['user_id'];
+$user_id        = $_SESSION['user_id'];
 $logged_in_user = strtolower($_SESSION['logged_in_user']);
-$user_level= $_SESSION['level'];
+$user_level     = $_SESSION['level'];
+
 require("html_functions.php");
 
 en_tete('Liste des pr&ecirc;ts');
@@ -22,189 +23,121 @@ else
 	$tri = $_GET['tri'];
 echo $tri;
 
-
-	//recupere l'equipe
-
+//recupere l'equipe
 if (empty($_GET['equipe']))
-	$eq ="";
+	$eq = '';
 else
 	$eq = $_GET['equipe'];
-
 ?>
+
+<i> Consulter la liste des &eacute;quipement communs disponibles au service instrumentation et choisir : 'Demande de pr&ecirc;t' en face de l'appareil souhait&eacute;</i><br />
 <br />
 <table cellpadding="2" cellspacing="2" border="1"
- style="width: 90%; text-align: left; margin-left: auto; margin-right: auto;">
-  <tbody>
-    <tr>
-	 <td style="vertical-align: top; text-align: center;">
-	<a href="<?php GESTEX_URL_SERVICE ?></a>">Retour &agrave;<br />la page du service</a>
-	<br /></td>
+	style="width: 90%; text-align: left; margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr>
+			<td style="vertical-align: top; text-align: center;">
+				<a href="<?php GESTEX_URL_SERVICE ?></a>">Retour &agrave;<br />la page du service</a>
+				<br />
+			</td>
 
-<td style="vertical-align: top; text-align: center;">
-<?php if ( $user_level >=3 ) 	{?>
-	<a href="list-pret.php">Retour<br />&agrave; la liste des pr&ecirc;ts</a>
-	<br /></td>
-<?php }
-else{	?>
-<a href="list-pret.php">Retour<br />&agrave; la liste des pr&ecirc;ts</a>
-<?php }?>
-</tr></tbody>
+			<td style="vertical-align: top; text-align: center;">
+				<a href="list-pret.php">Retour<br />&agrave; la liste des pr&ecirc;ts</a>
+				<br />
+			</td>
+		</tr>
+	</tbody>
 </table>
 
 <br />
 Liste des pr&ecirc;ts : <br /><br /><br />
 
 <table cellpadding="2" cellspacing="2" border="1"
- style="width: 90%; text-align: left; margin-left: auto; margin-right: auto;">
-  <tbody>
-    <tr bgcolor="#f7d709">
+	style="width: 90%; text-align: left; margin-left: auto; margin-right: auto;">
+	<tbody>
+		<tr bgcolor="#f7d709">
+			<th style="vertical-align: top; text-align: center;">
+				Nom<br />
+			</th>
+			<th style="vertical-align: top; text-align: center;">
+				&Eacute;quipe<br />
+			</th>
+			<th style="vertical-align: top; text-align: center;">
+				Date<br />
+			</th>
+			<th style="vertical-align: top; text-align: center;">
+				Retour<br />
+			</th>
+			<th style="vertical-align: top; text-align: center;">
+				Emprunteur<br />
+			</th>
+			<th style="vertical-align: top; text-align: center;">
+				<a href="list_pret.php?tri=nom">
+					Num&eacute;ro de l'appareil<br />
+				</a>
+			</th>
 
-      <th style="vertical-align: top; text-align: center;">
-	Nom<br />
-      </th>
+			<?php 
+			if ( $user_level >=3 )
+				echo "<th></th>";
+			?>
+		</tr>
 
-      <th style="vertical-align: top; text-align: center;">
-	&Eacute;quipe<br />
-      </th>
-
-       <th style="vertical-align: top; text-align: center;">
-	Date<br />
-      </th>
- <th style="vertical-align: top; text-align: center;">
-	Retour<br />
-      </th>
-
- <th style="vertical-align: top; text-align: center;">
- Emprunteur<br />
-      </th>
-	  <th style="vertical-align: top; text-align: center;">
-	  <a href="list_pret.php?tri=nom">
-	Num&eacute;ro de l'appareil<br /></a>
-      </th>
-
-<?php 
-	if ( $user_level >=3 )
-		echo "</th><th>";
-	  ?>
-    </tr>
 <?php	// interrogation base de donnees
-
-if ( $pdo = connect_db() ){
+if ($pdo = connect_db()) {
 	// recupere la liste de appareils
 
-$sql = 'SELECT * FROM pret ORDER BY ? DESC;';
-// 	list($qh,$num) = query_db($querry);
-
-// 	$last_id=0;
-
-// $data = result_db($qh);
-$stmt = $pdo->prepare($sql);
-$stmt->execute(array($tri));
-$pret = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// echo "<tr>";
-
-//  echo"</td><td style=\"vertical-align: top;\">";
-
-// 	$sql = 'SELECT id, nom FROM Listing WHERE id = ?;';
-// 	// list($qheeq,$numeeq) = query_db($querry);
-// 	// 	$nom = result_db($qheeq);
-// 	$stmt = $pdo->prepare($sql);
-// 	$stmt->execute(array($pret[0]['nom']));
-// 	$listing = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// 	// if(!emty($listing))
-//     echo $listing[0]['nom'];
-
-//   echo"</td><td style=\"vertical-align: top;\">";
-
-// 	// recupere le nom d'equipe
-
-// 	$sql = 'SELECT id, nom FROM equipe WHERE id = ?;';
-// 	// list($qheq,$numeq) = query_db($querry);
-// 	// 	$equip = result_db($qheq);
-
-// 	$stmt = $pdo->prepare($sql);
-// 	$stmt->execute(array($pret[0]['equipe']));
-// 	$equip = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-//       		echo $equip[0]['nom'];
-//        echo"</td><td style=\"vertical-align: top;\">";
-
-// echo $pret[0]['emprunt'];
-
-// 	 echo"</td><td style=\"vertical-align: top;\">";
-
-// echo $pret[0]['retour'];
-
-//  echo"</td><td style=\"vertical-align: top;\">";
-
-// echo $pret[0]['commentaire'];
-//  echo"</td><td style=\"vertical-align: top;\">";
-
-//       		echo $listing[0]['id'];
-
-// 	if ( $use >=3 ) 	{
-
-//       echo"</td><td style=\"vertical-align: top;\">";
-//       echo '<a href="del-pret.php?id=',$pret[0]['id'],'"><img src="images/trash.svg" nosave="" title="Supprimer"></a>';
-//       echo"</td>";}
-
-// echo"</tr>";
-foreach($pret as $data){
-// while ($data = result_db($qh)){
-
-	// remplit le tableau
-
-	echo"</td><td style=\"vertical-align: top;\">";
-	$sql = 'SELECT id, nom FROM Listing WHERE id = ?;';
-	// list($qheeq,$numeeq) = query_db($querry);
-	// 	$nom = result_db($qheeq);
+	$sql = 'SELECT * FROM pret ORDER BY ? DESC;';
 	$stmt = $pdo->prepare($sql);
-	$stmt->execute(array($data['nom']));
-	$listing = $stmt->fetchAll(PDO::FETCH_ASSOC);
-      		echo $listing[0]['nom'];
+	$stmt->execute(array($tri));
+	$pret = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-       echo"</td><td style=\"vertical-align: top;\">";
+	foreach ($pret as $data) {
+		// remplit le tableau
+		echo '<tr">'.PHP_EOL;
 
-	// recupere le nom d'equipe
+		$sql = 'SELECT id, nom FROM Listing WHERE id = ?;';
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute(array($data['nom']));
+		$listing = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		echo '  <td style="vertical-align: top;">';
+		echo      $listing[0]['nom'];
+		echo '  </td>'.PHP_EOL;
 
-	$sql = 'SELECT id, nom FROM equipe WHERE id = ?;';
-	// list($qheq,$numeq) = query_db($querry);
-	// 	$equip = result_db($qheq);
+		// recupere le nom d'equipe
+		$sql = 'SELECT id, nom FROM equipe WHERE id = ?;';
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute(array($data['equipe']));
+		$equip = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		echo '  <td style="vertical-align: top;">';
+		echo      $equip[0]['nom'];
+		echo '  </td>'.PHP_EOL;
 
-	$stmt = $pdo->prepare($sql);
-	$stmt->execute(array($data['equipe']));
-	$equip = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		echo '  <td style="vertical-align: top;">';
+		echo      $data['emprunt'];
+		echo '  </td>'.PHP_EOL;
+		echo '  <td style="vertical-align: top;">';
+		echo    $data['retour'];
+		echo '  </td>'.PHP_EOL;
+		echo '  <td style="vertical-align: top;">';
+		echo      $data['commentaire'];
+		echo '  </td>'.PHP_EOL;
+		echo '  <td style="vertical-align: top;">';
+		echo      $data['nom'];
+		echo '  </td>'.PHP_EOL;
 
-      		echo $equip[0]['nom'];
-       echo"</td><td style=\"vertical-align: top;\">";
+		if ($user_level >= 3) 	{
+			echo '  <td style="vertical-align: top;">';
+			echo '    <a href="del-pret.php?id=',$data['id'],'"><img src="images/trash.svg" nosave="" title="Supprimer" /></a>';
+			echo '  </td>'.PHP_EOL;
+		}
 
-echo $data['emprunt'];
-
-	 echo"</td><td style=\"vertical-align: top;\">";
-
-echo $data['retour'];
-
- echo"</td><td style=\"vertical-align: top;\">";
-
-echo $data['commentaire'];
-echo"</td><td style=\"vertical-align: top;\">";
-
-      		echo $data['nom'];
-
- if ( $user_level >=3 ) 	{
-	echo '  <td style="vertical-align: top;">';
-	echo '    <a href="del-pret.php?id=',$data['id'],'"><img src="images/trash.svg" nosave="" title="Supprimer" /></a>';
-	echo '  </td>'.PHP_EOL;
-	}
-
-echo"</tr>";
-
-	}//end while
-
-}//end if
+		echo '</tr>'.PHP_EOL;
+	} //end foreach
+} //end if
 
 ?>
-  </tbody>
+	</tbody>
 </table>
 <br />
 </div>
