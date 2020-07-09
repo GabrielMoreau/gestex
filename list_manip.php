50d6ff121db7e7da5aa6f22fc7c1429f896987c8
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-09 23:35:51 +0200

    Add even an odd in table but loose bold for my manip !

--- list_manip.php
+++ list_manip.php
@@ -29,42 +29,13 @@ if ( $pdo = connect_db() ){
 	$stmt = $pdo->prepare($sql);
 	$stmt->execute(array($logged_in_user));
 	$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-	// echo 'Bienvenue ',$user[0]['prenom'],'', $user[0]['nom'] , '(',$user_id,')<br /><br />';
-	// echo '<br>';
-
 	?>
 	<br />
 	<table cellpadding="2" cellspacing="2" border="0" style="width: 90%; text-align: left; margin-left: auto; margin-right: auto;">
 		<tbody>
 			<tr class=menu>
-				<th style="vertical-align: top; text-align: center; ">
-					<a href="list_fourn.php?tri=nom">Liste des<br />Fournisseurs</a>
-					<br />
-				</th>
-				<!-- <td style="vertical-align: top; text-align: center;">
-				<a href="list_machine.php">Liste des<br />Appareils</a>
-				<br /></td> -->
-				<th style="vertical-align: top; text-align: center; " >
-					<a href="list_user.php?tri=nom">Liste des<br />Utilisateurs</a>
-					<br />
-				</th>
-				<th style="vertical-align: top; text-align: center; " >
-					<a href="list_equip.php?tri=nom">Liste des<br />&Eacute;quipes</a>
-					<br />
-				</th>
-				<?php if ($user_level>=2){  ?>
-				<th style="vertical-align: top; text-align: center; " >
-					<a href="add_manip.php">Ajouter<br />une Manip</a>
-					<br />
-				</th>
 				<th style="vertical-align: top; text-align: center; class:menu;" >
 					<a href="rapport-create.php">Editer<br />les rapports</a>
-					<br />
-				</th>
-				<?php } ?>
-				<th style="vertical-align: top; text-align: center; " >
-					<a href="logout.php?variable=projet">Quitter</a>
 					<br />
 				</th>
 			</tr>
@@ -108,34 +79,34 @@ if ( $pdo = connect_db() ){
 			<?php	//interrogation base de donnees
 
 			// recupere la liste de manips
-			// $querry = "SELECT * FROM manip ";
-
-			// $querry.="ORDER BY '$tri' ";
-			// list($qh,$num) = query_db($querry);
 			$sql = 'SELECT * FROM manip ORDER BY ?;';
 			$stmt = $pdo->prepare($sql);
 			$stmt->execute(array($tri));
 			$manip_fetch = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-			// while ($data = result_db($qh)) {
+			$num_line = 0;
 			foreach ($manip_fetch as $manip) {
-				$relief =0;
-				if ($user_level >1)
+				if (($num_line % 2 )==0)
+					echo '<tr class="pair">'.PHP_EOL;
+				else
+					echo '<tr class="impair">'.PHP_EOL;
+				$num_line++;
+				$relief = 0;
+				if ($user_level > 1)
 					$relief = 1;
 				// remplit le tableau
-				if ($user_level <= 1) {
+				//if ($user_level <= 1) {
 				//mise en evidence des manips concernant le chercheur connecte
-					if ($manip['chercheur'] == $user_id || $manip['chercheur_bis'] == $user_id){
-						echo '<tr bgcolor="#FFFAD0" style="vertical-align: top;">';
-						$relief = 1;
-					} else {
-						echo '<tr style="vertical-align: top;">';
-					}
-				} else {
-					echo '<tr style="vertical-align: top;">';
-				}
+				//	if ($manip['chercheur'] == $user_id || $manip['chercheur_bis'] == $user_id){
+				//		echo '<tr bgcolor="#FFFAD0" style="vertical-align: top;">';
+				//		$relief = 1;
+				//	} else {
+				//		echo '<tr style="vertical-align: top;">';
+				//	}
+				//} else {
+				//	echo '<tr style="vertical-align: top;">';
+				//}
 				echo '  <td style="vertical-align: top;">';
-				echo $manip['date'];
+				echo      $manip['date'];
 				echo '  </td>';
 				echo '  <td style="vertical-align: top;">';
 				if ($relief ==1)
@@ -143,7 +114,7 @@ if ( $pdo = connect_db() ){
 					echo '    <a href="manip_maint.php?id=',$manip['id'],'">';
 				echo $manip['nom'];
 				if ($relief == 1)
-					echo '    </a>';
+					echo ' '.ICON_URL.'</a>';
 				echo '  </td>';
 				echo '  <td style="vertical-align: top;">';
 				echo $manip['descr'];
