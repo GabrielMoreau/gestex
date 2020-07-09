8a3f8cd89c6fda57ccf7a1bb920461d76a0f55eb
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-09 15:39:41 +0200

    corrcetion d'erreur

--- del-pret.php
+++ del-pret.php
@@ -4,29 +4,28 @@
 
 // Authenticate
 include("session_auth.php");
-
-//if (!auth(3))
-	Header("Location: reserva.php?user=3");
+if (!auth(3))
+	Header("Location: reserva.php");
 
 $user_id = $_SESSION['user_id'];
 $logged_in_user = strtolower($_SESSION['logged_in_user']);
 
 if (empty($_GET['id']))
-	Header("Location: reserva.php?user=3");
+	Header("Location: reserva.php");
 else
 	$id_pret = $_GET['id'];
 
 if(empty($_GET['ok'])) // On recupere une variable ok qui sert a verifier que la personne est bien sur de supprimer la categorie choisi
-	$valide ='no'	// s'il n'y a pas d'id, on met 'no' dans $valid
+	$valid ='no';	// s'il n'y a pas d'id, on met 'no' dans $valid
 else if($_GET['ok']=='yes') // si ok dans l'url est 'yes', on valide la suppression
-	$valide = 'yes';
+	$valid = 'yes';
 else	// si c'est n'importe quoi d'autre, on ne valide pas la suppression
 	$valid = 'no'; 
 
 if (!isset($valid) || empty($valid) || $valid=="no"){
-	echo "Sur de supprimer l'utilisateur ".$id_pret. " ?<br />";
-	echo "<a href=\"".$_SERVER[PHP_SELF]."?id=".$id_pret."&ok=yes\">OUI</a><br />";
-	echo "<a href=\"".$_SERVER[HTTP_REFERER]."\">NON</a><br />";
+	echo "Sur de supprimer le pret ".$id_pret. " ?<br />";
+	echo "<a href=\"".$_SERVER['PHP_SELF']."?id=".$id_pret."&ok=yes\">OUI</a><br />";
+	echo "<a href=\"".$_SERVER['HTTP_REFERER']."\">NON</a><br />";
 }
 else{
 	if ( $pdo = connect_db() ){
@@ -34,15 +33,10 @@ else{
 		$sql = 'DELETE LOW_PRIORITY FROM pret WHERE id = ? LIMIT 1';
 		// list($qh,$num) = query_db($querry);
 		$stmt = $pdo->prepare($sql);
-		$stmt->execute(array($id_app));
-		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		if (!$result){ // si ca n'a pas marche
-			echo "<br />Erreur dans la suppression du pr&ecirc;t : ".$id_pret;
-		}else{
-		  	echo "Pr&ecirc;t ".$id_pret." supprim&eacute;!<br />";
-		}
+		$stmt->execute(array($id_pret));
+		
 	}
 	//on retourne a la page d'accueil
-	Header("Location: reserva.php?user=3");
+	Header("Location: reserva.php");
 }
 ?>
