72b7570b04794491b15b62a360094f7c0d071117
Sylène Moreau <sylene.moreau@isen-ouest.yncrea.fr>
2020-07-09 15:38:36 +0200

    correction d'erreur

--- del_equip.php
+++ del_equip.php
@@ -20,16 +20,16 @@ else
   $id_equip = $_GET['id'];
 
 if(empty($_GET['ok'])) // On recupere une variable ok qui sert a verifier que la personne est bien sur de supprimer la categorie choisi
-  $valide ='no'	// s'il n'y a pas d'id, on met 'no' dans $valid
+  $valid ='no';	// s'il n'y a pas d'id, on met 'no' dans $valid
 else if($_GET['ok']=='yes') // si ok dans l'url est 'yes', on valide la suppression
-  $valide = 'yes';
+  $valid = 'yes';
 else	// si c'est n'importe quoi d'autre, on ne valide pas la suppression
   $valid = 'no'; 
 
 if (!isset($valid) || empty($valid) || $valid=="no"){ // on regarde ce qu'il y a dans $valid et si c'est NULL ou 'no', on pose la question
   echo "Sur de supprimer l'&eacute;quipe ".$id_equip. " ?<br />";
-  echo "<a href=\"".$_SERVER[PHP_SELF]."?id=".$id_equip."&ok=yes\">OUI</a><br />"; // si la personne repond 'oui', on recharge la page en mettant ok=yes dans l'url 
-  echo "<a href=\"".$_SERVER[HTTP_REFERER]."\">NON</a><br />"; // sinon, on retourne a la page precedente
+  echo "<a href=\"".$_SERVER['PHP_SELF']."?id=".$id_equip."&ok=yes\">OUI</a><br />"; // si la personne repond 'oui', on recharge la page en mettant ok=yes dans l'url 
+  echo "<a href=\"".$_SERVER['HTTP_REFERER']."\">NON</a><br />"; // sinon, on retourne a la page precedente
 }
 else{
   if ( $pdo = connect_db() ){
@@ -39,14 +39,9 @@ else{
     $stmt = $pdo->prepare($sql);
     $stmt->execute(array($id_equip));
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		if (!$result){ // si ca n'a pas marche
-			echo "<br />Erreur dans la suppression de l'&eacute;quipe : ".$id_equip;
-		}else{
-			echo "&Eacute,quipe ".$id_equip." supprim&eacute;!<br />";
-		}
   }
     //on retourne a la page precedente
-    echo "<a href=\"list_equip.php\">Suite</a><br />";
+    Header("Location: list_equip.php");
 }
 
 ?>
