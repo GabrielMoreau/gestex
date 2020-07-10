3022143b48de8ca60e6f6d327cf8f4ccb79034f0
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-10 22:52:36 +0200

    Mostly format

--- valid_user.php
+++ valid_user.php
@@ -53,65 +53,42 @@ require("db_functions.php");
 
 if ($pdo = connect_db()) {
 
-// if (check_val('users', 'nom', $nom) != 0){
-//   //nom existant deja dans db
-//   $erreur ="le nom <i>".$nom."</i> est d&eacute;j&agrave; entr&eacute; dans la base de donn&eacute;es";
-//   }
-// else
-if (!empty(check_val('users', 'loggin', $loggin))) {
-	//nom existant deja dans db
-	$erreur = 'L\'identifiant <i>'.$loggin.'</i> est d&eacute;j&agrave; utilis&eacute; dans la base de donn&eacute;es';
-}
+	if (!empty(check_val_in_db($pdo, 'users', 'loggin', $loggin))) {
+		//nom existant deja dans db
+		$erreur = 'L\'identifiant <i>'.$loggin.'</i> est d&eacute;j&agrave; utilis&eacute; dans la base de donn&eacute;es';
+	}
 
-// if (check_mail($mail) != 0){
-//   //adresse mail incorrecte
-//   $erreur ="l'adresse de courriel <i>".$mail."</i> est incorrecte";
-//   }
-
-/*if (!empty($_POST[loggin]))
-  echo "loggin :".$_POST[loggin].".";
-echo "<br />passwd :".$password.".";
-echo "<br />passwd :".$password2.".";
-echo "<br />nom :".$nom.".";
-echo "<br />mail :".$mail.".";
-echo "<br />tel :".$phone.".";
-echo "<br />Level :".$level.".";*/
-
-if (!empty($erreur)) {
-	//erreur
-	echo '<br /><b>Erreur de saisie : </b>'.$erreur;
-	echo '<br /><center><a href="add_user.php?'.$loggin.'">Suite</a></center><br />';
-}
-else {
-	/// tout est ok
-	// pas d'erreur
-	/// on inscrit
-	// inscription
-	$mot_crypte = md5($password);
-	// $table = "users";
-	$sql = 'INSERT INTO users (nom, prenom, loggin, password, email, level, tel, equipe, valid) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0);';
-	$stmt = $pdo->prepare($sql);
-	$stmt->execute(array($nom, $prenom, $loggin, $mot_crypte, $mail, $level, $phone, $equipe));
-	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  
-	if (!$result) {
-		// inscription !ok
-		// $erreur = mysql_error();
-		echo '<br /><b>erreur dans l\'acc&egrave;s &agrave; la base de donn&eacute;es</b>';
+	if (!empty($erreur)) {
+		//erreur
+		echo '<br /><b>Erreur de saisie : </b>'.$erreur;
+		echo '<br /><center><a href="add_user.php?'.$loggin.'">Suite</a></center><br />';
 	}
 	else {
-		// inscription enregistree mais pas encore validee!
-		// envoi d'un courriel a l'admin
-		$texte = 'Inscription de '.$prenom.' '.$nom;
-		mail(GESTEX_ADMIN_MAIL, "[GestEx] ajout utilisateur - ".$nom." ".$prenom, $texte);
+		/// tout est ok
+		$mot_crypte = md5($password);
+		$sql = 'INSERT INTO users (nom, prenom, loggin, password, email, level, tel, equipe, valid) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0);';
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute(array($nom, $prenom, $loggin, $mot_crypte, $mail, $level, $phone, $equipe));
+		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	  
+		//if (!$result) {
+			// inscription !ok
+			// $erreur = mysql_error();
+		//	echo '<br /><b>Erreur dans l\'acc&egrave;s &agrave; la base de donn&eacute;es</b>';
+		//}
+		//else {
+			// inscription enregistree mais pas encore validee!
+			// envoi d'un courriel a l'admin
+			$texte = 'Inscription de '.$prenom.' '.$nom;
+			mail(GESTEX_ADMIN_MAIL, "[GestEx] ajout utilisateur - ".$nom." ".$prenom, $texte);
 
-		echo 'Inscription de '.$prenom.' '.$nom.'<br />';
-		echo ' <img src="images/pool_project.jpg" height="100" nosave="" align="middle" alt="" />"';
-		echo ' est propos&eacute;e avec l\'identifiant (login) : '.$loggin;
-		echo '<br />Vous serez pr&eacute;venu de sa validation par courriel...';
-	} // end else
-	echo '<br /><center><a href="list_user.php">Suite</a></center><br /><br />';
-} // else end
+			echo 'Inscription de '.$prenom.' '.$nom.'<br />';
+			echo ' <img src="images/pool_project.jpg" height="100" nosave="" align="middle" alt="" />"';
+			echo ' est propos&eacute;e avec l\'identifiant (login) : '.$loggin;
+			echo '<br />Vous serez pr&eacute;venu de sa validation par courriel...';
+		//} // end else
+		echo '<br /><center><a href="list_user.php">Suite</a></center><br /><br />';
+	} // else end
 } //end if connect
 
 pied_page();
