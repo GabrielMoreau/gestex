0a16ee58840eae8319f930ae69ba1724a7fbc0ae
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-10 23:20:06 +0200

    Downgrade level to 1 if user disable

--- session_auth.php
+++ session_auth.php
@@ -1,61 +1,58 @@
 <?php
 
-	require("db_functions.php");
+require("db_functions.php");
 
-	/* authentication function: this is called by
-	   each page to ensure that there is an authenticated user
-	*/
-	function auth($reqlevel, $logged_in_user='', $password='') {
+/* authentication function: this is called by
+   each page to ensure that there is an authenticated user
+*/
+function auth($reqlevel, $logged_in_user='', $password='') {
 
-		//start/continue the session
-		session_start();
-		if (!empty($_SESSION['logged_in_user']))
-			return true;
+	//start/continue the session
+	session_start();
+	if (!empty($_SESSION['logged_in_user']))
+		return true;
 
-		$check = !empty($logged_in_user);
+	$check = !empty($logged_in_user);
 
-		if ($check) {
-			// echo " not empty!";
-			$pdo = connect_db();
-			// list($qh,$num) = query_db("select password , nom , id , level ".
-			// 						 "FROM users WHERE loggin = '$logged_in_user';");
-			// $data = result_db($qh);
-			$sql = 'SELECT password, id, level FROM users WHERE loggin = ?;';
-			$stmt = $pdo->prepare($sql);
-			$stmt->execute(array($logged_in_user));
-			$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-			// var_dump ($user);
+	if ($check) {
+		$pdo = connect_db();
+		$sql = 'SELECT password, id, level, status FROM users WHERE loggin = ?;';
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute(array($logged_in_user));
+		$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-			//echo "db->".$num." (".$data['password'].") ".$data['nom']."  pour ".$password."=".md5($password);
-			//is the password correct
-			if ($user[0]['password'] != md5($password)){
-				//pas le bon ppasswd
-				return 0;//false;
-			}else if ($reqlevel > $user['level']){
-				//pas le niveau d'autorisation requis
-				return 0;//false;
-
-			}else {	///tout ok
-				//set session variables
-				$_SESSION['user_id'] = $user[0]['id'];
-				$_SESSION['logged_in_user'] = $logged_in_user;
-				$_SESSION['level'] = $user[0]['level'];
-				// return $user['level'];//true
-				return 1;
-			}
-		}else {
-			//unset all the variables
-			session_unset();
-
-			//destroy the session
-			session_destroy();
-
-			return 0; ///false;
+		// is the password correct
+		if ($user[0]['password'] != md5($password)) {
+			//pas le bon ppasswd
+			return 0; // false;
+		} else if ($reqlevel > $user[0]['level']){
+			//pas le niveau d'autorisation requis
+			return 0;//false;
+		} else { // tout ok
+			// down the level for disable user
+			$level = $user[0]['level'];
+			if ($user[0]['status'] == 0 && $level > 1)
+				$level = 1;
+			//set session variables
+			$_SESSION['user_id'] = $user[0]['id'];
+			$_SESSION['logged_in_user'] = $logged_in_user;
+			$_SESSION['level'] = $level;
+			return 1;
 		}
+	} else {
+		//unset all the variables
+		session_unset();
+
+		//destroy the session
+		session_destroy();
+
+		return 0; ///false;
 	}
+}
+
 ////////////////////////////////////////////////////////////////////////////
 
- function logout(){
+function logout() {
 	//continue the session
 	session_start();
 	//unset all the variables
@@ -63,7 +60,6 @@
 
 	//destroy the session
 	session_destroy();
-
 }
 
 ?>
