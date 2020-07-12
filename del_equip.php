df4476b45d571a3e72954cb3ef0b908716a528cf
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-12 22:12:59 +0200

    Rewrite with new form and better secure

--- del_equip.php
+++ del_equip.php
@@ -9,23 +9,33 @@ require_once('html_functions.php');
 auth_or_login('del_equip.php');
 level_or_alert(3, 'Suppression d\'une &eacute;quipe');
 
-en_tete('Suppression &eacute;quipe');
-
 $user_id        = $_SESSION['user_id'];
 $logged_in_user = strtolower($_SESSION['logged_in_user']);
 
-$id_equip = $_GET['id'];
+$id_equip = $_POST['id'];
 if (empty($id_equip))
+	$id_equip = $_GET['id'];
+if (empty($id_equip) || $_POST['ok'] == 'cancel')
 	redirect('list_equip.php');
 
-if(empty($_GET['ok'])) // On recupere une variable ok qui sert a verifier que la personne est bien sur de supprimer la categorie choisi
-	$valid = 'no'; // s'il n'y a pas d'id, on met 'no' dans $valid
-else if ($_GET['ok'] == 'yes') // si ok dans l'url est 'yes', on valide la suppression
+$valid = 'no';
+if ($_POST['ok'] == 'yes') // si ok dans l'url est 'yes', on valide la suppression
 	$valid = 'yes';
-else	// si c'est n'importe quoi d'autre, on ne valide pas la suppression
-	$valid = 'no'; 
 
-if (!isset($valid) || empty($valid) || $valid== 'no'){ // on regarde ce qu'il y a dans $valid et si c'est NULL ou 'no', on pose la question
+if ($valid == 'yes') {
+	if ( $pdo = connect_db() ){
+		// on supprime l'equipe
+		$sql = 'DELETE LOW_PRIORITY FROM equipe WHERE id = ? LIMIT 1';
+		//  $result = mysql_query($querry);
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute(array($id_equip));
+		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+	//on retourne a la page precedente
+	redirect('list_equip.php');
+}
+
+en_tete('Suppression &eacute;quipe');
 ?>
 
 <center class="alert">
@@ -38,21 +48,5 @@ if (!isset($valid) || empty($valid) || $valid== 'no'){ // on regarde ce qu'il y 
 	<button type="submit" name="ok" value="cancel">Annuler</button>
 </form>
 </center>
-
-<?php
-}
-else{
-	if ( $pdo = connect_db() ){
-		// on supprime l'equipe
-		$sql = 'DELETE LOW_PRIORITY FROM equipe WHERE id = ? LIMIT 1';
-		//  $result = mysql_query($querry);
-		$stmt = $pdo->prepare($sql);
-		$stmt->execute(array($id_equip));
-		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}
-	//on retourne a la page precedente
-	redirect('list_equip.php');
-}
-?>
 
 <?php pied_page() ?>
