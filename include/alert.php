1be6584237dadfcd3c6895301a09a1ae60cce865
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-12 11:01:45 +0200

    Add a page template to alert when the user has not the level needed

---
+++ include/alert.php
@@ -0,0 +1,15 @@
+<?php if (!$web_page) exit() ?>
+
+<?php en_tete('Alerte') ?>
+
+<div class="alert">
+	<center>
+		Vous ne disposez pas des autorisations suffisantes pour consulter la page
+		<br>
+		<b><?php echo $msg_alert ?></b>
+		<br>
+		Veuillez voir avec votre administrateur pour vous faire changer éventuellement les droits.
+	</center>
+</div>
+
+<?php pied_page() ?>
