ac60520d15638ac152fbf9076072f67181ef1497
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-22 23:29:10 +0200

    Rename user_level SESSION key

--- user-add.php
+++ user-add.php
@@ -9,9 +9,9 @@ require_once('html-functions.php');
 auth_or_login('user-list.php');
 level_or_alert(3, 'Modification d\'un utilisateur');
 
-$logged_id        = $_SESSION['logged_id'];
-$logged_user = strtolower($_SESSION['logged_user']);
-$logged_level     = $_SESSION['level'];
+$logged_id    = $_SESSION['logged_id'];
+$logged_user  = strtolower($_SESSION['logged_user']);
+$logged_level = $_SESSION['logged_level'];
 
 if (empty($_GET['id'])) {
 	//->nouvelle inscription
