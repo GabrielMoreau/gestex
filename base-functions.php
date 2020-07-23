defa250d87a198ab9d21d39ca011185ff7a1afb7
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-23 11:17:53 +0200

    Move some string functions

--- base-functions.php
+++ base-functions.php
@@ -1,6 +1,57 @@
 <?php
 
-////////////////////////////////////////////////////////////////////////
+// ---------------------------------------------------------------------
+
+function string_to_filename_snake($string) {
+	# $string = strtolower($string);
+	$string = str_replace(' ', '_', $string);
+	$string = str_replace('à', 'a', $string);
+	$string = str_replace('â', 'a', $string);
+	$string = str_replace('é', 'e', $string);
+	$string = str_replace('è', 'e', $string);
+	$string = str_replace('ê', 'e', $string);
+	$string = str_replace('ë', 'e', $string);
+	$string = str_replace('î', 'i', $string);
+	$string = str_replace('ï', 'i', $string);
+	$string = str_replace('ô', 'o', $string);
+	$string = str_replace('oe', 'oe', $string);
+	$string = str_replace('û', 'u', $string);
+	$string = str_replace('ù', 'u', $string);
+	$string = str_replace('ç', 'c', $string);
+	$string = preg_replace('/[^A-Za-z0-9-_]/', '_', $string);
+	$string = preg_replace('/__+/', '_', $string);
+	return $string;
+}
+
+// ---------------------------------------------------------------------
+
+function string_to_filename_kebab($string) {
+	$string = strtolower($string);
+	$string = preg_replace('/[ÀÂÄàâä]/',    'a', $string);
+	$string = preg_replace('/[ÉÈÊËéèêë]/',  'e', $string);
+	$string = preg_replace('/[ÎÏîï]/',      'i', $string);
+	$string = preg_replace('/[ÔÖôö]/',      'o', $string);
+	$string = preg_replace('/[ÙÛÜùûü]/',    'u', $string);
+	$string = preg_replace('/[Çç]/',        'c', $string);
+	#$string = preg_replace('/[OEoe]/',       'oe', $string);
+	$string = preg_replace('/[^a-z0-9-]/',  '-', $string);
+	$string = preg_replace('/-+/',          '-', $string);
+	return $string;
+}
+
+// ---------------------------------------------------------------------
+
+function random_string($length = 10) {
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+	$characters_length = strlen($characters);
+	$random_string = '';
+	for ($i = 0; $i < $length; $i++) {
+		$random_string .= $characters[rand(0, $characters_length - 1)];
+	}
+	return $random_string;
+}
+
+// ---------------------------------------------------------------------
 
 function theme($theme) {
 	if (empty($theme) or $theme === 'random') {
@@ -18,5 +69,7 @@ function theme($theme) {
 	}
 	return $theme;
 }
+
+// ---------------------------------------------------------------------
 
 ?>
