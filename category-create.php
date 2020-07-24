06b9c5499964ae3fc5b5735d2af7258069175151
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-24 13:28:32 +0200

    Nicer code

--- category-create.php
+++ category-create.php
@@ -9,16 +9,16 @@ auth_or_login('category-create.php');
 level_or_alert(3, 'Ajout d\'une cat&eacute;gorie');
 
 unset($erreur);
-//variables ne pouvant etre nulles
 
-$categorie = param_post('categorie');
-if (empty($categorie))
+//variables ne pouvant etre nulles
+$categorie_name = strtolower(param_post('categorie'));
+if (empty($categorie_name))
 	$erreur = 'categorie non pr&eacute;cis&eacute;';
 
 $pdo = connect_db();
 
-if (check_category_by_name($pdo, $categorie))
-	$erreur = 'La cat&eacute;gorie existe d&eacute;j&agrave;';
+if (check_category_by_name($pdo, $categorie_name))
+	$erreur = 'la cat&eacute;gorie existe d&eacute;j&agrave;';
 
 en_tete('R&eacute;sultat ajout cat&eacute;gorie');
 
@@ -30,9 +30,9 @@ if (!empty($erreur)) {
 	exit();
 }
 
-$id_category = set_category_new($pdo, $categorie);
+$id_category = set_category_new($pdo, $categorie_name);
 
-echo '<br />Ajout de la cat&eacute;gorie '.$categorie.' valid&eacute;e';
+echo '<br />Ajout de la cat&eacute;gorie '.$categorie_name.' valid&eacute;e';
 echo '<br /><br /><a href="equipment-list.php">Suite</a><br /><br />';
 ?>
 
