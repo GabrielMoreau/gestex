cea5daac794a151e0c62cf266bf1617fb87ddcfd
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-25 10:30:35 +0200

    Add lert boxe for data operation

---
+++ include/alert-data.php
@@ -0,0 +1,14 @@
+<?php if (!$web_page) exit() ?>
+
+<?php en_tete('Erreur avec la base de donn&eacute;es') ?>
+
+<div class="box-alert">
+	<center>
+		Une erreur est survenue lors de l'acc&egrave;es aux donn&eacute;es avec la base de donn&eacute;e.
+		<br>
+		Veuillez voir avec votre administrateur pour vous corriger &eacute;ventuellement les probl&egrave;mes sur votre base de donné&eacute;es.
+		En cas d'erreurs syst&eacute;matiques, veuillez remonter un bogue aux d&eacute;veloppeurs amont.
+	</center>
+</div>
+
+<?php pied_page() ?>
