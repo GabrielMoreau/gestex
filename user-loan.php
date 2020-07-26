7da11866d2cad81a0f9be474bb62facf59b940ec
Gabriel Moreau <gabriel.moreau@legi.grenoble-inp.fr>
2020-07-26 12:25:36 +0200

    Search now ok with RLIKE on family name (will be better if everything authentificate !)

--- user-loan.php
+++ user-loan.php
@@ -8,9 +8,9 @@ require_once('module/auth-functions.php');
 if (!auth(1))
 	Header("Location: login.php");
 
-$logged_id        = $_SESSION['logged_id'];
-$logged_user = strtolower($_SESSION['logged_user']);
-$logged_level     = $_SESSION['logged_level'];
+$logged_id    = $_SESSION['logged_id'];
+$logged_user  = strtolower($_SESSION['logged_user']);
+$logged_level = $_SESSION['logged_level'];
 
 require_once('module/html-functions.php');
 
@@ -21,83 +21,85 @@ if (empty($_GET['tri']))
 else
 	$tri = $_GET['tri'];
 
-if( $pdo =connect_db()){
-	$sql = 'SELECT * from pret where nom_utilisateur = ?;';
+if ($pdo = connect_db()) {
+	$user = get_user_all_by_id($pdo, $logged_id);
+	$sql = 'SELECT * FROM pret WHERE commentaire RLIKE ?;';
 	$stmt = $pdo->prepare($sql);
-	$stmt->execute(array($logged_id));
+	$stmt->execute(array($user['nom']));
 	$pret = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 ?>
 
-<table cellpadding="2" cellspacing="2" border="1"
-	style="width: 90%; text-align: left; margin-left: auto; margin-right: auto;">
+<div class="catalog">
+<table>
 	<tbody>
-		<tr bgcolor="#f7d709">
-			<th style="vertical-align: top; text-align: center;">
-				Nom<br />
+		<tr>
+			<th>
+				Nom
 			</th>
-			<th style="vertical-align: top; text-align: center;">
-				&Eacute;quipe<br />
+			<th>
+				&Eacute;quipe
 			</th>
-			<th style="vertical-align: top; text-align: center;">
-				Date<br />
+			<th>
+				Date
 			</th>
-			<th style="vertical-align: top; text-align: center;">
-				Retour<br />
+			<th>
+				Retour
 			</th>
-			<th style="vertical-align: top; text-align: center;">
-				Emprunteur<br />
+			<th>
+				Emprunteur
 			</th>
-			<th style="vertical-align: top; text-align: center;">
-				<a href="loan-list.php?tri=nom">
-					Num&eacute;ro de l'appareil<br />
-				</a>
+			<th>
+				Num&eacute;ro de l'appareil
 			</th>
-			<?php
-			foreach ($pret as $data) {
-				if ($num_line % 2)
+		</tr>
+		<?php
+		foreach ($pret as $data) {
+			if ($num_line % 2)
 				echo '<tr class="pair">'.PHP_EOL;
 			else
 				echo '<tr class="impair">'.PHP_EOL;
 			$num_line++;
-	
+
 			$sql = 'SELECT id, nom FROM Listing WHERE id = ?;';
 			$stmt = $pdo->prepare($sql);
 			$stmt->execute(array($data['nom']));
 			$listing = $stmt->fetchAll(PDO::FETCH_ASSOC);
-			echo '  <td style="vertical-align: top;">';
+			echo '  <td>';
 			echo      $listing[0]['nom'];
 			echo '  </td>'.PHP_EOL;
-	
+
 			// recupere le nom d'equipe
 			$team = get_team_by_id($pdo, $data['equipe']);
-			echo '  <td style="vertical-align: top;">';
+			echo '  <td>';
 			echo      $team['nom'];
 			echo '  </td>'.PHP_EOL;
-	
-			echo '  <td style="vertical-align: top;">';
+
+			echo '  <td>';
 			echo      $data['emprunt'];
 			echo '  </td>'.PHP_EOL;
-			echo '  <td style="vertical-align: top;">';
+			echo '  <td>';
 			echo    $data['retour'];
 			echo '  </td>'.PHP_EOL;
-			echo '  <td style="vertical-align: top;">';
+			echo '  <td>';
 			echo      $data['commentaire'];
 			echo '  </td>'.PHP_EOL;
-			echo '  <td style="vertical-align: top;">';
+			echo '  <td>';
 			echo      $data['nom'];
 			echo '  </td>'.PHP_EOL;
-	
-			if ($logged_level >= 3) 	{
-				echo '  <td style="vertical-align: top;">';
+
+			if ($logged_level >= 3)  {
+				echo '  <td>';
 				echo '    <a href="loan-del.php?id=',$data['id'],'">'.ICON_TRASH.'</a>';
 				echo '  </td>'.PHP_EOL;
 			}
-	
+
 			echo '</tr>'.PHP_EOL;
 		}
-			?>
-
+		?>
+	</tbody>
+</table>
+</div>
 
 <?php pied_page() ?>
 
