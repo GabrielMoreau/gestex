0859c54e21ea25af2cd8d91b8b855c44ad7cba32
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-09-26 15:01:43 +0200

    * Rename file

---
+++ connect-sample.php
@@ -0,0 +1,16 @@
+<?php
+
+//connect.php
+//infos de connexions a la base mySQL
+
+define('GESTEX_DB_USER',     "gestex-server");
+define('GESTEX_DB_PASSWORD', "gestex-magic");
+define('GESTEX_DB_SERVER',   "localhost");
+define('GESTEX_DB_DATABASE', "gestex");
+
+define('GESTEX_ADMIN_MAIL',  "webmaster@your-entity.sample");
+define('GESTEX_ENTITY_NAME', "YOUR ENTITY");
+define('GESTEX_URL_ENTITY',  "http://www.your-entity.sample/");
+define('GESTEX_URL_SERVICE', "http://intranet.legi.grenoble-inp.fr/spip.php?article=16");
+define('GESTEX_LOGO_ENTITY', "your-entity.jpg");
+?>
