a5cfb9f1fb0f512c573325bd4e4c25a4a4c649ba
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-09-28 22:01:47 +0200

    Rename unused variable !

--- connect-sample.php
+++ connect-sample.php
@@ -1,15 +1,15 @@
 <?php
 
-//connect.php
-//infos de connexions a la base mySQL
+// connect.php
+// infos de connexions a la base mySQL
 
-define('GESTEX_DB_USER',     "gestex-server");
-define('GESTEX_DB_PASSWORD', "gestex-magic");
+define('GESTEX_DB_USER',     "gestex-service");
+define('GESTEX_DB_PASSWORD', "gestex-magic-password");
 define('GESTEX_DB_SERVER',   "localhost");
 define('GESTEX_DB_DATABASE', "gestex");
 
 define('GESTEX_ADMIN_MAIL',  "webmaster@your-entity.sample");
 define('GESTEX_ENTITY_NAME', "YOUR ENTITY");
 define('GESTEX_ENTITY_URL',  "http://www.your-entity.sample/");
-define('GESTEX_LOGO_ENTITY', "your-entity.jpg");
+define('GESTEX_ENTITY_LOGO', "your-entity.jpg");
 ?>
