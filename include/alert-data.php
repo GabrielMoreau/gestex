8a8af943e8ec5244f3dfa892ecde7eedbd20994c
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-09-29 10:02:10 +0200

    Remove an hyphen

--- include/alert-data.php
+++ include/alert-data.php
@@ -6,7 +6,7 @@
 	<center>
 		Une erreur est survenue lors de l'acc&egrave;es aux donn&eacute;es avec la base de donn&eacute;e.
 		<br>
-		Veuillez voir avec votre administrateur pour vous corriger &eacute;ventuellement les probl&egrave;mes sur votre base de donné&eacute;es.
+		Veuillez voir avec votre administrateur pour vous corriger &eacute;ventuellement les probl&egrave;mes sur votre base de donn&eacute;es.
 		En cas d'erreurs syst&eacute;matiques, veuillez remonter un bogue aux d&eacute;veloppeurs amont.
 	</center>
 </div>
