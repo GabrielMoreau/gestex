ba61a73613ade26825cafbfd44efdbde94b888c0
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-05 15:40:14 +0200

    Use ul, nicer display

--- equipment-view.php
+++ equipment-view.php
@@ -187,13 +187,15 @@ en_tete('Caract&eacute;ristiques de l\'appareil : <b>'.$equipment_selected['nom'
 			<td>
 				<?php if ($equipment_selected['loanable'] == 1) {
 					if ($loan) {
-						echo 'Oui <span class="option-right">';
+						echo 'Oui, en pr&ecirc;t <span class="option-right">';
 						if ($logged_level >= 3) {echo '<a href="loan-del.php?id='.$loan['id'].'">';}
 						echo ICON_RETURN;
 						if ($logged_level >= 3) {echo '</a></span> <span class="option-right"><a href="loan-add.php?id=',$loan['id'],'">'.ICON_EDIT.'</a>&nbsp;';}
 						echo '</span>';
-						echo '<br>En pr&ecirc;t '.$loan['emprunt'].' &#8594; '.$loan['retour'];
-						echo '<br>'.$loan['commentaire'];
+						echo '<ul>';
+						echo '<li>'.$loan['emprunt'].' &#8594; '.$loan['retour'].'/li>';
+						echo '<li>'.$loan['commentaire'].'/li>';
+						echo '</ul>';
 					} else {
 						echo 'Oui'.'<span class="option-right">';
 						if ($logged_level >= 3) {echo '<a href="loan-add.php?equipment='.$equipment_selected['id'].'">';}
