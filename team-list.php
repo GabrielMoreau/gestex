feae4a17ad7babb3bdc03f918196d4160af48e1a
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-05 20:46:48 +0200

    Readable for all

--- team-list.php
+++ team-list.php
@@ -7,12 +7,15 @@ require_once('module/auth-functions.php');
 require_once('module/html-functions.php');
 require_once('module/base-functions.php');
 
-auth_or_login('team-list.php');
-level_or_alert(1, 'Liste de toutes les &eacute;quipes');
+session_start();
+if (empty($_SESSION['logged_user'])) {
+	$logged_level = 0;
+} else {
+	$logged_id    = $_SESSION['logged_id'];
+	$logged_user  = strtolower($_SESSION['logged_user']);
+	$logged_level = $_SESSION['logged_level'];
+}
 
-$logged_id    = $_SESSION['logged_id'];
-$logged_user  = strtolower($_SESSION['logged_user']);
-$logged_level = $_SESSION['logged_level'];
 $id_highlight = param_get('highlight', 0);
 
 en_tete('Liste de toutes les &eacute;quipes');
