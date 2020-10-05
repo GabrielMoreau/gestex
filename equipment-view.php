52ccfe8e8bbdb37ffe87493c0a960f7d3836ca02
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-05 15:42:27 +0200

    Error on end of li

--- equipment-view.php
+++ equipment-view.php
@@ -193,8 +193,8 @@ en_tete('Caract&eacute;ristiques de l\'appareil : <b>'.$equipment_selected['nom'
 						if ($logged_level >= 3) {echo '</a></span> <span class="option-right"><a href="loan-add.php?id=',$loan['id'],'">'.ICON_EDIT.'</a>&nbsp;';}
 						echo '</span>';
 						echo '<ul>';
-						echo '<li>'.$loan['emprunt'].' &#8594; '.$loan['retour'].'/li>';
-						echo '<li>'.$loan['commentaire'].'/li>';
+						echo '<li>'.$loan['emprunt'].' &#8594; '.$loan['retour'].'</li>';
+						echo '<li>'.$loan['commentaire'].'</li>';
 						echo '</ul>';
 					} else {
 						echo 'Oui'.'<span class="option-right">';
