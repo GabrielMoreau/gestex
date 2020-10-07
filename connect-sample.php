290d2a222640bdd789d9608cfb1bd35789370405
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-07 12:03:25 +0200

    Add some explanation and add an exit code !

--- connect-sample.php
+++ connect-sample.php
@@ -1,13 +1,17 @@
 <?php
+// connect-sample.php
 
-// connect.php
-// infos de connexions a la base mySQL
+// Rename and adapt this file under the name connect.php
 
+exit(); // Comment this line
+
+// Informations de connexions a la base mySQL
 define('GESTEX_DB_USER',     "gestex-service");
 define('GESTEX_DB_PASSWORD', "gestex-magic-password");
 define('GESTEX_DB_SERVER',   "localhost");
 define('GESTEX_DB_DATABASE', "gestex");
 
+// Parametres generaux
 define('GESTEX_ADMIN_MAIL',  "webmaster@your-entity.sample");
 define('GESTEX_ENTITY_NAME', "YOUR ENTITY");
 define('GESTEX_ENTITY_URL',  "http://www.your-entity.sample/");
