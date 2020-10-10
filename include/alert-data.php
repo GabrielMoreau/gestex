69e0a3591c07655a468d41641d24779c4b13c1fd
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-10 15:56:35 +0200

    * Add message_alert variable

--- include/alert-data.php
+++ include/alert-data.php
@@ -1,10 +1,16 @@
 <?php if (!$web_page) exit() ?>
 
+<?php
+// $message_alert
+?>
+
 <?php en_tete('Erreur avec la base de donn&eacute;es') ?>
 
 <div class="box-alert">
 	<center>
 		Une erreur est survenue lors de l'acc&egrave;es aux donn&eacute;es avec la base de donn&eacute;e.
+		<br>
+		<b><?=$message_alert?></b>
 		<br>
 		Veuillez voir avec votre administrateur pour vous corriger &eacute;ventuellement les probl&egrave;mes sur votre base de donn&eacute;es.
 		En cas d'erreurs syst&eacute;matiques, veuillez remonter un bogue aux d&eacute;veloppeurs amont.
