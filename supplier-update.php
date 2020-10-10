554b96cefa662a6d8f5eb6af0e890b2bb0ccd891
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-10 15:57:46 +0200

    Rewrite code to clean it and use include function

--- supplier-update.php
+++ supplier-update.php
@@ -2,86 +2,89 @@
 // supplier-update.php
 $web_page = true;
 
-// Authenticate
+// Module
 require_once('module/auth-functions.php');
 require_once('module/html-functions.php');
 
+// Authenticate
 auth_or_login('supplier-update.php');
 level_or_alert(3, 'Modification d\'un fournisseur');
 
 $logged_user  = strtolower($_SESSION['logged_user']);
 $logged_level = $_SESSION['logged_level'];
 
-//modification d'un fournisseur
+// modification d'un fournisseur
 
 unset($erreur);
-//variables ne pouvant etre nulles
-if (empty($_POST['id_fourn']))
-	$erreur = "id non pr&eacute;cis&eacute;";
-else {
-	$id_supplier = $_POST['id_fourn'];
 
-	if (empty($_POST['nom']))
-		$erreur = "nom non pr&eacute;cis&eacute;";
-	else {
-		$nom=$_POST['nom'];
-		if (empty($_POST['adresse']))
-			$erreur = "adresse non pr&eacute;cis&eacute;";
-		else {
-			$adresse = $_POST['adresse'];
-			$tel = $_POST['phone'];
-			$fax = $_POST['fax'];
-			$mail = $_POST['addr_mail'];
-			$www = $_POST['www'];
-			$contact = $_POST['contact'];
-			$descr = $_POST['descr'];
-		}
-	}
+// variables ne pouvant pas etre nulles
+$id_supplier = param_post('id_fourn');
+if (empty($id_supplier))
+	$erreur = 'Id non pr&eacute;cis&eacute;';
+
+$nom = param_post('nom');
+if (empty($nom))
+	$erreur = 'Nom non pr&eacute;cis&eacute;';
+
+$adresse = param_post('adresse');
+if (empty($adresse))
+	$erreur = 'Adresse non pr&eacute;cis&eacute;';
+
+// variables pouvant etre nulles
+$tel     = param_post('phone');
+$fax     = param_post('fax');
+$mail    = param_post('addr_mail');
+$www     = param_post('www');
+$contact = param_post('contact');
+$descr   = param_post('descr');
+
+if (!empty($erreur)) {
+	//erreur
+	$title        = 'Erreur fournisseur';
+	$action       = 'supplier-list.php?highlight='.$id_supplier;
+	$highlight    = $id_supplier;
+	$message_text = $erreur;
+	include_once('include/warning-box.php');
+	exit();
 }
 
 en_tete('R&eacute;sultat modification');
 
-if (!empty($erreur)) {
-	//erreur
-	echo '<br />Erreur : '.$erreur;
-	echo '<br /><a href="supplier-list.php?highlight='.$id_supplier.'#item'.$id_supplier.'">Suite</a><br />';
-	pied_page();
-	exit();
-}
+$pdo = connect_db_or_alert();
 
-if ($pdo = connect_db()) {
+// recupere les anciennes caracteristiques
+$supplier_registered = get_supplier_all_by_id($pdo, $id_supplier);
 
-	//recupere les anciennes caracteristiques
-	$supplier_registered = get_supplier_all_by_id($pdo, $id_supplier);
+$modif = 0;
+if (   ($nom     != $supplier_registered['nom'])
+	|| ($adresse != $supplier_registered['adresse'])
+	|| ($tel     != $supplier_registered['tel'])
+	|| ($fax     != $supplier_registered['fax'])
+	|| ($mail    != $supplier_registered['mail'])
+	|| ($www     != $supplier_registered['www'])
+	|| ($contact != $supplier_registered['contact'])
+	|| ($descr   != $supplier_registered['descr']))
+	$modif = 1;
 
-	//modification fournisseur
-	$modif = 0;
-	if (($nom != $supplier_registered['nom'])
-		|| ($adresse != $supplier_registered['adresse'])
-		|| ($tel != $supplier_registered['tel'])
-		|| ($fax != $supplier_registered['fax'])
-		|| ($mail != $supplier_registered['mail'])
-		|| ($www != $supplier_registered['www'])
-		|| ($contact != $supplier_registered['contact'])
-		|| ($descr != $supplier_registered['descr']))
-		$modif = 1;
-
-	if ($modif != 0) {
-		$err_msg = set_supplier_update($pdo, $id_supplier, $nom, $adresse, $tel, $fax, $mail, $www, $contact, $descr);
-		if ($err_msg != '' && $logged_level > 3) {
-			echo 'Erreur : '. $err_msg.'<br>';
-			echo '<br><br><a href="supplier-list.php?highlight='.$id_supplier.'#item'.$id_supplier.'">Suite</a><br><br>\n';
-			pied_page();
-			exit();
-		}
-	} // end if modif
-	else {
-		echo 'Aucune modification &agrave; faire';
-		echo '<br><br><a href="supplier-list.php?highlight='.$id_supplier.'#item'.$id_supplier.'">Suite</a><br><br>\n';
-		pied_page();
+if ($modif != 0) {
+	$err_msg = set_supplier_update($pdo, $id_supplier, $nom, $adresse, $tel, $fax, $mail, $www, $contact, $descr);
+	if ($err_msg != '') {
+		$title       = 'Erreur fournisseur';
+		$action      = 'supplier-list.php?highlight='.$id_supplier;
+		$highlight   = $id_supplier;
+		$message_txt = ($logged_level > 3 ? $err_msg : 'Erreur dans la mise &agrave; jour de la fiche fournisseur');
+		include_once('include/message-box.php');
 		exit();
 	}
-} // end if connect
 
-redirect('supplier-list.php?highlight='.$id_supplier.'#item'.$id_supplier);
+	redirect('supplier-list.php?highlight='.$id_supplier.'#item'.$id_supplier);
+}
+
+$title       = 'Modification fournisseur';
+$action      = 'supplier-list.php?highlight='.$id_supplier;
+$highlight   = $id_supplier;
+$message_txt = 'Aucune modification &agrave; faire';
+include_once('include/message-box.php');
+exit();
+
 ?>
