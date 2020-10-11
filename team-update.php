5e8936fe1052f47ecfb6393cd4988260d367ba17
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-11 10:12:08 +0200

    Begin rewrite variable and clean code

--- team-update.php
+++ team-update.php
@@ -2,29 +2,30 @@
 // team-update.php
 $web_page = true;
 
-// Authenticate
+// Module
 require_once('module/auth-functions.php');
 require_once('module/html-functions.php');
 
+// Authenticate
 auth_or_login('team-list.php');
 level_or_alert(3, 'Modification d\'une &eacute;quipe');
 
-$logged_user = strtolower($_SESSION['logged_user']);
-$logged_level     = $_SESSION['logged_level'];
+$logged_user  = strtolower($_SESSION['logged_user']);
+$logged_level = $_SESSION['logged_level'];
 
 unset($erreur);
 
-$id_equip = $_POST['id_equip'];
-$nom      = $_POST['nom'];
-$compte   = $_POST['compte'];
-$chef     = $_POST['chef'];
-$descr    = $_POST['descr'];
+$team_id = param_post('id_equip', 0);
+$nom     = param_post('nom');
+$compte  = param_post('compte');
+$chef    = param_post('chef');
+$descr   = param_post('descr');
 //variables ne pouvant etre nulles
-if (empty($_POST['id_equip']))
-	$erreur = 'id non pr&eacute;cis&eacute;';
-if (empty($_POST['nom']))
+if ($team_id == 0)
+	$erreur = 'Id non pr&eacute;cis&eacute;';
+if (empty($nom))
 	$erreur = 'Nom d\'&eacute;quipe non pr&eacute;cis&eacute;';
-if (empty($_POST['compte']))
+if (empty($compte))
 	$erreur = 'Compte non pr&eacute;cis&eacute;';
 
 en_tete('R&eacute;sultat modification');
@@ -32,61 +33,61 @@ en_tete('R&eacute;sultat modification');
 if (!empty($erreur) ){
 	//erreur
 	echo '<br />Erreur : '.$erreur;
-	echo '<br /><a href="team-add.php?id='.$id_equip.'">Suite</a><br />'.PHP_EOL;
+	echo '<br /><a href="team-add.php?id='.$team_id.'">Suite</a><br />'.PHP_EOL;
 	pied_page();
 	exit();
 }
 
-if ($pdo = connect_db()) {
-	//recupere les anciennes caracteristiques
-	$team = get_team_all_by_id($pdo, $id_equip);
+$pdo = connect_db_or_alert();
 
-	echo $nom." ".$team['nom']."<br />";
-	echo $descr." ".$team['descr']."<br />";
-	echo $compte." ".$team['compte']."<br />";
-	echo $chef." ".$team['chef']."<br />";
+//recupere les anciennes caracteristiques
+$team_selected = get_team_all_by_id($pdo, $team_id);
 
-	//modification equip
-	$modif = 0;
-	//on construit la demande
-	$querry = 'UPDATE LOW_PRIORITY equipe SET ';
-		if ($nom != $team['nom']){
-			//modif du nom
-			$modif = 1;
-			$querry .= "nom='$nom',";
-		}
-		if ($descr != $team['descr']){
-			//modif de la descr
-			$modif = 1;
-			$querry .= "descr='$descr',";
-		}
-		if ($compte != $team['compte']){
-			//modif du compte
-			$modif = 1;
-			$querry .= "compte='$compte',";
-		}
-		if ($chef != $team['chef']){
-			//modif du chef
-			$modif = 1;
-			$querry .= "chef='$chef',";
-		}
-		// supprime la derniere virgule
-		$querry[strlen($querry)-1] = ' ';
-		//ajoute la clause
-		$querry .= " WHERE id='$id_equip'";
-	if ($modif != 0){
-		if ($logged_level >= 3)
-			$stmt = $pdo->prepare($querry);
-			$stmt->execute();
-			$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}//end if modif
-	else {
-		echo 'Aucune modif a faire';
-		echo '<br /><br /><a href="team-list.php?highlight='.$id_equip.'#item'.$id_equip.'">Suite</a><br /><br />';
-		pied_page();
-		exit();
-		} // else end
-	} // end if connect
+echo $nom." ".$team_selected['nom']."<br />";
+echo $descr." ".$team_selected['descr']."<br />";
+echo $compte." ".$team_selected['compte']."<br />";
+echo $chef." ".$team_selected['chef']."<br />";
 
-redirect('team-list.php?highlight='.$id_equip.'#item'.$id_equip);
+//modification equip
+$modif = 0;
+//on construit la demande
+$querry = 'UPDATE LOW_PRIORITY equipe SET ';
+	if ($nom != $team_selected['nom']){
+		//modif du nom
+		$modif = 1;
+		$querry .= "nom='$nom',";
+	}
+	if ($descr != $team_selected['descr']){
+		//modif de la descr
+		$modif = 1;
+		$querry .= "descr='$descr',";
+	}
+	if ($compte != $team_selected['compte']){
+		//modif du compte
+		$modif = 1;
+		$querry .= "compte='$compte',";
+	}
+	if ($chef != $team_selected['chef']){
+		//modif du chef
+		$modif = 1;
+		$querry .= "chef='$chef',";
+	}
+	// supprime la derniere virgule
+	$querry[strlen($querry)-1] = ' ';
+	//ajoute la clause
+	$querry .= " WHERE id='$team_id'";
+if ($modif != 0){
+	if ($logged_level >= 3)
+		$stmt = $pdo->prepare($querry);
+		$stmt->execute();
+		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}//end if modif
+else {
+	echo 'Aucune modif a faire';
+	echo '<br /><br /><a href="team-list.php?highlight='.$team_id.'#item'.$team_id.'">Suite</a><br /><br />';
+	pied_page();
+	exit();
+	} // else end
+
+redirect('team-list.php?highlight='.$team_id.'#item'.$team_id);
 ?>
