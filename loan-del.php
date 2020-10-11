c842dd457d25df634356928fac78798f8e3eb256
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-11 19:45:24 +0200

    All rewrite more clean

--- loan-del.php
+++ loan-del.php
@@ -12,38 +12,36 @@ level_or_alert(3, 'Suppression d\'un pr&ecirc;t');
 $logged_id   = $_SESSION['logged_id'];
 $logged_user = strtolower($_SESSION['logged_user']);
 
-$id_loan = param_post_or_get('id');
+$loan_id = param_post_or_get('id', 0);
 $valid   = param_post('ok', 'no');
 
-if (empty($id_loan) || $valid == 'cancel')
+if ($loan_id == 0 || $valid == 'cancel')
 	redirect('loan-list.php');
 
 if ($valid == 'edit')
-	redirect('loan-add.php?id='.$id_loan);
+	redirect('loan-add.php?id='.$loan_id);
+
+$pdo = connect_db_or_alert();
 
 if ($valid == 'yes') {
-	if ($pdo = connect_db()) {
-		// on supprime le pret
-		$sql = 'DELETE LOW_PRIORITY FROM pret WHERE id = ? LIMIT 1;';
-		// list($qh,$num) = query_db($querry);
-		$stmt = $pdo->prepare($sql);
-		$stmt->execute(array($id_loan));
-	}
-	//on retourne a la page d'accueil
-	redirect('loan-list.php');
+	$flag = del_loan($pdo, $loan_id);
+	if ($flag) // ca a marche
+		redirect('loan-list.php');
+	$message_alert = 'Erreur dans la suppression du pr&ecirc;t : '.$loan_id;
+	include_once('include/alert-data.php');
+	exit;
 	}
 
-$pdo = connect_db();
-$loan = get_loan_all_by_id($pdo, $id_loan);
-$equipment = get_equipment_by_id($pdo, $loan['nom']);
+$loan_selected      = get_loan_all_by_id($pdo, $loan_id);
+$equipment_selected = get_equipment_by_id($pdo, $loan_selected['nom']);
 
 en_tete('Retour d\'un appareil (fin du pr&ecirc;t)');
 ?>
 
 <center class="alert">
 <form action="loan-del.php" method="POST">
-	<input type="hidden" name="id" value="<?php echo $id_loan ?>" >
-	Concernant le pr&ecirc;t <?php echo $id_loan ?> (<?php echo $equipment['nom'] ?>), voulez-vous :
+	<input type="hidden" name="id" value="<?php echo $loan_id ?>" >
+	Concernant le pr&ecirc;t <?php echo $loan_id ?> (<?php echo $equipment_selected['nom'] ?>), voulez-vous :
 	<ul>
 		<li>Modifier / &Eacute;diter le pr&ecirc;t ? <button type="submit" name="ok" value="edit"><?php echo ICON_EDIT ?></button></li>
 		<li>Supprimer le pr&ecirc;t (retour de l'appareil) ?
