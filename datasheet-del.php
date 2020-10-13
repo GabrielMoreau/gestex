72c8292cb9bff2c5d2f7ec8ffe8efab1884a04ab
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-13 23:06:24 +0200

    Rewrite everything from scratch !

--- datasheet-del.php
+++ datasheet-del.php
@@ -2,61 +2,57 @@
 // datasheet-del.php
 $web_page = true;
 
-// Authenticate
+// Module
 require_once('module/auth-functions.php');
 require_once('module/html-functions.php');
 
+// Authenticate
 auth_or_login('equipment-list.php');
 level_or_alert(3, 'Suppression d\'une notice');
 
 $logged_id   = $_SESSION['logged_id'];
 $logged_user = strtolower($_SESSION['logged_user']);
 
-$id_datasheet = $_GET['id'];
-if (empty($_GET['id']))
-	redirect('equipment-list.php');
+$datasheet_id = param_post_or_get('id', 0);
+$valid        = param_post('ok', 'no');
 
-$valid = 'no';
-if (param_get('ok') == 'yes') // si ok dans l'url est 'yes', on valide la suppression
-	$valid = 'yes';
+$equipment_id = 0;
+if ($datasheet_id > 0) {
+	$pdo = connect_db_or_alert();
+
+	$datasheet_selected = get_datasheet_all_by_id($pdo, $datasheet_id);
+	$equipment_id       = $datasheet_selected['id_equipment'];
+}
+
+if ($datasheet_id == 0 || $equipment_id == 0 || $valid == 'cancel') {
+	if ($equipment_id > 0)
+		redirect('equipment-view.php?id='.$equipment_id);
+	redirect('equipment-list.php');
+}
+
+if ($valid == 'yes') {
+	$flag = del_datasheet($pdo, $datasheet_id);
+	if ($flag) // ca a marche
+		redirect('equipment-view.php?id='.$equipment_id);
+	$message_alert = 'Erreur dans la suppression de la notice : '.$datasheet_id;
+	include_once('include/alert-data.php');
+	exit;
+	}
+
+$equipment_selected = get_equipment_by_id($pdo, $datasheet_selected['id_equipment']);
 
 en_tete('Suppression d\'une notice');
-
-if ($valid == 'no') {
-	echo 'Sur de supprimer la notice '.$id_datasheet.' ?<br>';
-	echo '<a href="datasheet-del.php?id='.$id_datasheet.'&ok=yes">OUI</a><br>';
-	echo '<a href="equipment-list.php">NON</a><br>';
-}
-else {
-	if ($pdo = connect_db()) {
-
-		// on supprime la notice
-		$sql = 'SELECT chemin_notice FROM notice WHERE id = ?;';
-		$stmt = $pdo->prepare($sql);
-		$stmt->execute(array($id_datasheet));
-		$datasheet_fetch = $stmt->fetchAll(PDO::FETCH_ASSOC);  // on recupere le chemin de la notice a supprimer
-
-		if (!$datasheet_fetch) { // si ca n'a pas marche
-			echo "<br />Erreur dans la r&eacute;cup&eacute;ration du chemin de la notice : ".$id_datasheet;
-		} else {
-			if(file_exists($datasheet_fetch[0]['chemin_notice'])) {
-				$result = unlink($datasheet_fetch[0]['chemin_notice']);
-				if (!$result) { // si ca n'a pas marche
-					echo "<br />Erreur dans la suppression du fichier avec la notice : ".$id_datasheet;
-				} else {
-					$sql = 'DELETE LOW_PRIORITY FROM notice WHERE id = ? LIMIT 1;';
-					$stmt = $pdo->prepare($sql);
-					$stmt->execute(array($id_datasheet));
-					echo "Notice ".$id_datasheet." supprim&eacute;!<br />";
-				}
-			} else {
-				echo "Erreur: la notice &agrave; supprimer n'existe pas";
-			}
-		}
-	}
-	else // on retourne a la page d'accueil
-		redirect('equipment-list.php');
-}
 ?>
+
+<center class="alert">
+<form action="datasheet-del.php" method="POST">
+	<input type="hidden" name="id" value="<?php echo $datasheet_id ?>" >
+	Voulez-vous supprimer la notice <?php echo $datasheet_selected['pathname'] ?> (<?php echo $equipment_selected['nom'] ?>) ?
+	<button class="red" type="submit" name="ok" value="yes">Oui</button>
+	<button class="green" type="submit" formaction="equipment-view.php?id=<?php echo $equipment_id ?>" value="no">Non</button>
+	<hr>
+	<button type="submit" name="ok" value="cancel">Annuler</button>
+</form>
+</center>
 
 <?php pied_page() ?>
