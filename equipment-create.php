66b9b15fb049ca14e7c8a4ffbe25348d630fcded
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-16 19:12:03 +0200

    * Many update and add update part

--- equipment-create.php
+++ equipment-create.php
@@ -2,12 +2,13 @@
 // equipment-create.php
 $web_page = true;
 
-// Authenticate
+// Module
 require_once('module/auth-functions.php');
 require_once('module/html-functions.php');
 
-auth_or_login('equipment-create.php');
-level_or_alert(3, 'Ajout d\'un appareil');
+// Authenticate
+auth_or_login('equipment-list.php');
+level_or_alert(3, 'Ajout / Modification d\'un appareil');
 
 $logged_level = $_SESSION['logged_level'];
 
@@ -15,45 +16,41 @@ $logged_level = $_SESSION['logged_level'];
 
 unset($erreur);
 
-//variables ne pouvant etre nulles
-$categorie = param_post('categorie');
-if (empty($categorie))
-	$erreur = 'Cat&eacute;gorie non pr&eacute;cis&eacute;';
+$equipment_id = param_post('id_equipment', 0);
+$flag_new = true;
+if ($equipment_id > 0)
+	$flag_new = false;
 
-$nom = param_post('nom');
-if (empty($nom))
-	$erreur = 'Nom de famille non pr&eacute;cis&eacute;';
-
-$modele = param_post('modele');
-if (empty($modele))
-	$erreur = 'Mod&egrave;le non pr&eacute;cis&eacute;';
-
-$feature = param_post('gamme');
-if (empty($feature))
-	$erreur = 'Caract&eacute;ristique non pr&eacute;cis&eacute;';
-
-$equipe = param_post('equipe');
-if (empty($equipe))
-	$erreur = '&Eacute;quipe non pr&eacute;cis&eacute;';
-
-$fourn = param_post('fourn');
-if (empty($fourn))
-	$erreur = 'Fournisseur non pr&eacute;cis&eacute;';
-
-$achat = param_post('achat');
-if (empty($achat))
-	$erreur = 'Achat non pr&eacute;cis&eacute;';
-
-$tech = param_post('tech');
-if (empty($tech))
-	$erreur = 'Tech non pr&eacute;cis&eacute;';
-
-// variables pouvant etre nulles
+$categorie   = param_post('categorie');
+$nom         = param_post('nom');
+$modele      = param_post('modele');
+$feature     = param_post('gamme');
+$equipe      = param_post('equipe');
+$fourn       = param_post('fourn');
+$achat       = param_post('achat');
+$tech        = param_post('tech');
 $reparation  = param_post('reparation');
 $accessoires = param_post('accessoires');
 $inventaire  = param_post('inventaire');
 $barcode     = param_post('barcode');
 $loanable    = param_post('loanable');
+//variables ne pouvant etre nulles
+if (empty($categorie))
+	$erreur = 'Cat&eacute;gorie non pr&eacute;cis&eacute;';
+if (empty($nom))
+	$erreur = 'Nom de l\'appareil non pr&eacute;cis&eacute;';
+if (empty($modele))
+	$erreur = 'Mod&egrave;le non pr&eacute;cis&eacute;';
+if (empty($equipe))
+	$erreur = '&Eacute;quipe non pr&eacute;cis&eacute;';
+if (empty($tech))
+	$erreur = 'Tech non pr&eacute;cis&eacute;';
+if (empty($fourn))
+	$erreur = 'Fournisseur non pr&eacute;cis&eacute;';
+if (empty($achat))
+	$erreur = 'Achat non pr&eacute;cis&eacute;';
+if (empty($feature))
+	$erreur = 'Caract&eacute;ristique non pr&eacute;cis&eacute;';
 
 $notice = '';
 if (isset($_FILES["notice"])) {
@@ -64,32 +61,91 @@ if (isset($_FILES["notice"])) {
 	$notice = str_replace('à', 'a', $notice);
 }
 
-en_tete('R&eacute;sultat ajout appareil');
-
-$cat = $_GET['categorie'];
-//recupere la categorie de la page ajout appareil
-
-if (!empty($erreur) ){
+if (!empty($erreur)) {
 	//erreur
-	echo '<br />Erreur : '.$erreur;
-	echo '<br /><a href="equipment-edit.php">Suite</a><br />';
-	pied_page();
+	$title         = 'Erreur';
+	$action        = 'equipment-edit.php?id='.$equipment_id;
+	$message_text  =  $erreur;
+	$transmit_post = true;
+	include_once('include/warning-box.php');
 	exit();
 }
 
-// tout est ok
-if ($pdo = connect_db()) {
-	list($id_equipment, $err_msg) = set_equipment_new($pdo, $categorie, $nom, $modele, $feature, $equipe, $fourn, $achat, $tech, $reparation, $accessoires, $inventaire, $notice, $barcode, $loanable);
-	if ($err_msg != '' && $logged_level > 3)
-		echo 'Erreur : '. $err_msg.'<br>';
-	if ($notice != '')
-		$id_datasheet = set_datasheet_new($pdo, $id_equipment, 'notice');
-		if (!$id_datasheet)
-			echo 'Erreur : la notice n\'est pas prise en compte car elle n\'est pas un fichier au format PDF !<br>';
-} //end if connect
+$pdo = connect_db_or_alert();
 
-echo '<br>Ajout de '.$nom.' valid&eacute;e';
-echo '<br><br><a href="equipment-list.php?highlight='.$id_equipment.'#item'.$id_equipment.'">Suite</a><br><br>';
+if ($flag_new) { // new
+	list($equipment_id, $err_msg) = set_equipment_new($pdo, $categorie, $nom, $modele, $feature, $equipe, $fourn, $achat, $tech, $reparation, $accessoires, $inventaire, $notice, $barcode, $loanable);
+	if ($err_msg != '') {
+		$message_alert = ($logged_level > 3 ? $err_msg : '');
+		include_once('include/alert-data.php');
+		exit();
+	}
+	if ($notice != '') {
+		$id_datasheet = set_datasheet_new($pdo, $equipment_id, 'notice');
+		if (!$id_datasheet) {
+			$title        = 'Erreur appareil';
+			$action       = 'equipment-view.php?id='.$equipment_id;
+			$message_text = ($logged_level > 3 ? $err_msg : 'Erreur dans l\'ajout d\'une notice &agrave; appareil (pas au format PDF ?)');
+			include_once('include/message-box.php');
+			exit();
+		}
+	}
+
+	$title        = 'R&eacute;sultat ajout d\'un appareil';
+	$action       = 'equipment-view.php?id='.$equipment_id;
+	$message_text = 'Ajout d\'un appareil '.$nom.' valid&eacute;';
+	include_once('include/message-box.php');
+	exit();
+}
+
+// modify
+// recupere les anciennes caracteristiques
+$equipment_selected = get_equipment_all_by_id($pdo, $equipment_id);
+
+//modification app
+$modif = false;
+if (   ($categorie   != $equipment_selected['categorie'])
+	|| ($nom         != $equipment_selected['nom'])
+	|| ($modele      != $equipment_selected['modele'])
+	|| ($feature     != $equipment_selected['gamme'])
+	|| ($tech        != $equipment_selected['responsable'])
+	|| ($equipe      != $equipment_selected['equipe'])
+	|| ($fourn       != $equipment_selected['fournisseur'])
+	|| ($achat       != $equipment_selected['achat'])
+	|| ($reparation  != $equipment_selected['reparation'])
+	|| ($accessoires != $equipment_selected['accessoires'])
+	|| ($inventaire  != $equipment_selected['inventaire'])
+	|| ($notice      != $equipment_selected['notice'])
+	|| ($barcode     != $equipment_selected['barcode'])
+	|| ($loanable    != $equipment_selected['loanable']))
+	$modif = true;
+
+if ($modif) {
+	$err_msg = set_equipment_update($pdo, $equipment_id, $categorie, $nom, $modele, $feature, $equipe, $fourn, $achat, $tech, $reparation, $accessoires, $inventaire, $notice, $barcode, $loanable);
+	if ($err_msg != '') {
+		$title        = 'Erreur appareil';
+		$action       = 'equipment-view.php?id='.$equipment_id;
+		$message_text = ($logged_level > 3 ? $err_msg : 'Erreur dans la mise &agrave; jour de la fiche appareil');
+		include_once('include/message-box.php');
+		exit();
+	}
+	if ($notice != '') {
+		$id_datasheet = set_datasheet_new($pdo, $equipment_id, 'notice');
+		if (!$id_datasheet) {
+			$title        = 'Erreur appareil';
+			$action       = 'equipment-view.php?id='.$equipment_id;
+			$message_text = ($logged_level > 3 ? $err_msg : 'Erreur dans l\'ajout d\'une notice &agrave; appareil (pas au format PDF ?)');
+			include_once('include/message-box.php');
+			exit();
+		}
+	}
+
+	redirect('equipment-view.php?id='.$equipment_id);
+}
+
+$title        = 'Modification appareil';
+$action       = 'equipment-view.php?id='.$equipment_id;
+$message_text = 'Aucune modification &agrave; faire';
+include_once('include/message-box.php');
+exit();
 ?>
-
-<?php pied_page() ?>
