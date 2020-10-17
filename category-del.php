986c740d60f55438da68145a50e1e396306941d7
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-17 13:44:51 +0200

    Rename var and remove echo, use message box

--- category-del.php
+++ category-del.php
@@ -10,25 +10,28 @@ require_once('module/base-functions.php');
 auth_or_login('category-list.php');
 level_or_alert(3, 'Suppression d\'une cat&eacute;gorie');
 
-$id_category = param_post_or_get('id');
+$category_id = param_post_or_get('id');
 $valid       = param_post('ok', 'no');
 
-if (empty($id_category) || $valid == 'cancel')
+if (empty($category_id) || $valid == 'cancel')
 	redirect('category-list.php');
 
 $pdo = connect_db();
-$category_name = get_category_by_id($pdo, $id_category)['nom'];
+$category_name = get_category_by_id($pdo, $category_id)['nom'];
 
 if ($valid == 'yes') {
 	// on supprime la categorie
-	$result = del_category_by_id($pdo, $id_category);
-	if (!$result) { // si ca n'a pas marche
-		echo "<br />Erreur dans la suppression de la cat&eacute;gorie : ".$id_category;
-	}
-	//on retourne a la page precedente
-	redirect('category-list.php');
-}
+	$flag = del_category_by_id($pdo, $category_id);
+	if ($flag) // ca a marche
+		redirect('category-list.php');
 
-// $id_category $category_name
+	redirect('category-list.php');
+	$message_alert = 'Erreur dans la suppression de la cat&eacute;gorie : '.$category_id;
+	include_once('include/alert-data.php');
+	exit;
+	}
+
+
+// $category_id $category_name
 include_once('include/category-del.php');
 ?>
--- include/category-del.php
+++ include/category-del.php
@@ -1,10 +1,15 @@
 <?php if (!$web_page) exit() ?>
 
+<?php
+// $category_name
+// $category_id
+?>
+
 <?php en_tete('Suppression de la cat&eacute;gorie <i>'.$category_name.'</i>') ?>
 
 <center class="box-alert">
 <form action="category-del.php" method="POST">
-	<input type="hidden" name="id" value="<?=$id_category?>">
+	<input type="hidden" name="id" value="<?=$category_id?>">
 	Voulez-vous supprimer la cat&eacute;gorie <i><?=$category_name?></i> ?
 	<button class="red" type="submit" name="ok" value="yes">Oui</button>
 	<button class="green" type="submit" formaction="category-list.php" value="no">Non</button>
