5945cfd170ab945e584c52c9ff786dc7baf16657
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-17 22:00:32 +0200

    Small error on super-admin... And update manu var names

--- user-edit.php
+++ user-edit.php
@@ -13,31 +13,32 @@ $logged_id    = $_SESSION['logged_id'];
 $logged_user  = strtolower($_SESSION['logged_user']);
 $logged_level = $_SESSION['logged_level'];
 
-if (empty($_GET['id'])) {
+$user_id = param_post_or_get('id', 0);
+if ($user_id == 0)) {
 	//->nouvelle inscription
 	$mode       = 'Ajouter';
 	$action     = 'user-create.php';
-	$user2ch_id = '';
 } else {
 	//->modif coordonnees
 	$mode       = 'Modifier';
 	$action     = 'user-update.php';
-	$user2ch_id = $_GET['id'];
 }
 
-if ($pdo = connect_db()) {
-	if ($mode == 'Ajouter') {
-		en_tete('Inscrire un utilisateur');
-	} else if ($mode == 'Modifier') {
-		en_tete('Modifier mon profil');
-		// recupere le user concerne
-		$data = get_user_all_by_id($pdo, $user2ch_id);
-	}
+$pdo = connect_db_or_alert();
+
+$user_selected = [];
+if ($mode == 'Ajouter') {
+	en_tete('Inscrire un utilisateur');
+} else if ($mode == 'Modifier') {
+	en_tete('Modifier mon profil');
+	// recupere le user concerne
+	$user_selected = get_user_all_by_id($pdo, $user_id);
+}
 ?>
 
 <div class="form">
 <form action="<?php echo $action ?>" method="POST"  class="form" name="inscrForm">
-	<input type="hidden" name="user2ch_id"  value="<?php echo $user2ch_id ?>">
+	<input type="hidden" name="user2ch_id"  value="<?php echo $user_id ?>">
 <table>
 	<tbody>
 		<tr>
@@ -53,7 +54,7 @@ if ($pdo = connect_db()) {
 				Identifiant (login)
 			</th>
 			<td>
-				<b><?php echo $data['loggin'] ?></b>
+				<b><?php echo $user_selected['loggin'] ?></b>
 			</td>
 			<?php } ?>
 		</tr>
@@ -82,7 +83,7 @@ if ($pdo = connect_db()) {
 				Nom de famille *
 			</th>
 			<td>
-				<input type="text" name="nom" size="30" maxlength="30" value="<?php if ($mode == 'Modifier') echo $data['nom'] ?>" placeholder="Nom de famille *">
+				<input type="text" name="nom" size="30" maxlength="30" value="<?php if ($mode == 'Modifier') echo $user_selected['nom'] ?>" placeholder="Nom de famille *">
 			</td>
 		</tr>
 		<tr>
@@ -90,7 +91,7 @@ if ($pdo = connect_db()) {
 				Pr&eacute;nom
 			</th>
 			<td>
-				<input type="text" name="prenom" size="30" maxlength="30" value="<?php if ($mode == 'Modifier') echo $data['prenom'] ?>" placeholder="Pr&eacute;nom">
+				<input type="text" name="prenom" size="30" maxlength="30" value="<?php if ($mode == 'Modifier') echo $user_selected['prenom'] ?>" placeholder="Pr&eacute;nom">
 			</td>
 		</tr>
 		<tr>
@@ -98,7 +99,7 @@ if ($pdo = connect_db()) {
 				Adresse courriel *
 			</th>
 			<td>
-				<input type="text" name="addr_mail" size="30" maxlength="50" value="<?php if ($mode == 'Modifier') echo $data['email'] ?>" placeholder="Adresse courriel *">
+				<input type="text" name="addr_mail" size="30" maxlength="50" value="<?php if ($mode == 'Modifier') echo $user_selected['email'] ?>" placeholder="Adresse courriel *">
 			</td>
 		</tr>
 		<tr>
@@ -106,7 +107,7 @@ if ($pdo = connect_db()) {
 				T&eacute;l&eacute;phone
 			</th>
 			<td>
-				<input type="text" name="phone" size="10" maxlength="10" value="<?php if ($mode=='Modifier') echo $data['tel'] ?>" placeholder="T&eacute;l&eacute;phone">
+				<input type="text" name="phone" size="10" maxlength="10" value="<?php if ($mode=='Modifier') echo $user_selected['tel'] ?>" placeholder="T&eacute;l&eacute;phone">
 			</td>
 		</tr>
 		<tr>
@@ -114,19 +115,19 @@ if ($pdo = connect_db()) {
 				&Eacute;quipe
 			</th>
 			<?php // recupere la liste des equipes
-			$equipe_fetch = get_team_listshort($pdo);
+			$team_fetch = get_team_listshort($pdo);
 			?>
 			<td>
 				<select  name="equipe">
 					<?php
-					foreach ($equipe_fetch as $equipe) {
-						echo '<option value="'.$equipe['id'].'"';
+					foreach ($team_fetch as $team_current) {
+						echo '<option value="'.$team_current['id'].'"';
 						// selectionne la bonne equipe
 						if($mode == 'Modifier'){
-							if ($equipe['id'] == $data['equipe'])
+							if ($team_current['id'] == $user_selected['equipe'])
 								echo ' selected';
 						}
-						echo '>'.$equipe['nom'].'</option>';
+						echo '>'.$team_current['nom'].'</option>';
 					} // end foreach ?>
 				</select>
 				<span class="option-right"><a href="team-edit.php"><?php echo ICON_ADD_TEAM ?></a></span>
@@ -138,21 +139,21 @@ if ($pdo = connect_db()) {
 			</th>
 			<td>
 				<?php  if ($logged_level >= 3 || !isset($logged_level)) { // admin loggue ou premiere inscription: modif possible ?>
-				<input type="radio" name="level" value="0" <?php if ($mode === 'Modifier' && $data['level'] == 0) echo 'checked="checked"' ?> >&Eacute;tudiant<br>
-				<input type="radio" name="level" value="1" <?php if ($mode === 'Modifier' && $data['level'] == 1) echo 'checked="checked"' ?> >Chercheur<br>
-				<input type="radio" name="level" value="2" <?php if ($mode === 'Modifier' && $data['level'] == 2) echo 'checked="checked"' ?> >ITA<br>
+				<input type="radio" name="level" value="0" <?php if ($mode === 'Modifier' && $user_selected['level'] == 0) echo 'checked="checked"' ?> >&Eacute;tudiant<br>
+				<input type="radio" name="level" value="1" <?php if ($mode === 'Modifier' && $user_selected['level'] == 1) echo 'checked="checked"' ?> >Chercheur<br>
+				<input type="radio" name="level" value="2" <?php if ($mode === 'Modifier' && $user_selected['level'] == 2) echo 'checked="checked"' ?> >ITA<br>
 				<?php } ?>
 				
 				<?php if (isset($logged_level) && $logged_level >= 3) { ?>
-				<input type="radio" name="level" value="3" <?php if ($mode === 'Modifier' && $data['level'] >= 3) echo 'checked="checked"' ?> >Admin<br>
+				<input type="radio" name="level" value="3" <?php if ($mode === 'Modifier' && $user_selected['level'] == 3) echo 'checked="checked"' ?> >Admin<br>
 				<?php } ?>
 
 				<?php if (isset($logged_level) && $logged_level >= 4) { ?>
-				<input type="radio" name="level" value="4" <?php if ($mode === 'Modifier' && $data['level'] >= 3) echo 'checked="checked"' ?> >SuperAdmin<br>
+				<input type="radio" name="level" value="4" <?php if ($mode === 'Modifier' && $user_selected['level'] == 4) echo 'checked="checked"' ?> >SuperAdmin<br>
 				<?php } ?>
 				
 				<?php if (isset($logged_level) && ($logged_level < 3)) { // consultation seulement
-					switch ($data['level']) {
+					switch ($user_selected['level']) {
 						case 0: echo "&Eacute;tudiant"; break;
 						case 1: echo "Chercheur"; break;
 						case 2: echo "ITA"; break;
@@ -167,10 +168,10 @@ if ($pdo = connect_db()) {
 				Th&egrave;me
 			</th>
 			<td>
-				<input type="radio" name="theme" value="random" <?php if ($mode === 'Modifier' && $data['theme'] == 'random') echo 'checked' ?> >Al&eacute;atoire<br>
-				<input type="radio" name="theme" value="clair" <?php if ($mode === 'Modifier' && $data['theme'] == 'clair') echo 'checked' ?> >Clair<br>
-				<input type="radio" name="theme" value="sombre" <?php if ($mode === 'Modifier' && $data['theme'] == 'sombre') echo 'checked' ?>>Sombre<br>
-				<input type="radio" name="theme" value="solarizeddark" <?php if ($mode === 'Modifier' && $data['theme'] == 'solarizeddark') echo 'checked' ?>>Solarized-Dark<br>
+				<input type="radio" name="theme" value="random" <?php if ($mode === 'Modifier' && $user_selected['theme'] == 'random') echo 'checked' ?> >Al&eacute;atoire<br>
+				<input type="radio" name="theme" value="clair" <?php if ($mode === 'Modifier' && $user_selected['theme'] == 'clair') echo 'checked' ?> >Clair<br>
+				<input type="radio" name="theme" value="sombre" <?php if ($mode === 'Modifier' && $user_selected['theme'] == 'sombre') echo 'checked' ?>>Sombre<br>
+				<input type="radio" name="theme" value="solarizeddark" <?php if ($mode === 'Modifier' && $user_selected['theme'] == 'solarizeddark') echo 'checked' ?>>Solarized-Dark<br>
 			</td>
 		</tr>
 		<tr>
@@ -185,7 +186,5 @@ if ($pdo = connect_db()) {
 </table>
 </form>	
 </div>
-
-<?php } else { redirect('user-list.php'); } ?>
 
 <?php pied_page() ?>
