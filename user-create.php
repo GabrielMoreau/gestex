02d8db94aa23b4b0e76dd200149adf833f4504df
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-22 23:54:04 +0200

    Update code, remove echo, more robust

--- user-create.php
+++ user-create.php
@@ -12,49 +12,36 @@ level_or_alert(3, 'Validation d\'un utilisateur');
 
 //validation d'un nouvel utilisateur
 
-unset($erreur);
-unset($loggin);
-unset($password);
-unset($password2);
-unset($nom);
+$user_id   = param_post('id', 0); // -> modify
+$flag_new = true;
+if ($user_id > 0)
+	$flag_new = false;
 
-//variables ne pouvant etre nulles
-if (empty($_POST['loggin']))
+$loggin    = param_post('loggin');    // *
+$password  = param_post('password');  // *
+$password2 = param_post('password2'); // *
+$nom       = param_post('nom');       // *
+$level     = param_post('level');     // *
+$theme     = param_post('theme');     // *
+$mail      = param_post('addr_mail');
+$prenom    = param_post('prenom');
+$phone     = param_post('phone');
+$equipe    = param_post('equipe');
+
+if (empty($loggin))
 	$erreur = 'Identifiant (login) non pr&eacute;cis&eacute;';
-
-$loggin = $_POST['loggin'];
-if (empty($_POST['password']))
+if (empty($password))
 	$erreur = 'Password non pr&eacute;cis&eacute;';
-
-$password = $_POST['password'];
-if (empty($_POST['password2']))
+if (empty($password2))
 	$erreur = 'Confirmation de password non pr&eacute;cis&eacute;';
-
-$password2 = $_POST['password2'];
 if ($password != $password2)
 	$erreur = 'Les passwords diff&egrave;rent';
-
-$nom = $_POST['nom'];
-if (empty($_POST['nom']))
+if (empty($nom))
 	$erreur = 'Nom de famille non pr&eacute;cis&eacute;';
-
-$level = $_POST['level'];
-if (empty($_POST['level']))
+if (empty($level))
 	$erreur = 'Qualit&eacute; non pr&eacute;cis&eacute;';
-
-if (empty($_POST['theme']))
+if (empty($theme))
 	$erreur = 'Th&egrave;me non pr&eacute;cis&eacute;';
-else
-	$theme = $_POST['theme'];
-
-$mail = $_POST['addr_mail'];
-
-//variables pouvant etre nulles
-$prenom = $_POST['prenom'];
-$phone  = $_POST['phone'];
-$equipe = $_POST['equipe'];
-
-en_tete('R&eacute;sultat inscription');
 
 $pdo = connect_db_or_alert();
 
@@ -64,23 +51,38 @@ if (check_val_in_db($pdo, 'users', 'loggin', $loggin)) {
 }
 
 if (!empty($erreur)) {
-	//erreur
-	echo '<br /><b>Erreur de saisie : </b>'.$erreur;
-	echo '<br /><center><a href="user-edit.php?'.$loggin.'">Suite</a></center><br />';
+	// erreur
+	$title         = 'Erreur utilisateur';
+	$action        = 'user-edit.php?id='.$user_id;
+	$highlight     = $user_id;
+	$message_text  = $erreur;
+	$transmit_post = true;
+	include_once('include/warning-box.php');
+	exit();
 }
-else {
-	/// tout est ok
+
+if ($flag_new) { // new
 	$mot_crypte = md5($password);
 	list($user_id, $err_msg) = set_user_new($pdo, $nom, $prenom, $loggin, $mot_crypte, $mail, $level, $phone, $equipe, $theme);
 
+	if ($err_msg != '') {
+		$title        = 'Erreur utilisateur';
+		$action       = 'user-list.php';
+		$message_text = ($logged_level > 3 ? $err_msg : 'Erreur dans la cr&eacute;ation de l\'utilisateur');
+		include_once('include/message-box.php');
+		exit();
+	}
+
 	// inscription enregistree mais pas encore validee !
 	// envoi d'un courriel a l'admin
-	$texte = 'Inscription de '.$prenom.' '.$nom;
-	mail(GESTEX_ADMIN_MAIL, "[GestEx] ajout utilisateur - ".$nom." ".$prenom, $texte);
+	// $texte = 'Inscription de '.$prenom.' '.$nom;
+	// mail(GESTEX_ADMIN_MAIL, '[GestEx] ajout utilisateur - '.$nom.'  '.$prenom, $texte);
 
-	echo 'Ajout de '.$prenom.' '.$nom.' valid&eacute;<br />';
-	echo '<br /><center><a href="user-list.php">Suite</a></center><br /><br />';
-} // else end
-
-pied_page();
+	$title        = 'Ajout utilisateur';
+	$action       = 'user-list.php?highlight='.$user_id;
+	$highlight    = $user_id;
+	$message_text = 'Ajout de l\'utilisateur '.$nom.' '.$prenom' valid&eacute;e';
+	include_once('include/message-box.php');
+	exit();
+}
 ?>
