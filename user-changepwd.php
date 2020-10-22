81e588c9321a2e23f5a68afda6d289023afbb7a0
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-22 23:13:45 +0200

    Replace SELECT and UPDATE by function

--- user-changepwd.php
+++ user-changepwd.php
@@ -10,9 +10,9 @@ require_once('module/html-functions.php');
 auth_or_login('index.php');
 level_or_alert(1, 'Modification du mot de passe');
 
-$logged_id        = $_SESSION['logged_id'];
-$logged_user = strtolower($_SESSION['logged_user']);
-$logged_level     = $_SESSION['logged_level'];
+$logged_id    = $_SESSION['logged_id'];
+$logged_user  = strtolower($_SESSION['logged_user']);
+$logged_level = $_SESSION['logged_level'];
 
 $errormsg = '';
 
@@ -20,7 +20,7 @@ if (!empty($_GET['id'] )) {
 	$user2chg = $_GET['id'];
 }
 
-if ($pdo = connect_db()) {
+$pdo = connect_db_or_alert();
 
 unset($passwd1);
 unset($passwd2);
@@ -37,11 +37,7 @@ if (!empty($_POST['passwd2']))
 if (!empty($_POST['old_pass']))
 	$old_pass = $_POST['old_pass'];
 
-	// recupere l'ancien pasword et  le nom
-	$sql = 'SELECT nom, prenom, password FROM users WHERE id = ?';
-	$stmt = $pdo->prepare($sql);
-	$stmt->execute(array($user2chg));
-	$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$user_selected = get_user_all_by_id($pdo, $user2chg);
 
 if (isset($passwd1) && isset($passwd2)){
 	// check that passwords match
@@ -49,29 +45,26 @@ if (isset($passwd1) && isset($passwd2)){
  		 $errormsg = 'Passwords do not match, please try again';
 
 	if (!isset($errormsg) && isset($old_pass) && $logged_level < 3) {
-		if(md5($old_pass) != $user[0]['password'])
+		if(md5($old_pass) != $user_selected['password'])
 			$errormsg = 'Wrong password, sorry!';
 	}
 	echo $errormsg;
 	if ($errormsg == '') {
 		$mot_crypte = md5($passwd1);
 		// ok on change
-		$sql = 'UPDATE users SET password = ? WHERE id = ?;';
-		$stmt = $pdo->prepare($sql);
-		$stmt->execute(array($mot_crypte, $user2chg));
+		set_user_password_by_id($pdo, $user2chg, $mot_crypte);
 		redirect('user-list.php');
 	}
 } // end if isset
-} // end pdo
 
-en_tete('Changement de mot de passe pour <i>'.$user[0]['nom'].' '.$user[0]['prenom'].'</i>');
+en_tete('Changement de mot de passe pour <i>'.$user_selected['nom'].' '.$user_selected['prenom'].'</i>');
 
 if (!empty($_GET['id'])) {
 ?>
 
 <div class="auth">
 	<form action="user-changepwd.php" method="post">
-		<input type="hidden" name="user" value="<?php echo $user2chg; ?>">
+		<input type="hidden" name="user" value="<?php echo $user2chg ?>">
 		<table>
 			<tbody>
 				<?php if ($logged_level < 3) { ?>
