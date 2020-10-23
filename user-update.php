2b68b31e0d01a0554bda368e295d3123b74b301d
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2020-10-23 09:44:16 +0200

    Update code to use function

--- user-update.php
+++ user-update.php
@@ -14,26 +14,22 @@ $logged_id    = $_SESSION['logged_id'];
 $logged_user  = strtolower($_SESSION['logged_user']);
 $logged_level = $_SESSION['logged_level'];
 
-//modification d'un utilisateur
+// modification d'un utilisateur
 unset($erreur);
 
-//variables ne pouvant etre nulles
 $user2ch_id = param_post('user2ch_id'); // *
 $nom        = param_post('nom');        // *
 $mail       = param_post('addr_mail');  // *
-//variables pouvant etre nulles
-$prenom = param_post('prenom');
-$phone  = param_post('phone');
-$equipe = param_post('equipe');
-$level  = param_post('level');
-$theme  = param_post('theme');
+$prenom     = param_post('prenom');
+$phone      = param_post('phone');
+$equipe     = param_post('equipe');
+$level      = param_post('level');
+$theme      = param_post('theme');
 
 if (empty($user2ch_id))
 	$erreur = 'Identifiant utilisateur non pr&eacute;cis&eacute;';
-
 if (empty($nom))
 	$erreur = 'Nom de famille non pr&eacute;cis&eacute;';
-
 if (empty($mail))
 	$erreur = 'Adresse de courriel non pr&eacute;cis&eacute;';
 
@@ -50,37 +46,35 @@ $pdo = connect_db_or_alert();
 
 $user_selected = get_user_all_by_id($pdo, $user2ch_id);
 
+if ($level != $user_selected['level'] && $logged_level < 3)
+	$level = $user_selected['level'];
+
 //modif inscription
 //on construit la demande
-$querry = "UPDATE LOW_PRIORITY users SET ";
-if ($nom != $user_selected['nom'])
-	$querry .= "nom='$nom',";
-if ($prenom != $user_selected['prenom'])
-	$querry .= " prenom='$prenom',";
-if ($logged_level >= 3) {
-	if ($level != $user_selected['level'])
-		$querry .= " level='$level',";
-	if ($user_selected['valid'] == 0) {
-		//validation du user
-		$querry .= " valid=1,";
-		$valid = 1;
+$modif = false;
+if (   ($nom    != $user_selected['nom'])
+	|| ($prenom != $user_selected['prenom'])
+	|| ($mail   != $user_selected['mail'])
+	|| ($level  != $user_selected['level'])
+	|| ($phone  != $user_selected['tel'])
+	|| ($equipe != $user_selected['equipe'])
+	|| ($theme  != $user_selected['theme']))
+	$modif = true;
+
+if ($modif) {
+	$err_msg = set_user_update($pdo, $user2ch_id, $nom, $prenom, $mail, $level, $phone, $equipe, $theme) {
+	if ($err_msg != '') {
+		$title        = 'Erreur utilisateur';
+		$action       = 'user-list.php?highlight='.$user2ch_id;
+		$highlight    = $user2ch_id;
+		$message_text = ($logged_level > 3 ? $err_msg : 'Erreur dans la mise &agrave; jour de l\'utilisateur');
+		include_once('include/message-box.php');
+		exit();
 	}
+
+	redirect('user-list.php?highlight='.$user2ch_id.'#item'.$user2ch_id);
 }
-if ($phone != $user_selected['tel'])
-	$querry.=" tel='$phone',";
-if ($mail != $user_selected['mail'])
-	$querry.=" email='$mail',";
-if ($equipe != $user_selected['equipe'])
-	$querry .=" equipe='$equipe',";
-$querry .= " theme='$theme',";
-// supprime la derniere virgule
-$querry[strlen($querry)-1] = ' ';
-//ajoute la clause
-$querry.= " WHERE id='$user2ch_id'";
-if ($logged_level >= 3)
-	echo $querry;
-$stmt = $pdo->prepare($querry);
-$stmt->execute();
+
 
 if ($user2ch_id == $logged_id)
 	$_SESSION['logged_theme'] = theme($theme);
@@ -88,7 +82,7 @@ if ($user2ch_id == $logged_id)
 if ($logged_level >= 3 && $valid == 1) {
 	//validation d'un user acceptee
 	//envoi d'un mail a cet user
-	$texte = $prenom.' '.$nom.' votre inscription au systeme GestEx &agrave; &eacute;t&eacute; accept&eacute;e !';
+	//$texte = $prenom.' '.$nom.' votre inscription au systeme GestEx &agrave; &eacute;t&eacute; accept&eacute;e !';
 	// mail($mail, "[GestEx] inscription accept&eacute;e - ".$nom." ".$prenom, $texte);
 }
 
