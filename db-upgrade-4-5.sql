e755ef67478803f17d03fe046c2fbbda49fcb694
Estéban Ristich <esteban.ristich@univ-grenoble-alpes.fr>
2021-06-25 11:29:16 +0200

    Fix loan default

--- db-upgrade-4-5.sql
+++ db-upgrade-4-5.sql
@@ -12,8 +12,8 @@ ALTER TABLE `listing` MODIFY COLUMN `barcode` BIGINT DEFAULT NULL;
 ALTER TABLE `version` ADD COLUMN `updated_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 -- TIMESTAMP =< MySQL 5.5.x < DATETIME
 
-ALTER TABLE `pret` ADD COLUMN `state` ENUM('booking', 'loan', 'undefined', 'old') NOT NULL;
-UPDATE `pret` SET `state` = 3;
+ALTER TABLE `pret` ADD COLUMN `state` ENUM('BOOKING', 'LOAN', 'OLD') NOT NULL;
+UPDATE `pret` SET `state` = 2;
 
 --
 -- Fix global DB version
