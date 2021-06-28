222b0b60d519567593d9b78935e37ab6ec9bd16d
Estéban Ristich <esteban.ristich@univ-grenoble-alpes.fr>
2021-06-28 12:19:46 +0200

    Update to set loan returned and not deleted

--- loan-del.php
+++ loan-del.php
@@ -22,7 +22,8 @@ if ($valid == 'edit')
 $pdo = connect_db_or_alert();
 
 if ($valid == 'yes') {
-	$iostat = del_loan_by_id($pdo, $loan_id);
+	/* $iostat = del_loan_by_id($pdo, $loan_id); */
+	$iostat = set_loan_to_returned($pdo, $loan_id);
 	if ($iostat) // ca a marche
 		redirect('loan-list.php');
 	$message_alert = 'Erreur dans la suppression du pr&ecirc;t : '.$loan_id;
