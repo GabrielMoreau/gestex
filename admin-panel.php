81611ef4521bb31f3886aaedb3cd10815c51f8a1
Estéban Ristich <esteban.ristich@univ-grenoble-alpes.fr>
2021-07-02 13:50:44 +0200

    Add file admin-panel.php

---
+++ admin-panel.php
@@ -0,0 +1,28 @@
+<?php
+// category-list.php
+$web_page = true;
+
+// Authenticate
+require_once('module/auth-functions.php');
+require_once('module/html-functions.php');
+require_once('module/base-functions.php');
+
+session_start();
+if (empty($_SESSION['logged_user'])) {
+	$logged_level = 0;
+} else {
+	$logged_id    = $_SESSION['logged_id'];
+	$logged_user  = strtolower($_SESSION['logged_user']);
+	$logged_level = $_SESSION['logged_level'];
+}
+
+en_tete('Console Administrateur');
+
+if ($logged_level < 4) {
+    echo 'Permission denied !';
+    exit();
+}
+
+echo 'console admin';
+
+?>
