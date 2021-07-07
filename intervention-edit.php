26a54c6938977ee0eca9d623aeb686fc06b97727
Estéban Ristich <esteban.ristich@univ-grenoble-alpes.fr>
2021-07-07 10:37:16 +0200

    Update intervention form

--- intervention-edit.php
+++ intervention-edit.php
@@ -24,23 +24,73 @@ if ($mode == 'Ajouter') {
 }
 ?>
 
+
+<div class="form">
+<form action="team-process.php" method="POST" name="inscrForm">
+	<input type="hidden" name="id_equip" value="<?php if( $mode=='Modifier'){ echo $team_id; }?>" >
 <table>
     <tbody>
         <tr>
             <th>Description</th>
-            <td></td>
+            <td>
+                <textarea name="description" rows="4" cols="33">Description de l'intervention...
+                </textarea>
+            </td>
         </tr>
         <tr>
-            <th></th>
-            <td></td>
+            <th>Société</th>
+            <?php // recupere la liste des equipes
+			$company_fetch = get_supplier_listshort($pdo);
+			?>
+			<td>
+				<select  name="company">
+					<?php
+					foreach ($company_fetch as $company_current) {
+						echo '<option value="'.$company_current['id'].'"';
+						// selectionne la bonne equipe
+/* 						if ($company_current['id'] == param_post_key('company', $user_selected, 0))
+							echo ' selected'; */
+						echo '>'.$company_current['nom'].'</option>';
+					} // end foreach
+					?>
+				</select>
+				<span class="option-right"><a href="team-edit.php"><?php echo ICON_ADD_TEAM ?></a></span>
+			</td>
         </tr>
         <tr>
-            <th></th>
-            <td></td>
-        </tr>
+            <th>Fiche d'interevention</th>
+            <td>
+				<?php if ($mode == 'Modifier' && $datasheet_count > 0) { ?>
+				<ul>
+					<?php foreach ($datasheet_fetch as $datasheet_current) { ?>
+					<li><a href="<?php echo $datasheet_path.'/'.$datasheet_current['pathname'] ?>" target="_top"><?php echo $datasheet_current['description'] ?> (<?php echo $datasheet_current['pathname']?>)</a></li>
+					<?php } ?>
+				</ul>
+				<?php } ?>
+				<input type="file" name="notice" value="<?= param_post_key('notice', $equipment_selected) ?>" placeholder="Notice (facultatif)">
+			</td>
         <tr>
+            <td></td>
+			<th class="button">
+				<input type="submit" name="Login" value="<?php echo $mode ?>">
+			</th>
+		</tr>
             <th></th>
             <td></td>
         </tr>
     </tbody>
+    <tbody>
+		<tr>
+			<td colspan="2" class="button">
+				<?php if ($mode == 'Modifier') { ?>
+				<input class="cancel" type="submit" name="ok" formaction="equipment-view.php?id=<?php echo $equipment_id ?>" value="Annuler">
+				<?php } else { ?>
+				<input class="cancel" type="submit" name="ok" formaction="equipment-list.php" value="Annuler">
+				<?php } ?>
+			</td>
+		</tr>
+        </form>
+    </tbody>
 </table>
+</form>
+</div>
