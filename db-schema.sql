ab209dc49faf70e684c0098c6478bb0f4f723859
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2021-07-15 14:09:18 +0200

    var type in uppercase

--- db-schema.sql
+++ db-schema.sql
@@ -5,21 +5,21 @@
 
 DROP TABLE IF EXISTS `Listing`;
 CREATE TABLE `Listing` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `categorie` varchar(255) DEFAULT NULL,
-  `nom` varchar(255) DEFAULT NULL,
-  `modele` varchar(255) DEFAULT NULL,
-  `gamme` varchar(255) DEFAULT NULL,
-  `equipe` int(11) DEFAULT NULL,
-  `fournisseur` int(11) DEFAULT NULL,
-  `achat` date DEFAULT NULL,
-  `responsable` int(11) DEFAULT NULL,
-  `reparation` varchar(30) DEFAULT NULL,
-  `accessoires` varchar(255) DEFAULT NULL,
-  `notice` varchar(255) DEFAULT NULL,
-  `inventaire` varchar(50) DEFAULT NULL,
-  `loanable` boolean NOT NULL DEFAULT false,
-  `barcode` int(11) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `categorie` VARCHAR(255) DEFAULT NULL,
+  `nom` VARCHAR(255) DEFAULT NULL,
+  `modele` VARCHAR(255) DEFAULT NULL,
+  `gamme` VARCHAR(255) DEFAULT NULL,
+  `equipe` INT(11) DEFAULT NULL,
+  `fournisseur` INT(11) DEFAULT NULL,
+  `achat` DATE DEFAULT NULL,
+  `responsable` INT(11) DEFAULT NULL,
+  `reparation` VARCHAR(30) DEFAULT NULL,
+  `accessoires` VARCHAR(255) DEFAULT NULL,
+  `notice` VARCHAR(255) DEFAULT NULL,
+  `inventaire` VARCHAR(50) DEFAULT NULL,
+  `loanable` BOOLEAN NOT NULL DEFAULT FALSE,
+  `barcode` INT(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`equipe`) REFERENCES `equipe` (`id`);
   FOREIGN KEY (`fournisseur`) REFERENCES `fournisseurs` (`id`);
@@ -34,14 +34,14 @@ CREATE TABLE `Listing` (
 
 DROP TABLE IF EXISTS `appareils`;
 CREATE TABLE `appareils` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(30) NOT NULL DEFAULT '',
-  `descr` varchar(255) DEFAULT NULL,
-  `equipe` int(11) NOT NULL DEFAULT '0',
-  `tech` int(11) NOT NULL DEFAULT '0',
-  `fournisseur` int(11) NOT NULL DEFAULT '0',
-  `achat` date DEFAULT NULL,
-  `facture` varchar(30) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(30) NOT NULL DEFAULT '',
+  `descr` VARCHAR(255) DEFAULT NULL,
+  `equipe` INT(11) NOT NULL DEFAULT '0',
+  `tech` INT(11) NOT NULL DEFAULT '0',
+  `fournisseur` INT(11) NOT NULL DEFAULT '0',
+  `achat` DATE DEFAULT NULL,
+  `facture` VARCHAR(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -52,8 +52,8 @@ CREATE TABLE `appareils` (
 
 DROP TABLE IF EXISTS `categorie`;
 CREATE TABLE `categorie` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(50) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -64,14 +64,14 @@ CREATE TABLE `categorie` (
 
 DROP TABLE IF EXISTS `demandes`;
 CREATE TABLE `demandes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tache` varchar(200) DEFAULT NULL,
-  `nomdemandeur` varchar(50) DEFAULT NULL,
-  `details` text,
-  `achat` date DEFAULT NULL,
-  `avancement` varchar(255) DEFAULT NULL,
-  `termine` varchar(15) DEFAULT NULL,
-  `piecesjointes` varchar(30) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `tache` VARCHAR(200) DEFAULT NULL,
+  `nomdemandeur` VARCHAR(50) DEFAULT NULL,
+  `details` TEXT,
+  `achat` DATE DEFAULT NULL,
+  `avancement` VARCHAR(255) DEFAULT NULL,
+  `termine` VARCHAR(15) DEFAULT NULL,
+  `piecesjointes` VARCHAR(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -84,11 +84,11 @@ CREATE TABLE `demandes` (
 
 DROP TABLE IF EXISTS `equipe`;
 CREATE TABLE `equipe` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` text,
-  `descr` varchar(255) NOT NULL DEFAULT '',
-  `compte` int(11) NOT NULL DEFAULT '0',
-  `chef` int(11) NOT NULL DEFAULT '0',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nom` TEXT,
+  `descr` VARCHAR(255) NOT NULL DEFAULT '',
+  `compte` INT(11) NOT NULL DEFAULT '0',
+  `chef` INT(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
   FOREIGN KEY (`chef`) REFERENCES `users` (`id`);
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
@@ -100,15 +100,15 @@ CREATE TABLE `equipe` (
 
 DROP TABLE IF EXISTS `fournisseurs`;
 CREATE TABLE `fournisseurs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(50) DEFAULT NULL,
-  `adresse` varchar(50) NOT NULL DEFAULT '',
-  `www` varchar(50) DEFAULT NULL,
-  `tel` varchar(15) DEFAULT '00 00 00 00 00',
-  `fax` varchar(15) DEFAULT '00 00 00 00 00',
-  `mail` varchar(50) DEFAULT NULL,
-  `contact` varchar(255) DEFAULT NULL,
-  `descr` varchar(255) NOT NULL DEFAULT '',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(50) DEFAULT NULL,
+  `adresse` VARCHAR(50) NOT NULL DEFAULT '',
+  `www` VARCHAR(50) DEFAULT NULL,
+  `tel` VARCHAR(15) DEFAULT '00 00 00 00 00',
+  `fax` VARCHAR(15) DEFAULT '00 00 00 00 00',
+  `mail` VARCHAR(50) DEFAULT NULL,
+  `contact` VARCHAR(255) DEFAULT NULL,
+  `descr` VARCHAR(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -119,13 +119,13 @@ CREATE TABLE `fournisseurs` (
 
 DROP TABLE IF EXISTS `intervention`;
 CREATE TABLE `intervention` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `descr` varchar(255) DEFAULT NULL,
-  `tech` int(11) NOT NULL DEFAULT '0',
-  `fournisseur` int(11) NOT NULL DEFAULT '0',
-  `date` date NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `facture` varchar(30) DEFAULT NULL,
-  `appareil` int(11) NOT NULL DEFAULT '0',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `descr` VARCHAR(255) DEFAULT NULL,
+  `tech` INT(11) NOT NULL DEFAULT '0',
+  `fournisseur` INT(11) NOT NULL DEFAULT '0',
+  `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `facture` VARCHAR(30) DEFAULT NULL,
+  `appareil` INT(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -136,16 +136,16 @@ CREATE TABLE `intervention` (
 
 DROP TABLE IF EXISTS `labview`;
 CREATE TABLE `labview` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `manipch` varchar(60) DEFAULT NULL,
-  `technicien` varchar(30) DEFAULT NULL,
-  `localisation` varchar(30) DEFAULT NULL,
-  `matos` varchar(100) DEFAULT NULL,
-  `code` text,
-  `driver` varchar(60) DEFAULT NULL,
-  `module` varchar(60) DEFAULT NULL,
-  `ecran` varchar(30) DEFAULT NULL,
-  `pdf` varchar(30) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `manipch` VARCHAR(60) DEFAULT NULL,
+  `technicien` VARCHAR(30) DEFAULT NULL,
+  `localisation` VARCHAR(30) DEFAULT NULL,
+  `matos` VARCHAR(100) DEFAULT NULL,
+  `code` TEXT,
+  `driver` VARCHAR(60) DEFAULT NULL,
+  `module` VARCHAR(60) DEFAULT NULL,
+  `ecran` VARCHAR(30) DEFAULT NULL,
+  `pdf` VARCHAR(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -158,15 +158,15 @@ CREATE TABLE `labview` (
 
 DROP TABLE IF EXISTS `manip`;
 CREATE TABLE `manip` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(20) NOT NULL DEFAULT '',
-  `descr` varchar(255) NOT NULL DEFAULT '',
-  `equipe` int(4) NOT NULL DEFAULT '0',
-  `chercheur` int(11) NOT NULL DEFAULT '0',
-  `chercheur_bis` int(11) DEFAULT NULL,
-  `assoc_proj` varchar(10) DEFAULT NULL,
-  `local` varchar(5) NOT NULL DEFAULT '',
-  `date` date NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(20) NOT NULL DEFAULT '',
+  `descr` VARCHAR(255) NOT NULL DEFAULT '',
+  `equipe` INT(4) NOT NULL DEFAULT '0',
+  `chercheur` INT(11) NOT NULL DEFAULT '0',
+  `chercheur_bis` INT(11) DEFAULT NULL,
+  `assoc_proj` VARCHAR(10) DEFAULT NULL,
+  `local` VARCHAR(5) NOT NULL DEFAULT '',
+  `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nom` (`nom`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
@@ -178,12 +178,12 @@ CREATE TABLE `manip` (
 
 DROP TABLE IF EXISTS `pret`;
 CREATE TABLE `pret` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(60) DEFAULT NULL,
-  `equipe` int(11) DEFAULT NULL,
-  `emprunt` date DEFAULT NULL,
-  `retour` date DEFAULT NULL,
-  `commentaire` varchar(60) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(60) DEFAULT NULL,
+  `equipe` INT(11) DEFAULT NULL,
+  `emprunt` DATE DEFAULT NULL,
+  `retour` DATE DEFAULT NULL,
+  `commentaire` VARCHAR(60) DEFAULT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`equipe`) REFERENCES `equipe` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
@@ -197,11 +197,11 @@ CREATE TABLE `pret` (
 
 DROP TABLE IF EXISTS `projet`;
 CREATE TABLE `projet` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `manip` int(11) NOT NULL DEFAULT '0',
-  `nom` varchar(20) NOT NULL DEFAULT '',
-  `descr` varchar(255) NOT NULL DEFAULT '',
-  `date` date NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `manip` INT(11) NOT NULL DEFAULT '0',
+  `nom` VARCHAR(20) NOT NULL DEFAULT '',
+  `descr` VARCHAR(255) NOT NULL DEFAULT '',
+  `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -214,14 +214,14 @@ CREATE TABLE `projet` (
 
 DROP TABLE IF EXISTS `tache`;
 CREATE TABLE `tache` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `projet` int(11) NOT NULL DEFAULT '0',
-  `date` date NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `nom` varchar(20) NOT NULL DEFAULT '',
-  `descr` varchar(255) NOT NULL DEFAULT '',
-  `user` varchar(30) DEFAULT NULL,
-  `fourniss` varchar(30) DEFAULT NULL,
-  `temps` int(4) unsigned NOT NULL DEFAULT '0',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `projet` INT(11) NOT NULL DEFAULT '0',
+  `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nom` VARCHAR(20) NOT NULL DEFAULT '',
+  `descr` VARCHAR(255) NOT NULL DEFAULT '',
+  `user` VARCHAR(30) DEFAULT NULL,
+  `fourniss` VARCHAR(30) DEFAULT NULL,
+  `temps` INT(4) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -232,12 +232,12 @@ CREATE TABLE `tache` (
 
 DROP TABLE IF EXISTS `temps`;
 CREATE TABLE `temps` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_tache` int(11) NOT NULL DEFAULT '0',
-  `user` int(11) DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  `duree` int(11) DEFAULT NULL,
-  `remarks` varchar(255) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_tache` INT(11) NOT NULL DEFAULT '0',
+  `user` INT(11) DEFAULT NULL,
+  `date` DATE DEFAULT NULL,
+  `duree` INT(11) DEFAULT NULL,
+  `remarks` VARCHAR(255) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -248,16 +248,16 @@ CREATE TABLE `temps` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `loggin` varchar(20) NOT NULL DEFAULT '',
-  `password` varchar(40) NOT NULL DEFAULT '',
-  `level` int(11) NOT NULL DEFAULT '1',
-  `nom` varchar(20) NOT NULL DEFAULT '',
-  `prenom` varchar(20) NOT NULL DEFAULT '',
-  `tel` int(11) NOT NULL DEFAULT '0',
-  `email` varchar(50) NOT NULL DEFAULT '',
-  `equipe` int(11) NOT NULL DEFAULT '1',
-  `valid` int(11) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `loggin` VARCHAR(20) NOT NULL DEFAULT '',
+  `password` VARCHAR(40) NOT NULL DEFAULT '',
+  `level` INT(11) NOT NULL DEFAULT '1',
+  `nom` VARCHAR(20) NOT NULL DEFAULT '',
+  `prenom` VARCHAR(20) NOT NULL DEFAULT '',
+  `tel` INT(11) NOT NULL DEFAULT '0',
+  `email` VARCHAR(50) NOT NULL DEFAULT '',
+  `equipe` INT(11) NOT NULL DEFAULT '1',
+  `valid` INT(11) DEFAULT NULL,
   `theme` VARCHAR (50) DEFAULT 'clair';
   PRIMARY KEY (`id`),
   UNIQUE KEY `loggin` (`loggin`),
@@ -271,10 +271,10 @@ CREATE TABLE `users` (
 
 DROP TABLE IF EXISTS `notice`;
 CREATE TABLE `notice` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `chemin_notice` varchar(500) NOT NULL DEFAULT '',
-  `nom_notice` varchar(150) NOT NULL DEFAULT '',
-  `id_appareil` int(11) NOT NULL DEFAULT '0',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `chemin_notice` VARCHAR(500) NOT NULL DEFAULT '',
+  `nom_notice` VARCHAR(150) NOT NULL DEFAULT '',
+  `id_appareil` INT(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
   FOREIGN KEY (`id_appareil`) REFERENCES `Listing` (`id`);
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
@@ -286,10 +286,10 @@ CREATE TABLE `notice` (
 
 DROP TABLE IF EXISTS `datasheet`;
 CREATE TABLE `datasheet` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pathname` varchar(500) NOT NULL DEFAULT '',
-  `description` varchar(150) NOT NULL DEFAULT '',
-  `id_equipment` int(11) NOT NULL DEFAULT '0',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `pathname` VARCHAR(500) NOT NULL DEFAULT '',
+  `description` VARCHAR(150) NOT NULL DEFAULT '',
+  `id_equipment` INT(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   FOREIGN KEY (`id_equipment`) REFERENCES `Listing` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
@@ -300,9 +300,9 @@ CREATE TABLE `datasheet` (
 
 DROP TABLE IF EXISTS `relation_equipment_datasheet`;
 CREATE TABLE `relation_equipment_datasheet` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_equipment` int(11) NOT NULL,
-  `id_datasheet` int(11) NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_equipment` INT(11) NOT NULL,
+  `id_datasheet` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`id_equipment`) REFERENCES `listing` (`id`),
   FOREIGN KEY (`id_datasheet`) REFERENCES `datasheet` (`id`)
@@ -315,9 +315,9 @@ CREATE TABLE `relation_equipment_datasheet` (
 
 DROP TABLE IF EXISTS `version`;
 CREATE TABLE `version` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `soft` varchar(20) NOT NULL DEFAULT '',
-  `version` int(11) NOT NULL DEFAULT '0',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `soft` VARCHAR(20) NOT NULL DEFAULT '',
+  `version` INT(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `soft` (`soft`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
