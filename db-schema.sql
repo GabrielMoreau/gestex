a9626dfcae9624efc821be288da21c731019f279
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-11-10 16:32:55 +0100

    hange comment from 60 to 100 !

--- db-schema.sql
+++ db-schema.sql
@@ -204,7 +204,7 @@ CREATE TABLE `pret` (
   `equipe` INT(11) DEFAULT NULL,
   `emprunt` DATE DEFAULT NULL,
   `retour` DATE DEFAULT NULL,
-  `commentaire` VARCHAR(60) DEFAULT NULL,
+  `commentaire` VARCHAR(100) DEFAULT NULL,
   `status` ENUM('LOAN_BORROWED','LOAN_RESERVED','LOAN_RETURNED') NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`equipe`) REFERENCES `equipe` (`id`)
