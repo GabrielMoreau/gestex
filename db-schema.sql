fb769e636dafa1a8db1a4ba5463e148f7df805d3
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-07-10 11:27:56 +0200

    Drop many old table

--- db-schema.sql
+++ db-schema.sql
@@ -30,23 +30,6 @@ CREATE TABLE `equipment` (
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 --
--- Table structure for table `appareils`
---
-
-DROP TABLE IF EXISTS `appareils`;
-CREATE TABLE `appareils` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(30) NOT NULL DEFAULT '',
-  `descr` VARCHAR(255) DEFAULT NULL,
-  `equipe` INT(11) NOT NULL DEFAULT 0,
-  `tech` INT(11) NOT NULL DEFAULT 0,
-  `fournisseur` INT(11) NOT NULL DEFAULT 0,
-  `achat` DATE DEFAULT NULL,
-  `facture` VARCHAR(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
---
 -- Table structure for table `category`
 --
 
@@ -69,23 +52,6 @@ CREATE TABLE `datasheet` (
   `id_equipment` INT(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`id_equipment`) REFERENCES `equipment` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `demandes`
---
-
-DROP TABLE IF EXISTS `demandes`;
-CREATE TABLE `demandes` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `tache` VARCHAR(200) DEFAULT NULL,
-  `nomdemandeur` VARCHAR(50) DEFAULT NULL,
-  `details` TEXT,
-  `achat` DATE DEFAULT NULL,
-  `avancement` VARCHAR(255) DEFAULT NULL,
-  `termine` VARCHAR(15) DEFAULT NULL,
-  `piecesjointes` VARCHAR(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 --
@@ -138,60 +104,6 @@ CREATE TABLE `intervention` (
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 --
--- Table structure for table `labview`
---
-
-DROP TABLE IF EXISTS `labview`;
-CREATE TABLE `labview` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `manipch` VARCHAR(60) DEFAULT NULL,
-  `technicien` VARCHAR(30) DEFAULT NULL,
-  `localisation` VARCHAR(30) DEFAULT NULL,
-  `matos` VARCHAR(100) DEFAULT NULL,
-  `code` TEXT,
-  `driver` VARCHAR(60) DEFAULT NULL,
-  `module` VARCHAR(60) DEFAULT NULL,
-  `ecran` VARCHAR(30) DEFAULT NULL,
-  `pdf` VARCHAR(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `manip`
---
-
--- liste des manips
-
-DROP TABLE IF EXISTS `manip`;
-CREATE TABLE `manip` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(20) NOT NULL DEFAULT '',
-  `descr` VARCHAR(255) NOT NULL DEFAULT '',
-  `equipe` INT(4) NOT NULL DEFAULT 0,
-  `chercheur` INT(11) NOT NULL DEFAULT 0,
-  `chercheur_bis` INT(11) DEFAULT NULL,
-  `assoc_proj` VARCHAR(10) DEFAULT NULL,
-  `local` VARCHAR(5) NOT NULL DEFAULT '',
-  `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `nom` (`nom`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `notice`
---
-
-DROP TABLE IF EXISTS `notice`;
-CREATE TABLE `notice` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `chemin_notice` VARCHAR(500) NOT NULL DEFAULT '',
-  `nom_notice` VARCHAR(150) NOT NULL DEFAULT '',
-  `id_appareil` INT(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-  FOREIGN KEY (`id_appareil`) REFERENCES `equipment` (`id`);
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
---
 -- Table structure for table `loan`
 --
 
@@ -209,22 +121,6 @@ CREATE TABLE `loan` (
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 --
--- Table structure for table `projet`
---
-
--- liste des projets d''une manip
-
-DROP TABLE IF EXISTS `projet`;
-CREATE TABLE `projet` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `manip` INT(11) NOT NULL DEFAULT 0,
-  `nom` VARCHAR(20) NOT NULL DEFAULT '',
-  `descr` VARCHAR(255) NOT NULL DEFAULT '',
-  `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
---
 -- Table structure for table `recipe`
 --
 
@@ -237,40 +133,6 @@ CREATE TABLE `recipe` (
   PRIMARY KEY (`id`),
   KEY `intervention_id` (`intervention_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `tache`
---
-
--- liste des taches d''un projet
-
-DROP TABLE IF EXISTS `tache`;
-CREATE TABLE `tache` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `projet` INT(11) NOT NULL DEFAULT 0,
-  `date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `nom` VARCHAR(20) NOT NULL DEFAULT '',
-  `descr` VARCHAR(255) NOT NULL DEFAULT '',
-  `user` VARCHAR(30) DEFAULT NULL,
-  `fourniss` VARCHAR(30) DEFAULT NULL,
-  `temps` INT(4) unsigned NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `temps`
---
-
-DROP TABLE IF EXISTS `temps`;
-CREATE TABLE `temps` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_tache` INT(11) NOT NULL DEFAULT 0,
-  `user` INT(11) DEFAULT NULL,
-  `date` DATE DEFAULT NULL,
-  `duree` INT(11) DEFAULT NULL,
-  `remarks` VARCHAR(255) DEFAULT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `users`
--- db-upgrade-5-6.sql
+++ db-upgrade-5-6.sql
@@ -19,6 +19,19 @@ RENAME TABLE `pret` TO `loan`;
 RENAME TABLE `categorie` TO `category`;
 
 --
+-- Drop old table
+--
+
+DROP TABLE IF EXISTS `appareils`;
+DROP TABLE IF EXISTS `demandes`;
+DROP TABLE IF EXISTS `labview`;
+DROP TABLE IF EXISTS `manip`;
+DROP TABLE IF EXISTS `notice`;
+DROP TABLE IF EXISTS `projet`;
+DROP TABLE IF EXISTS `tache`;
+DROP TABLE IF EXISTS `temps`;
+
+--
 -- Fix global DB version
 --
 
