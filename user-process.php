11d0324516c2244ef68c325a3bb30bb9a67d3881
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-07-16 18:18:49 +0200

    Empty team

--- user-process.php
+++ user-process.php
@@ -73,7 +73,7 @@ if ($flag_new) { // new
 	level_or_alert(3, 'Validation d’un nouvel utilisateur');
 
 	$new_pwhash = password_hash($password, PASSWORD_DEFAULT);
-	if ($team_id === 0) {
+	if ($team_id === 0 || $team_id === '') {
 		$team_id = 1;
 	}
 	list($user_id, $err_msg) = set_user_new($pdo, $familyname, $firstname, $username, $new_pwhash, $mail, $level, $phone, $team_id, $theme);
