4280a1981a3721d669d89d3245bded63835ee108
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-07-17 11:07:03 +0200

    Better message

--- include/alert-resource.php
+++ include/alert-resource.php
@@ -10,10 +10,9 @@
 <div class="box-alert">
 	<center>
 		Une erreur est survenue lors du traitement de votre demande.
-		L’accès à la ressource est impossible, car celle-ci n’existe pas <i>a priori</i>.
-		<ul>
-			<li>ressource : <?=$resource_name?> = <?=$resource_index?></li>
-		</ul>
+		L’accès à la ressource demandée est impossible, car celle-ci n’existe probablement pas.
+		<br>
+		<b>Ressource</b> : <?=$resource_name?> = <?=$resource_index?>
 		<br>
 		Si vous avez été sur ce lien erroné via l'application (en suivant les liens proposés),
 		veuillez voir avec votre administrateur pour corriger éventuellement les problèmes.
