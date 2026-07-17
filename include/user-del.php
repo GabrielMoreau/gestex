d4e6998df9894b8f1c97c4ee06fabca652adf59e
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-07-17 15:26:36 +0200

    Better text

--- include/user-del.php
+++ include/user-del.php
@@ -6,21 +6,21 @@
 // $user_fullname
 ?>
 
-$user_status_str = 'déactivé';
+$user_status_msg = 'déactivé';
 if ($user_status === '0')
-	$user_status_str = 'activé';
+	$user_status_msg = 'activé';
 
-<?php en_tete('Changer l’état de l’utilisateur '.$user_fullname.'('.$user_status_str.')'); ?>
+<?php en_tete('Changer l’état de l’utilisateur '.$user_fullname.' ('.$user_status_msg.')'); ?>
 
 <center class="alert">
 <form action="user-del.php" method="POST">
 	<input type="hidden" name="user_id" value="<?=$user_id?>">
 	<input type="hidden" name="status" value="<?=$user_status?>">
-	Voulez-vous changer l’état de l'utilisateur <?=$user_fullname?> (actuellement <?=$user_status_str?>) ?
+	Voulez-vous changer l’état de l'utilisateur <?=$user_fullname?> (actuellement <?=$user_status_msg?>) ?
 	<button class="red" type="submit" name="ok" value="yes">Oui</button>
 	<button class="green" type="submit" formaction="user-list.php" value="no">Non</button>
 	<hr>
-	Voulez-vous supprimer définitivement l'utilisateur <?=$user_fullname?> (#<?=$user_id?>) ?
+	Voulez-vous supprimer <b>définitivement</b> l'utilisateur <?=$user_fullname?> (#<?=$user_id?>) ?
 	<button class="red" type="submit" name="ok" value="destroy">Supprimer</button>
 	<hr>
 	<button type="submit" name="ok" value="cancel">Annuler</button>
