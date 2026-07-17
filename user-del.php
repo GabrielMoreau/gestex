18d0a9156dad5dcde8cb9b1192936050c08cf7e5
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-07-17 16:02:52 +0200

    Good test

--- user-del.php
+++ user-del.php
@@ -14,16 +14,16 @@ $user_id     = param_post_or_get('user_id', 0);
 $valid       = param_post('ok', 'no');
 $user_status = param_post_or_get('status', 0);
 
-if ($user_id == 0 || $valid == 'cancel')
+if ($user_id === 0 || $valid === 'cancel')
 	redirect('user-list.php');
 
 $pdo = connect_db_or_alert();
 $user_selected = get_user_short_by_id($pdo, $user_id);
 $user_fullname = $user_selected['firstname'].' '.$user_selected['familyname'];
 
-if ($valid == 'yes') {
+if ($valid === 'yes') {
 	// Changer le statut de cet utilisateur
-	if ($user_status == 0 || $user_status == 1) {
+	if ($user_status === 0 || $user_status === 1) {
 		$iostat = set_user_valid_by_id($pdo, $user_id, (($user_status + 1) % 2));
 		if ($iostat) // Ça a marché
 			redirect('user-list.php?highlight='.$user_id.'#item'.$user_id);
@@ -36,11 +36,11 @@ if ($valid == 'yes') {
 	redirect('user-list.php?highlight='.$user_id.'#item'.$user_id);
 }
 
-if ($valid == 'destroy') {
+if ($valid === 'destroy') {
 	// Supprimer cet utilisateur
 	// On commence par le déactiver
-	if ($user_selected['valid'] === 0) {
-		$iostat = set_user_valid_by_id($pdo, $user_id, ((0 + 1) % 2));
+	if ($user_selected['valid'] === 1) {
+		$iostat = set_user_valid_by_id($pdo, $user_id, ((1 + 1) % 2));
 		if ($iostat) // Ça a marché
 			redirect('user-list.php?highlight='.$user_id.'#item'.$user_id);
 		$message_alert = 'Erreur dans le changement du statut de l’utilisateur : '.$user_fullname.' (#'.$user_id.')';
@@ -65,7 +65,7 @@ if ($valid == 'destroy') {
 		} catch (PDOException $e) {
 			// Erreur 1451 = contrainte de clef étrangère
 			$err_msg = 'Impossible de supprimer cet utilisateur.';
-			if ($e->errorInfo[1] == 1451) {
+			if ($e->errorInfo[1] === 1451) {
 				$err_msg = 'Impossible de supprimer cet utilisateur : il est référencé ailleurs.';
 			}
 			en_tete('Échec de la suppression de l’utilisateur '.$user_fullname);
